a782804008da63c2c9a16aa200d7a9cc6b7066c2
george@moralis.gr <georgemoralis@users.noreply.github.com>
2008-07-22 17:13:05 +0000

    all syscalls of minifire demos disasm okay. Helper function in disasm . A lot new disasm opcodes

--- src/jpcsp/Debugger/Disasembler.java
+++ src/jpcsp/Debugger/Disasembler.java
@@ -37,12 +37,12 @@ public class Disasembler extends javax.swing.JInternalFrame {
 
     /** Creates new form Disasembler */
     public Disasembler() {
-        
+
         DebuggerPC = 0;
         model_1 = new DefaultListModel();
         initComponents();
         RefreshDebugger();
-        
+
     }
 
     /** This method is called from within the constructor to
@@ -91,31 +91,27 @@ public class Disasembler extends javax.swing.JInternalFrame {
 private void jList1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jList1KeyPressed
 // TODO add your handling code here:
 
-        if(evt.getKeyCode() == java.awt.event.KeyEvent.VK_DOWN && jList1.getSelectedIndex() == jList1.getLastVisibleIndex())
-        {
-            DebuggerPC+=4;
-            RefreshDebugger();
-            evt.consume();
-            jList1.setSelectedIndex(jList1.getLastVisibleIndex());
-        }else if(evt.getKeyCode() == java.awt.event.KeyEvent.VK_UP && jList1.getSelectedIndex() == 0)
-        {
-            DebuggerPC-=4;
-            RefreshDebugger();
-            evt.consume();
-            jList1.setSelectedIndex(0);
-        }else if(evt.getKeyCode() == java.awt.event.KeyEvent.VK_PAGE_UP && jList1.getSelectedIndex() == 0)
-        {
-            DebuggerPC-=74;
-            RefreshDebugger();
-            evt.consume();
-            jList1.setSelectedIndex(0);
-        }else if(evt.getKeyCode() == java.awt.event.KeyEvent.VK_PAGE_DOWN && jList1.getSelectedIndex() == jList1.getLastVisibleIndex())
-        {
-            DebuggerPC+=74;
-            RefreshDebugger();
-            evt.consume();
-            jList1.setSelectedIndex(jList1.getLastVisibleIndex());
-        }
+    if (evt.getKeyCode() == java.awt.event.KeyEvent.VK_DOWN && jList1.getSelectedIndex() == jList1.getLastVisibleIndex()) {
+        DebuggerPC += 4;
+        RefreshDebugger();
+        evt.consume();
+        jList1.setSelectedIndex(jList1.getLastVisibleIndex());
+    } else if (evt.getKeyCode() == java.awt.event.KeyEvent.VK_UP && jList1.getSelectedIndex() == 0) {
+        DebuggerPC -= 4;
+        RefreshDebugger();
+        evt.consume();
+        jList1.setSelectedIndex(0);
+    } else if (evt.getKeyCode() == java.awt.event.KeyEvent.VK_PAGE_UP && jList1.getSelectedIndex() == 0) {
+        DebuggerPC -= 74;
+        RefreshDebugger();
+        evt.consume();
+        jList1.setSelectedIndex(0);
+    } else if (evt.getKeyCode() == java.awt.event.KeyEvent.VK_PAGE_DOWN && jList1.getSelectedIndex() == jList1.getLastVisibleIndex()) {
+        DebuggerPC += 74;
+        RefreshDebugger();
+        evt.consume();
+        jList1.setSelectedIndex(jList1.getLastVisibleIndex());
+    }
 }//GEN-LAST:event_jList1KeyPressed
 
     public void RefreshDebugger() {
@@ -125,8 +121,8 @@ private void jList1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jL
             DebuggerPC = 0x08900000;//test
         }
         model_1.clear();
-        
-        for (t = DebuggerPC  , cnt = 0; t < (DebuggerPC + 0x00000074); t += 0x00000004, cnt++) {
+
+        for (t = DebuggerPC    , cnt = 0; t < (DebuggerPC + 0x00000074); t += 0x00000004, cnt++) {
 
             int memread = Memory.get_instance().read32((int) t);
             if (memread == 0) {
@@ -138,78 +134,146 @@ private void jList1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jL
 
     }
 
-   String disasm(int value) {
+    String disasm(int value) {
         String s = new String();
 
         int rs = (value >> 21) & 0x1f;
         int rt = (value >> 16) & 0x1f;
         int rd = (value >> 11) & 0x1f;
         int imm = value & 0xffff;
-        if ((imm & 0x8000) == 0x8000) 
-        {
-	   imm |= 0xffff0000;
-	}
+        if ((imm & 0x8000) == 0x8000) {
+            imm |= 0xffff0000;
+        }
         int opcode = (value >> 26) & 0x3f;
-        
+
         //s = Integer.toString(opcode);
         switch (opcode) {
-            case  0: //Special table 
+            case 0: //Special table 
                 int specialop = (value & 0x3f);
-                switch(specialop) {
+                switch (specialop) {
+                    case 4: //sllv
+                        s = s + Dis_RDRSRT("sllv" ,value);
+                        break; 
+                    case 6://srlv
+                        s = s + Dis_RDRSRT("srlv" ,value);
+                        break;
+                    case 7://srav
+                        s = s + Dis_RDRSRT("srav" ,value);
+                        break;
                     case 12://syscall
-                        int code = (value >> 6) & 0xFFFFF;
-                                            
-                        for(syscalls.calls c : syscalls.calls.values())
-                        {
-                            //System.out.println(c.getValue());
-                            if(c.getValue() == code)
-                            {
-                                s=s + "syscall " + Integer.toHexString(code) + "     " + c;
-                                break;
-                            }
-                           // else
-                           // {
-                           ///     
-                           //     //break;
-                           // }
-                        }
-                        //s=s + "syscall " + Integer.toHexString(code);   
+                        s = s + Dis_Syscall(value);
+                        break;
+                    case 32://add
+                        s = s + Dis_RDRSRT("add" ,value);
                         break;
                     case 33://addu
-                       if (rs==0 && rt==0)
-                        s= s+ "li " + cpuregs[rd] + " ,0"; 
-                       else if (rs == 0)
-                        s= s + "move " + cpuregs[rd] + " ," + cpuregs[rs];
-		       else if (rt == 0)
-                        s= s + "move " + cpuregs[rd] + " ," + cpuregs[rs];
-		       else
-                        s= s + "addu " + cpuregs[rd] + " ," + cpuregs[rs] + " , " + cpuregs[rt];
-                    break;   
+                        s = s + Dis_RDRSRT("addu" ,value);
+                        break;
+                    case 34://sub
+                        s = s + Dis_RDRSRT("sub" ,value);
+                        break;
+                    case 35://subu
+                        s = s + Dis_RDRSRT("subu" ,value);
+                        break;
+                    case 36://and
+                        s = s + Dis_RDRSRT("and" ,value);
+                        break;                        
+                    case 37://or
+                         s = s + Dis_RDRSRT("or" ,value);
+                        break;                       
+                    case 38://xor
+                        s = s + Dis_RDRSRT("xor" ,value);
+                        break;                        
+                    case 39://nor
+                        s = s + Dis_RDRSRT("nor" ,value);
+                        break;                        
+                    case 42://slt
+                        s = s + Dis_RDRSRT("slt" ,value);
+                        break;
+                    case 43://sltu
+                        s = s + Dis_RDRSRT("sltu" ,value);
+                        break;
                     default:
-                    //s = "special + " + Integer.toString(specialop); 
-                    break;
+                       // s = "special + " + Integer.toString(specialop);
+                        break;
                 }
                 break;
-            case  9: //addiu
-                if(rs==0)
-                {
-                    s  = s + "li " + cpuregs[rt] + " ," + imm;
-                }
-                else
-                {
-                 s= s + "addiu " + cpuregs[rt] + " ," + cpuregs[rs] + " , " + imm;
-                }
+            /*case  1: //Bcond table  //the immediate is not correct!
+            int code = value & 0x1f0000;
+            if (code == 0) 
+            s = s + "bltz " + cpuregs[rs] + ", " + (value & 0xffff)*0x04;
+            else if (code == 0x10000) 
+            s = s + "bgez " + cpuregs[rs] + ", " + Integer.toHexString((((value & 0xffff)<<2))*4);
+            else if (code == 0x100000) 
+            s = s + "bltzal " + cpuregs[rs] + ", " + imm*0x04;
+            else if (code == 0x110000) 
+            s = s + "bgezal " + cpuregs[rs] + ", " + imm*0x04;
+            else
+            s = s + "unimplement Bcond opcode";
+            break;    */
+            case 9: //addiu
+                s = s + Dis_RTRSIMM("addiu",value);
+                break;
+            case 13: //ori   
+                s = s + Dis_RTRSIMM("ori",value);
                 break;
             case 15://lui
                 s = s + "lui " + cpuregs[rt] + " , " + imm;
                 break;
             default:
-               // s = Integer.toString(opcode);
+                //s = Integer.toString(opcode);
                 break;
 
 
         }
         return s;
+    }
+    
+    private String Dis_Syscall(int value) {  /* probably okay */
+        String s = new String();
+        int code = (value >> 6) & 0xFFFFF;
+        for (syscalls.calls c : syscalls.calls.values()) {
+            if (c.getValue() == code) {
+                s = "syscall " + Integer.toHexString(code) + "     " + c;
+                return s;
+
+            }
+        }
+        s = "syscall 0x" + Integer.toHexString(code) + " [unknown]";
+        return s;
+    }
+    private String Dis_RDRSRT(String opname,int value)
+    {
+        
+        int rs = (value >> 21) & 0x1f;
+        int rt = (value >> 16) & 0x1f;
+        int rd = (value >> 11) & 0x1f;
+        if (rs == 0 && rt == 0) 
+             return "li " + cpuregs[rd] + " ,0";
+         else if (rs == 0) 
+             return "move " + cpuregs[rd] + " ," + cpuregs[rs];
+         else if (rt == 0) {
+             return "move " + cpuregs[rd] + " ," + cpuregs[rs];
+         } else {
+             return opname + " " + cpuregs[rd] + " ," + cpuregs[rs] + " , " + cpuregs[rt];
+         }
+        
+    }
+    private String Dis_RTRSIMM(String opname,int value)
+    {
+        int rs = (value >> 21) & 0x1f;
+        int rt = (value >> 16) & 0x1f;
+        int rd = (value >> 11) & 0x1f;
+        int imm = value & 0xffff;
+        if ((imm & 0x8000) == 0x8000) {
+            imm |= 0xffff0000;
+        } 
+       if (rs == 0) 
+          return  "li " + cpuregs[rt] + " ," + imm;
+       else 
+          return  opname + " " + cpuregs[rt] + " ," + cpuregs[rs] + " , " + imm;
+              
+        
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JList jList1;
