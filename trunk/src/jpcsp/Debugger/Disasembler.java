3f441549450128b33d21821fc90803a908dc5586
xtsoler <xtsoler@4ab978e6-3352-0410-996c-0b42db0051c8>
2008-07-23 14:22:44 +0000

    disasembler window now scrollable with mouse roller.started registers window

--- trunk/src/jpcsp/Debugger/Disasembler.java
+++ trunk/src/jpcsp/Debugger/Disasembler.java
@@ -35,11 +35,12 @@ public class Disasembler extends javax.swing.JInternalFrame {
     long DebuggerPC;
     private DefaultListModel model_1 = new DefaultListModel();
     int pcreg;
+
     /** Creates new form Disasembler */
     public Disasembler(Processor c) {
 
-        DebuggerPC =0;
-        pcreg=c.pc;
+        DebuggerPC = 0;
+        pcreg = c.pc;
         model_1 = new DefaultListModel();
         initComponents();
         RefreshDebugger();
@@ -63,6 +64,11 @@ public class Disasembler extends javax.swing.JInternalFrame {
         setTitle("Disassembler");
 
         jList1.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jList1.addMouseWheelListener(new java.awt.event.MouseWheelListener() {
+            public void mouseWheelMoved(java.awt.event.MouseWheelEvent evt) {
+                jList1MouseWheelMoved(evt);
+            }
+        });
         jList1.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 jList1KeyPressed(evt);
@@ -82,7 +88,7 @@ public class Disasembler extends javax.swing.JInternalFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(23, Short.MAX_VALUE)
+                .addContainerGap(31, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 469, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
@@ -115,6 +121,29 @@ private void jList1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jL
     }
 }//GEN-LAST:event_jList1KeyPressed
 
+private void jList1MouseWheelMoved(java.awt.event.MouseWheelEvent evt) {//GEN-FIRST:event_jList1MouseWheelMoved
+// TODO add your handling code here:
+    if (evt.getWheelRotation() < 0) {
+        evt.consume();
+        if (jList1.getSelectedIndex() == 0) {
+            DebuggerPC -= 4;
+            RefreshDebugger();
+            jList1.setSelectedIndex(0);
+        } else {
+            jList1.setSelectedIndex(jList1.getSelectedIndex() - 1);
+        }
+    } else {
+        evt.consume();
+        if (jList1.getSelectedIndex() == jList1.getLastVisibleIndex()) {
+            DebuggerPC += 4;
+            RefreshDebugger();
+            jList1.setSelectedIndex(jList1.getLastVisibleIndex());
+        } else {
+            jList1.setSelectedIndex(jList1.getSelectedIndex() + 1);
+        }
+    }
+}//GEN-LAST:event_jList1MouseWheelMoved
+
     public void RefreshDebugger() {
         long t;
         long cnt;
@@ -123,7 +152,7 @@ private void jList1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jL
         }
         model_1.clear();
 
-        for (t = DebuggerPC    , cnt = 0; t < (DebuggerPC + 0x00000074); t += 0x00000004, cnt++) {
+        for (t = DebuggerPC      , cnt = 0; t < (DebuggerPC + 0x00000074); t += 0x00000004, cnt++) {
 
             int memread = Memory.get_instance().read32((int) t);
             if (memread == 0) {
@@ -153,49 +182,49 @@ private void jList1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jL
                 int specialop = (value & 0x3f);
                 switch (specialop) {
                     case 4: //sllv
-                        s = s + Dis_RDRSRT("sllv" ,value);
-                        break; 
+                        s = s + Dis_RDRSRT("sllv", value);
+                        break;
                     case 6://srlv
-                        s = s + Dis_RDRSRT("srlv" ,value);
+                        s = s + Dis_RDRSRT("srlv", value);
                         break;
                     case 7://srav
-                        s = s + Dis_RDRSRT("srav" ,value);
+                        s = s + Dis_RDRSRT("srav", value);
                         break;
                     case 12://syscall
                         s = s + Dis_Syscall(value);
                         break;
                     case 32://add
-                        s = s + Dis_RDRSRT("add" ,value);
+                        s = s + Dis_RDRSRT("add", value);
                         break;
                     case 33://addu
-                        s = s + Dis_RDRSRT("addu" ,value);
+                        s = s + Dis_RDRSRT("addu", value);
                         break;
                     case 34://sub
-                        s = s + Dis_RDRSRT("sub" ,value);
+                        s = s + Dis_RDRSRT("sub", value);
                         break;
                     case 35://subu
-                        s = s + Dis_RDRSRT("subu" ,value);
+                        s = s + Dis_RDRSRT("subu", value);
                         break;
                     case 36://and
-                        s = s + Dis_RDRSRT("and" ,value);
-                        break;                        
+                        s = s + Dis_RDRSRT("and", value);
+                        break;
                     case 37://or
-                         s = s + Dis_RDRSRT("or" ,value);
-                        break;                       
+                        s = s + Dis_RDRSRT("or", value);
+                        break;
                     case 38://xor
-                        s = s + Dis_RDRSRT("xor" ,value);
-                        break;                        
+                        s = s + Dis_RDRSRT("xor", value);
+                        break;
                     case 39://nor
-                        s = s + Dis_RDRSRT("nor" ,value);
-                        break;                        
+                        s = s + Dis_RDRSRT("nor", value);
+                        break;
                     case 42://slt
-                        s = s + Dis_RDRSRT("slt" ,value);
+                        s = s + Dis_RDRSRT("slt", value);
                         break;
                     case 43://sltu
-                        s = s + Dis_RDRSRT("sltu" ,value);
+                        s = s + Dis_RDRSRT("sltu", value);
                         break;
                     default:
-                       // s = "special + " + Integer.toString(specialop);
+                        // s = "special + " + Integer.toString(specialop);
                         break;
                 }
                 break;
@@ -213,10 +242,10 @@ private void jList1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jL
             s = s + "unimplement Bcond opcode";
             break;    */
             case 9: //addiu
-                s = s + Dis_RTRSIMM("addiu",value);
+                s = s + Dis_RTRSIMM("addiu", value);
                 break;
             case 13: //ori   
-                s = s + Dis_RTRSIMM("ori",value);
+                s = s + Dis_RTRSIMM("ori", value);
                 break;
             case 15://lui
                 s = s + "lui " + cpuregs[rt] + " , " + imm;
@@ -229,7 +258,7 @@ private void jList1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jL
         }
         return s;
     }
-    
+
     private String Dis_Syscall(int value) {  /* probably okay */
         String s = new String();
         int code = (value >> 6) & 0xFFFFF;
@@ -243,38 +272,37 @@ private void jList1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jL
         s = "syscall 0x" + Integer.toHexString(code) + " [unknown]";
         return s;
     }
-    private String Dis_RDRSRT(String opname,int value)
-    {
-        
+
+    private String Dis_RDRSRT(String opname, int value) {
+
         int rs = (value >> 21) & 0x1f;
         int rt = (value >> 16) & 0x1f;
         int rd = (value >> 11) & 0x1f;
-        if (rs == 0 && rt == 0) 
-             return "li " + cpuregs[rd] + " ,0";
-         else if (rs == 0) 
-             return "move " + cpuregs[rd] + " ," + cpuregs[rs];
-         else if (rt == 0) {
-             return "move " + cpuregs[rd] + " ," + cpuregs[rs];
-         } else {
-             return opname + " " + cpuregs[rd] + " ," + cpuregs[rs] + " , " + cpuregs[rt];
-         }
-        
+        if (rs == 0 && rt == 0) {
+            return "li " + cpuregs[rd] + " ,0";
+        } else if (rs == 0) {
+            return "move " + cpuregs[rd] + " ," + cpuregs[rs];
+        } else if (rt == 0) {
+            return "move " + cpuregs[rd] + " ," + cpuregs[rs];
+        } else {
+            return opname + " " + cpuregs[rd] + " ," + cpuregs[rs] + " , " + cpuregs[rt];
+        }
+
     }
-    private String Dis_RTRSIMM(String opname,int value)
-    {
+
+    private String Dis_RTRSIMM(String opname, int value) {
         int rs = (value >> 21) & 0x1f;
         int rt = (value >> 16) & 0x1f;
         int rd = (value >> 11) & 0x1f;
         int imm = value & 0xffff;
         if ((imm & 0x8000) == 0x8000) {
             imm |= 0xffff0000;
-        } 
-       if (rs == 0) 
-          return  "li " + cpuregs[rt] + " ," + imm;
-       else 
-          return  opname + " " + cpuregs[rt] + " ," + cpuregs[rs] + " , " + imm;
-              
-        
+        }
+        if (rs == 0) {
+            return "li " + cpuregs[rt] + " ," + imm;
+        } else {
+            return opname + " " + cpuregs[rt] + " ," + cpuregs[rs] + " , " + imm;
+        }
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JList jList1;
