d8375cc5944ffefa887e8abee3cddbbfc7014f84
leandro.rhcp99 <leandro.rhcp99@4ab978e6-3352-0410-996c-0b42db0051c8>
2008-07-27 02:37:38 +0000

    project information centered on this place

---
+++ trunk/src/jpcsp/info/MetaInformation.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jpcsp.info;
+
+/**
+ *
+ * @author Leandro
+ */
+public class MetaInformation {
+    public static String NAME = "Jpcsp";
+    public static String VERSION = "v0.03";
+    public static String FULL_NAME = NAME + " " + VERSION;
+    public static String OFICIAL_SITE = "http://jpcsp.org/";
+    public static String OFICIAL_FORUM = "http://jpcsp.org/forum/";
+    public static String OFICIAL_REPOSITORY = "http://code.google.com/p/jpcsp/";
+    public static String TEAM = "gmoralis, xtsoler, dreampeppers99";
+}
