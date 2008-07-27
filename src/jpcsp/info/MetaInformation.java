e48e4d4433ab4e5cceee15ae9e3e0e6f514eb70d
leandro.rhcp99 <leandro.rhcp99@gmail.com>
2008-07-27 02:47:33 +0000

    miss spelling correction

--- src/jpcsp/info/MetaInformation.java
+++ src/jpcsp/info/MetaInformation.java
@@ -13,8 +13,8 @@ public class MetaInformation {
     public static String NAME = "Jpcsp";
     public static String VERSION = "v0.03";
     public static String FULL_NAME = NAME + " " + VERSION;
-    public static String OFICIAL_SITE = "http://jpcsp.org/";
-    public static String OFICIAL_FORUM = "http://jpcsp.org/forum/";
-    public static String OFICIAL_REPOSITORY = "http://code.google.com/p/jpcsp/";
+    public static String OFFICIAL_SITE = "http://jpcsp.org/";
+    public static String OFFICIAL_FORUM = "http://jpcsp.org/forum/";
+    public static String OFFICIAL_REPOSITORY = "http://code.google.com/p/jpcsp/";
     public static String TEAM = "gmoralis, xtsoler, dreampeppers99";
 }
