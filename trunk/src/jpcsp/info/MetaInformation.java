cad7d9099eedc5793b80bf6e43cc967203c7b193
george@moralis.gr <george@moralis.gr@4ab978e6-3352-0410-996c-0b42db0051c8>
2008-07-30 10:08:22 +0000

    Added textfield for entering address in memory viewer a few missing GPL headers update version number to v0.04

--- trunk/src/jpcsp/info/MetaInformation.java
+++ trunk/src/jpcsp/info/MetaInformation.java
@@ -19,7 +19,7 @@ package jpcsp.info;
 
 public class MetaInformation {
     public static String NAME = "Jpcsp";
-    public static String VERSION = "v0.03";
+    public static String VERSION = "v0.04";
     public static String FULL_NAME = NAME + " " + VERSION;
     public static String OFFICIAL_SITE = "http://jpcsp.org/";
     public static String OFFICIAL_FORUM = "http://jpcsp.org/forum/";
