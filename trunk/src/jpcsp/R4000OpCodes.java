cbb3a378326187a7d1664724f997efa277f18460
george@moralis.gr <george@moralis.gr@4ab978e6-3352-0410-996c-0b42db0051c8>
2008-07-31 09:54:49 +0000

    Redone the R4000 opcodes a bit better . Not all have added but it is enough for now i think.

--- trunk/src/jpcsp/R4000OpCodes.java
+++ trunk/src/jpcsp/R4000OpCodes.java
@@ -17,81 +17,132 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
 package jpcsp;
 
 public class R4000OpCodes {
-
-    //Load and Store Instructions
-    public static final byte LB = 0x20; //Load Byte
-    public static final byte LBU = 0x24; // Load Byte Unsigned
-    public static final byte LH = 0x21; // Load Halfword
-    public static final byte LHU = 0x25; // Load Halfword Unsigned
-    public static final byte LW = 0x23; // Load Word
-    public static final byte LWL = 0x22; // Load Word Left
-    public static final byte LWR = 0x26; // Load Word Right
-    public static final byte SB = 0x28; // Store Byte
-    public static final byte SH = 0x29; // Store Halfword
-    public static final byte SW = 0x2B; // Store Word
-    public static final byte SWL = 0x2A; // Store Word Left
-    public static final byte SWR = 0x2E; // Store Word Right
-
-    //Arithmetic Instructions (ALU Immediate)
-    public static final byte ADDI = 0x8; // Add Immediate
-    public static final byte ADDIU = 0x9; // Add Immediate Unsigned
-    public static final byte SLTI = 0xA; // Set on Less Than Immediate
-    public static final byte SLTIU = 0xB; // Set on Less Than Immediate Unsigned
-    public static final byte ANDI = 0xC; // AND Immediate
-    public static final byte ORI = 0xD; // OR Immediate
-    public static final byte XORI = 0xE; // Exclusive OR Immediate
-    public static final byte LUI = 0xF; // Load Upper Immediate
-    
-    
-    //Arithmetic (3-Operand, R-Type)
-    public static final byte ADD = 0x0; // Add
-    public static final byte ADDU = 0x0; // Add Unsigned
-    public static final byte SUB = 0x0; // Subtract
-    public static final byte SUBU = 0x0; // Subtract Unsigned
-    public static final byte SLT = 0x0; // Set on Less Than
-    public static final byte SLTU = 0x0; // Set on Less Than Unsigned
-    public static final byte AND = 0x0; // AND
-    public static final byte OR = 0x0; // OR
-    public static final byte XOR = 0x0; // Exclusive OR
-    public static final byte NOR = 0x0; // NOR
-
-    
-    //Multiply and Divide Instructions
-    public static final byte MULT = 0x0; // Multiply
-    public static final byte MULTU = 0x0; // Multiply Unsigned
-    public static final byte DIV = 0x0; // Divide
-    public static final byte DIVU = 0x0; // Divide Unsigned
-    public static final byte MFHI = 0x0; // Move From HI
-    public static final byte MTHI = 0x0; // Move To HI
-    public static final byte MFLO = 0x0; // Move From LO
-    public static final byte MTLO = 0x0; // Move To LO
-    
-    
-    //Jump and Branch Instructions
+/*
+    CPU: Instructions encoded by opcode field.  NOT COMPLETE!! (31/07/1008) shadow
+    31---------26---------------------------------------------------0
+    |  opcode   |                                                   |
+    ------6----------------------------------------------------------
+    |--000--|--001--|--010--|--011--|--100--|--101--|--110--|--111--| lo
+000 | *1    | *2    | J     | JAL   | BEQ   | BNE   | BLEZ  | BGTZ  |
+001 | ADDI  | ADDIU | SLTI  | SLTIU | ANDI  | ORI   | XORI  | LUI   |
+010 | *3    | *4    | ---   | ---   | BEQL  | BNEL  | BLEZL | BGTZL |
+011 | ---   | ---   | ---   | ---   | ---   |  ---  | ---   | ---   |
+100 | LB    | LH    | LWL   | LW    | LBU   | LHU   | LWR   | ---   |
+101 | SB    | SH    | SWL   | SW    | ---   | ---   | SWR   | CACHE |
+110 | ---   | LWC1  | ---   | PREF  | ---   | ---   | ---   | ---   |
+111 | ---   | SWC1  | ---   | ---   | ---   | ---   | ---   | ---   |
+ hi |-------|-------|-------|-------|-------|-------|-------|-------|
+     *1 = SPECIAL, see SPECIAL list    *2 = REGIMM, see REGIMM list
+     *3 = COP0                         *4 = COP1     
+*/
     public static final byte J = 0x2; // Jump
     public static final byte JAL = 0x3; // Jump And Link
-    public static final byte JR = 0x0; // Jump Register
-    public static final byte JALR = 0x0; // Jump And Link Register
     public static final byte BEQ = 0x4; // Branch on Equal
     public static final byte BNE = 0x5; // Branch on Not Equal
     public static final byte BLEZ = 0x6; // Branch on Less Than or Equal to Zero
     public static final byte BGTZ = 0x7; // Branch on Greater Than Zero
-    public static final byte BLTZ = 0x1; // Branch on Less Than Zero
-    public static final byte BGEZ = 0x1; // Branch on Greater Than or Equal to Zero
-    public static final byte BLTZAL = 0x1; // Branch on Less Than Zero And Link
-    public static final byte BGEZAL = 0x1; // Branch on Greater Than or Equal to Zero And Link
+    public static final byte ADDI = 0x8; // Add Immediate
+    public static final byte ADDIU = 0x9; // Add Immediate Unsigned
+    public static final byte SLTI = 0xa; // Set on Less Than Immediate
+    public static final byte SLTIU = 0xb; // Set on Less Than Immediate Unsigned
+    public static final byte ANDI = 0xc; // AND Immediate
+    public static final byte ORI = 0xd; // OR Immediate
+    public static final byte XORI = 0xe; // Exclusive OR Immediate
+    public static final byte LUI = 0xf; // Load Upper Immediate
+    public static final byte COP0 = 0x10; // Coprocessor Operation 
+    public static final byte COP1 = 0x11; // Coprocessor Operation 
+    public static final byte BEQL = 0x14; // Branch on Equal Likely
+    public static final byte BNEL = 0x15; // Branch on Not Equal Likely
+    public static final byte BLEZL = 0x16; // Branch on Less Than or Equal to Zero Likely
+    public static final byte BGTZL = 0x17; // Branch on Greater Than Zero Likely
+    public static final byte LB = 0x20; //Load Byte
+    public static final byte LH = 0x21; // Load Halfword
+    public static final byte LWL = 0x22; // Load Word Left
+    public static final byte LW = 0x23; // Load Word
+    public static final byte LBU = 0x24; // Load Byte Unsigned
+    public static final byte LHU = 0x25; // Load Halfword Unsigned
+    public static final byte LWR = 0x26; // Load Word Right
+    public static final byte SB = 0x28; // Store Byte
+    public static final byte SH = 0x29; // Store Halfword
+    public static final byte SWL = 0x2A; // Store Word Left
+    public static final byte SW = 0x2B; // Store Word
+    public static final byte SWR = 0x2E; // Store Word Right
+    public static final byte CACHE = 0x2f; // Cache Operation
+    public static final byte LWC1 = 0x31;
+    public static final byte PREF = 0x33;
+    public static final byte SWC1 = 0x39;
     
-    
-    //Shift Instructions
+    	  /*
+     SPECIAL: Instr. encoded by function field when opcode field = SPECIAL  NOT COMPLETE!!! (31/07/2008) shadow
+    31---------26------------------------------------------5--------0
+    | = SPECIAL |                                         | function|
+    ------6----------------------------------------------------6-----
+    |--000--|--001--|--010--|--011--|--100--|--101--|--110--|--111--| lo
+000 | SLL   | ---   | SRL   | SRA   | SLLV  |  ---  | SRLV  | SRAV  |
+001 | JR    | JALR  | ---   | ---   |SYSCALL| BREAK |  ---  | SYNC  |
+010 | MFHI  | MTHI  | MFLO  | MTLO  | ---   |  ---  |  ---  | ---   |
+011 | MULT  | MULTU | DIV   | DIVU  | ----  |  ---  | ----  | ----- |
+100 | ADD   | ADDU  | SUB   | SUBU  | AND   | OR    | XOR   | NOR   |
+101 | ---   |  ---  | SLT   | SLTU  | ---   | ---   | ---   | ---   |
+110 | ---   |  ---  | ---   | ---   | ---   |  ---  | ---   | ---   |
+111 | ---   |  ---  | ---   | ---   | ---   |  ---  | ---   | ---   |
+ hi |-------|-------|-------|-------|-------|-------|-------|-------|
+*/
     public static final byte SLL = 0x0; // Shift Left Logical
-    public static final byte SRL = 0x0; // Shift Right Logical
-    public static final byte SRA = 0x0; // Shift Right Arithmetic
-    public static final byte SLLV = 0x0; // Shift Left Logical Variable
-    public static final byte SRLV = 0x0; // Shift Right Logical Variable
-    public static final byte SRAV = 0x0; // Shift Right Arithmetic Variable
+    public static final byte SRL = 0x2; // Shift Right Logical
+    public static final byte SRA = 0x3; // Shift Right Arithmetic
+    public static final byte SLLV = 0x4; // Shift Left Logical Variable
+    public static final byte SRLV = 0x6; // Shift Right Logical Variable
+    public static final byte SRAV = 0x7; // Shift Right Arithmetic Variable
+    public static final byte JR = 0x08; // Jump Register
+    public static final byte JALR = 0x09; // Jump And Link Register
+    public static final byte SYSCALL = 0xc; // System Call
+    public static final byte BREAK = 0xd; // Break
+    public static final byte SYNC = 0xf; // Sync
+    public static final byte MFHI = 0x10; // Move From HI
+    public static final byte MTHI = 0x11; // Move To HI
+    public static final byte MFLO = 0x12; // Move From LO
+    public static final byte MTLO = 0x13; // Move To LO
+    public static final byte MULT = 0x18; // Multiply
+    public static final byte MULTU = 0x19; // Multiply Unsigned
+    public static final byte DIV = 0x1a; // Divide
+    public static final byte DIVU = 0x1b; // Divide Unsigned
+    public static final byte ADD = 0x21; // Add
+    public static final byte ADDU = 0x22; // Add Unsigned
+    public static final byte SUB = 0x23; // Subtract
+    public static final byte SUBU = 0x24; // Subtract Unsigned
+    public static final byte AND = 0x25; // AND
+    public static final byte OR = 0x26; // OR
+    public static final byte XOR = 0x27; // Exclusive OR
+    public static final byte NOR = 0x28; // NOR   
+    public static final byte SLT = 0x2b; // Set on Less Than
+    public static final byte SLTU = 0x2c; // Set on Less Than Unsigned
+ /*
+    REGIMM: Instructions encoded by the rt field when opcode field = REGIMM.  NOT COMPLETE!!! (31/07/2008) shadow
+    31---------26----------20-------16------------------------------0
+    | = REGIMM  |          |   rt    |                              |
+    ------6---------------------5------------------------------------
+    |--000--|--001--|--010--|--011--|--100--|--101--|--110--|--111--| lo
+ 00 | BLTZ  | BGEZ  | BLTZL | BGEZL |  ---  |  ---  |  ---  |  ---  |
+ 01 | ----  | ----  | ----  | ----  |  ---- |  ---  | ---   |  ---  |
+ 10 | BLTZAL| BGEZAL|BLTZALL|BGEZALL|  ---  |  ---  |  ---  |  ---  |
+ 11 | ----  | ----  |  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |
+ hi |-------|-------|-------|-------|-------|-------|-------|-------|
+*/
+    
+    public static final byte BLTZ = 0x0; // Branch on Less Than Zero
+    public static final byte BGEZ = 0x1; // Branch on Greater Than or Equal to Zero
+    public static final byte BLTZL = 0x2; // Branch on Less Than Zero Likely
+    public static final byte BGEZL = 0x3; // Branch on Greater Than or Equal to Zero Likely
+    public static final byte BLTZAL = 0x10; // Branch on Less Than Zero And Link
+    public static final byte BGEZAL = 0x11; // Branch on Greater Than or Equal to Zero And Link    
+    public static final byte BLTZALL = 0x12; // Branch on Less Than Zero And Link Likely
+    public static final byte BGEZALL = 0x13; // Branch on Greater Than or Equal to Zero And Link Likely
     
     
     
+    //These are not sure they exist and will be add later when emu will become more advance. (shadow)
+   /* 
     //Coprocessor Instructions CO
     public static final byte LWCz = 0x0; // Load Word to Coprocessor z
     public static final byte SWCz = 0x0; // Store Word from Coprocessor z
@@ -99,16 +150,10 @@ public class R4000OpCodes {
     public static final byte MFCz = 0x0; // Move From Coprocessor z
     public static final byte CTCz = 0x0; // Move Control to Coprocessor z
     public static final byte CFCz = 0x0; // Move Control From Coprocessor z
-    public static final byte COPz = 0x0; // Coprocessor Operation z
+    
     public static final byte BCzT = 0x0; // Branch on Coprocessor z True
     public static final byte BCzF = 0x0; // Branch on Coprocessor z False
-    
-    
-    //Special Instructions
-    public static final byte SYSCALL = 0x0; // System Call
-    public static final byte BREAK = 0x0; // Break
-    
-    
+     
     //Extensions to the ISA: Load and Store Instructions
     public static final byte LD = 0x37; // Load Doubleword
     public static final byte LDL = 0x1a; // Load Doubleword Left
@@ -121,9 +166,7 @@ public class R4000OpCodes {
     public static final byte SD = 0x3f; // Store Doubleword
     public static final byte SDL = 0x2c; // Store Doubleword Left
     public static final byte SDR = 0x2d; // Store Doubleword Right
-    public static final byte SYNC = 0x0; // Sync
-    
-    
+   
     //Extensions to the ISA: Arithmetic Instructions (ALU Immediate)
     public static final byte DADDI = 0x18; // Doubleword Add Immediate
     public static final byte DADDIU = 0x19; // Doubleword Add Immediate Unsigned
@@ -136,18 +179,10 @@ public class R4000OpCodes {
     
     
     //Extensions to the ISA: Branch Instructions
-    public static final byte BEQL = 0x14; // Branch on Equal Likely
-    public static final byte BNEL = 0x15; // Branch on Not Equal Likely
-    public static final byte BLEZL = 0x16; // Branch on Less Than or Equal to Zero Likely
-    public static final byte BGTZL = 0x17; // Branch on Greater Than Zero Likely
-    public static final byte BLTZL = 0x1; // Branch on Less Than Zero Likely
-    public static final byte BGEZL = 0x1; // Branch on Greater Than or Equal to Zero Likely
-    public static final byte BLTZALL = 0x1; // Branch on Less Than Zero And Link Likely
-    public static final byte BGEZALL = 0x1; // Branch on Greater Than or Equal to Zero And Link Likely
+
     public static final byte BCzTL = 0x0; // Branch on Coprocessor z True Likely
     public static final byte BCzFL = 0x0; // Branch on Coprocessor z False Likely
-            
-            
+                     
     //Extensions to the ISA: Arithmetic Instructions (3-operand, R-type)
     public static final byte DADD = 0x0; // Doubleword Add
     public static final byte DADDU = 0x0; // Doubleword Add Unsigned
@@ -197,6 +232,6 @@ public class R4000OpCodes {
     public static final byte TLBWI = 0x10; // Write Indexed TLB Entry
     public static final byte TLBWR = 0x10; // Write Random TLB Entry
     public static final byte TLBP = 0x10; // Probe TLB for Matching Entry
-    public static final byte CACHE = 0x2f; // Cache Operation
-    public static final byte ERET = 0x10; // Exception Return            
+    
+    public static final byte ERET = 0x10; // Exception Return            */
 }
