05f84efd103d6bf10b2fd9cdfb202aed8dc4722c
leandro.rhcp99 <leandro.rhcp99@gmail.com>
2008-08-02 14:38:16 +0000

    rename package, disassembler already had a classe inside with the same name and it gives build error.

--- src/jpcsp/Debugger/DisassemblerModule/DisVFPU.java
+++ src/jpcsp/Debugger/DisassemblerModule/DisVFPU.java
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package jpcsp.Debugger.Disassembler;
+package jpcsp.Debugger.DisassemblerModule;
 
 /**
  *
