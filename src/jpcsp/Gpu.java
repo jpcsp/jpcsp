10c3327e68253264bbe3135b4fc287b85d61b9c8
leandro.rhcp99 <leandro.rhcp99@gmail.com>
2008-08-05 02:16:19 +0000

    basic (or structure) code to core runs

---
+++ src/jpcsp/Gpu.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jpcsp;
+
+public class Gpu {
+
+        public void draw(){
+            
+        }
+}
