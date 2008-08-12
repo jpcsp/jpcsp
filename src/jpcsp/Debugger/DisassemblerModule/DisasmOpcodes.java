5d7ca998efcf886d3a4e9a41f2cfcc6f2cd5ab29
george@moralis.gr <georgemoralis@users.noreply.github.com>
2008-08-12 12:55:03 +0000

    Fixed a bug in Decoder.java (SLTI was calling SLT ) Added in Processor.java the following instructions LWL,LWR,SWL,SWR thanks to molecule Processor.java :Debug messages for unimplement opcodes. Rewrote the disasm using the Decoder generic class. Now disasm and interpreter are sync Rewrote Helper class for disasm Removed old useless classes of disasm TODO:redone the FPU + other missing disasm opcodes

---
+++ src/jpcsp/Debugger/DisassemblerModule/DisasmOpcodes.java
@@ -0,0 +1,231 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jpcsp.Debugger.DisassemblerModule;
+import static jpcsp.Debugger.DisassemblerModule.DisHelper.*;
+import jpcsp.AllegrexInstructions;
+import jpcsp.Decoder;
+
+/**
+ *
+ * @author shadow
+ */
+public class DisasmOpcodes implements AllegrexInstructions {
+   private final Decoder disasm = new Decoder();
+    int opcode_address;
+    String returnString = "Unsupported Instruction"; // set default to unsupported
+    public String disasm(int value,int opcode_address)
+    {
+      this.opcode_address = opcode_address;
+      returnString = "Unsupported Instruction";
+      disasm.process(this, value);
+      return returnString;
+    }
+    @Override
+    public void doUNK(String reason){ returnString = reason; }
+    @Override
+    public void doNOP(){ returnString = "nop"; }
+    @Override
+    public void doSLL(int rd, int rt, int sa){ returnString = Dis_RDRTSA("sll",rd,rt,sa); }
+    @Override
+    public void doSRL(int rd, int rt, int sa){ returnString = Dis_RDRTSA("srl",rd,rt,sa); }
+    @Override
+    public void doSRA(int rd, int rt, int sa){ returnString =Dis_RDRTSA("sra",rd,rt,sa);  }
+    @Override
+    public void doSLLV(int rd, int rt, int rs){returnString =Dis_RDRTRS("sllv",rd,rt,rs); }
+    @Override
+    public void doSRLV(int rd, int rt, int rs){returnString =Dis_RDRTRS("srlv",rd,rt,rs);  }
+    @Override
+    public void doSRAV(int rd, int rt, int rs){ returnString =Dis_RDRTRS("srav",rd,rt,rs); }
+    @Override
+    public void doJR(int rs){ returnString =Dis_RS("jr",rs); }
+    @Override
+    public void doJALR(int rd, int rs){ returnString = Dis_RDRS("jalr",rd,rs); } 
+    @Override
+    public void doMFHI(int rd){returnString = Dis_RD("mfhi",rd); }
+    @Override
+    public void doMTHI(int rs){returnString = Dis_RS("mthi",rs); }
+    @Override
+    public void doMFLO(int rd){returnString = Dis_RD("mflo",rd); }   
+    @Override
+    public void doMTLO(int rs){returnString = Dis_RS("mtlo",rs); }
+    @Override
+    public void doMULT(int rs, int rt){ returnString =Dis_RSRT("mult",rs,rt);  }
+    @Override
+    public void doMULTU(int rs, int rt){ returnString =Dis_RSRT("multu",rs,rt); }
+    @Override
+    public void doDIV(int rs, int rt){ returnString =Dis_RSRT("div",rs,rt); }  
+    @Override
+    public void doDIVU(int rs, int rt){returnString = Dis_RSRT("divu",rs,rt);  }  
+    @Override
+    public void doADD(int rd, int rs, int rt){ returnString =Dis_RDRSRT("add",rd,rs,rt); }
+    @Override
+    public void doADDU(int rd, int rs, int rt){ returnString =Dis_RDRSRT("addu",rd,rs,rt); }  
+    @Override
+    public void doSUB(int rd, int rs, int rt){ returnString =Dis_RDRSRT("sub",rd,rs,rt); }
+    @Override
+    public void doSUBU(int rd, int rs, int rt){ returnString =Dis_RDRSRT("subu",rd,rs,rt); }
+    @Override
+    public void doAND(int rd, int rs, int rt){ returnString =Dis_RDRSRT("and",rd,rs,rt); }
+    @Override
+    public void doOR(int rd, int rs, int rt){ returnString =Dis_RDRSRT("or",rd,rs,rt); }
+    @Override
+    public void doXOR(int rd, int rs, int rt){ returnString =Dis_RDRSRT("xor",rd,rs,rt); }
+    @Override
+    public void doNOR(int rd, int rs, int rt){ returnString =Dis_RDRSRT("nor",rd,rs,rt); }
+    @Override
+    public void doSLT(int rd, int rs, int rt){ returnString =Dis_RDRSRT("slt",rd,rs,rt); }
+    @Override
+    public void doSLTU(int rd, int rs, int rt){returnString =Dis_RDRSRT("sltu",rd,rs,rt); }
+    @Override
+    public void doBLTZ(int rs, int simm16){ returnString = Dis_RSOFFSET("bltz",rs,simm16,opcode_address); }  
+    @Override
+    public void doBGEZ(int rs, int simm16){ returnString = Dis_RSOFFSET("bgez",rs,simm16,opcode_address); }  
+    @Override
+    public void doBLTZL(int rs, int simm16){ returnString = Dis_RSOFFSET("bltzl",rs,simm16,opcode_address); } 
+    @Override
+    public void doBGEZL(int rs, int simm16){ returnString = Dis_RSOFFSET("bgezl",rs,simm16,opcode_address); }  
+    @Override
+    public void doBLTZAL(int rs, int simm16){ returnString = Dis_RSOFFSET("bltzal",rs,simm16,opcode_address); } 
+    @Override
+    public void doBGEZAL(int rs, int simm16){ returnString = Dis_RSOFFSET("bgezal",rs,simm16,opcode_address); }   
+    @Override
+    public void doBLTZALL(int rs, int simm16){ returnString = Dis_RSOFFSET("bltzall",rs,simm16,opcode_address); } 
+    @Override
+    public void doBGEZALL(int rs, int simm16){ returnString = Dis_RSOFFSET("bgezall",rs,simm16,opcode_address); }  
+    @Override
+    public void doJ(int uimm26){ returnString = Dis_JUMP("j",uimm26,opcode_address); }  
+    @Override
+    public void doJAL(int uimm26){ returnString = Dis_JUMP("jal",uimm26,opcode_address); }
+    @Override
+    public void doBEQ(int rs, int rt, int simm16){ returnString = Dis_RSRTOFFSET("beq",rs,rt,simm16,opcode_address); } 
+    @Override
+    public void doBNE(int rs, int rt, int simm16){ returnString = Dis_RSRTOFFSET("bne",rs,rt,simm16,opcode_address);  }    
+    @Override
+    public void doBLEZ(int rs, int simm16){ returnString = Dis_RSOFFSET("blez",rs,simm16,opcode_address); }    
+    @Override
+    public void doBGTZ(int rs, int simm16){ returnString = Dis_RSOFFSET("bgtz",rs,simm16,opcode_address); }    
+    @Override
+    public void doBEQL(int rs, int rt, int simm16){ returnString = Dis_RSRTOFFSET("beql",rs,rt,simm16,opcode_address);  }   
+    @Override
+    public void doBNEL(int rs, int rt, int simm16){ returnString = Dis_RSRTOFFSET("bnel",rs,rt,simm16,opcode_address);  }   
+    @Override
+    public void doBLEZL(int rs, int simm16){ returnString = Dis_RSOFFSET("blezl",rs,simm16,opcode_address); }
+    @Override
+    public void doBGTZL(int rs, int simm16){ returnString = Dis_RSOFFSET("bgtzl",rs,simm16,opcode_address); }
+    @Override
+    public void doADDI(int rt, int rs, int simm16){ returnString = Dis_RTRSIMM("addi",rt,rs,simm16); }
+    @Override
+    public void doADDIU(int rt, int rs, int simm16){ returnString = Dis_RTRSIMM("addiu",rt,rs,simm16); }
+    @Override
+    public void doSLTI(int rt, int rs, int simm16){ returnString = Dis_RTRSIMM("slti",rt,rs,simm16); } 
+    @Override
+    public void doSLTIU(int rt, int rs, int simm16){ returnString = Dis_RTRSIMM("sltiu",rt,rs,simm16); }  
+    @Override
+    public void doANDI(int rt, int rs, int uimm16){ returnString = Dis_RTRSIMM("andi",rt,rs,uimm16); }  
+    @Override
+    public void doORI(int rt, int rs, int uimm16){ returnString = Dis_RTRSIMM("ori",rt,rs,uimm16); }    
+    @Override
+    public void doXORI(int rt, int rs, int uimm16){ returnString = Dis_RTRSIMM("xori",rt,rs,uimm16); }  
+    @Override
+    public void doLUI(int rt, int uimm16){ returnString = Dis_RTIMM("lui",rt,uimm16);  }
+    @Override
+    public void doHALT(){ returnString =  "halt"; }    
+    @Override
+    public void doMFIC(int rt){ returnString = Dis_RT("mfic",rt);}
+    @Override
+    public void doMTIC(int rt){ returnString = Dis_RT("mtic",rt); }
+    @Override
+    public void doMFC0(int rt, int c0dr){  }  //TODO
+    @Override
+    public void doCFC0(int rt, int c0cr){  }  //TODO
+    @Override
+    public void doMTC0(int rt, int c0dr){  }   //TODO
+    @Override
+    public void doCTC0(int rt, int c0cr){  } //TODO
+    @Override
+    public void doERET(){  } //TODO
+    @Override
+    public void doLB(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("lb",rt,rs,simm16); }
+    @Override
+    public void doLBU(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("lbu",rt,rs,simm16);}
+    @Override
+    public void doLH(int rt, int rs, int simm16){returnString = Dis_RTIMMRS("lh",rt,rs,simm16);  }
+    @Override
+    public void doLHU(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("lhu",rt,rs,simm16); }
+    @Override
+    public void doLWL(int rt, int rs, int simm16){returnString = Dis_RTIMMRS("lwl",rt,rs,simm16);  }
+    @Override
+    public void doLW(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("lw",rt,rs,simm16); }
+    @Override
+    public void doLWR(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("lwr",rt,rs,simm16); }
+    @Override
+    public void doSB(int rt, int rs, int simm16){returnString = Dis_RTIMMRS("sb",rt,rs,simm16);  }
+    @Override
+    public void doSH(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("sh",rt,rs,simm16); }
+    @Override
+    public void doSWL(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("swl",rt,rs,simm16); }
+    @Override
+    public void doSW(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("sw",rt,rs,simm16); }
+    @Override
+    public void doSWR(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("swr",rt,rs,simm16); }
+    @Override
+    public void doCACHE(int rt, int rs, int simm16){ returnString = "cache"; } //TODO!
+    @Override
+    public void doLL(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("ll",rt,rs,simm16); }
+    @Override
+    public void doSC(int rt, int rs, int simm16){ returnString = Dis_RTIMMRS("sc",rt,rs,simm16); }
+    @Override
+    public void doROTR(int rd, int rt, int sa){ returnString = Dis_RDRTSA("rotr",rd,rt,sa);  } 
+    @Override
+    public void doROTRV(int rd, int rt, int rs){ returnString = Dis_RDRTRS("rotrv",rd,rt,rs); } 
+    @Override
+    public void doMOVZ(int rd, int rs, int rt){ returnString = Dis_RDRSRT("movz",rd,rs,rt); } 
+    @Override
+    public void doMOVN(int rd, int rs, int rt){ returnString = Dis_RDRSRT("movn",rd,rs,rt); } 
+    @Override
+    public void doSYSCALL(int code){ returnString = Dis_Syscall(code); }    
+    @Override
+    public void doBREAK(int code){ returnString = Dis_Break(code); }  
+    @Override
+    public void doSYNC(){ returnString = "sync"; }
+    @Override
+    public void doCLZ(int rd, int rs){ returnString = Dis_RDRS("clz",rd,rs); }   
+    @Override
+    public void doCLO(int rd, int rs){ returnString = Dis_RDRS("clo",rd,rs); }   
+    @Override
+    public void doMADD(int rs, int rt){ returnString = Dis_RSRT("madd",rs,rt); }  
+    @Override
+    public void doMADDU(int rs, int rt){ returnString = Dis_RSRT("maddu",rs,rt); } 
+    @Override
+    public void doMAX(int rd, int rs, int rt){ returnString = Dis_RDRSRT("max",rd,rs,rt); }
+    @Override
+    public void doMIN(int rd, int rs, int rt){ returnString = Dis_RDRSRT("min",rd,rs,rt); }  
+    @Override
+    public void doMSUB(int rs, int rt){ returnString = Dis_RSRT("msub",rs,rt); }  
+    @Override
+    public void doMSUBU(int rs, int rt){ returnString = Dis_RSRT("msubu",rs,rt); }  
+    @Override
+    public void doEXT(int rt, int rs, int rd, int sa)
+    {  
+     returnString = "ext " + gprNames[rt] + ", " + gprNames[rs] + ", " + sa + ", " + (rd + 1);
+    }
+    @Override
+    public void doINS(int rt, int rs, int rd, int sa)
+    {  
+      returnString = "ins " + gprNames[rt] + ", " + gprNames[rs] + ", " + sa + ", " + (rd - sa + 1);           
+    }
+    @Override
+    public void doWSBH(int rd, int rt){ returnString = Dis_RDRT("wsbh",rd,rt); } 
+    @Override
+    public void doWSBW(int rd, int rt){ returnString = Dis_RDRT("wsbw",rd,rt); }
+    @Override
+    public void doSEB(int rd, int rt){ returnString = Dis_RDRT("seb",rd,rt); }   
+    @Override
+    public void doBITREV(int rd, int rt){ returnString = Dis_RDRT("bitrev",rd,rt); }  
+    @Override
+    public void doSEH(int rd, int rt){ returnString = Dis_RDRT("seh",rd,rt); }
+   
+}
