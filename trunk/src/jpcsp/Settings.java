ede1785fa3b1c5ede21119486aed18303b5e3231
george@moralis.gr <george@moralis.gr@4ab978e6-3352-0410-996c-0b42db0051c8>
2008-08-28 07:59:16 +0000

    Added -Xms128m -Xmx128m  to netbeans project file. That will increase java heap space Added new setttings file by spip2001 and removed jdom library

--- trunk/src/jpcsp/Settings.java
+++ trunk/src/jpcsp/Settings.java
@@ -13,28 +13,47 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 package jpcsp;
 
 import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import jpcsp.Controller.keyCode;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
-import org.jdom.output.XMLOutputter;
 
 /**
- *
- * @author shadow
- */
+*
+* @author spip2001
+*/
 public class Settings {
+   
+   private final static String SETTINGS_FILE_NAME = "Settings.xml";
 
     private static Settings instance = null;
 
@@ -48,33 +67,90 @@ public class Settings {
     public void NullSettings() {
         instance = null;
     }
+   
+    /**
+     * Reads a setting from the xml file and
+     * returns it as a string.
+     * @param path Path of the wanted value
+     * @return
+     */
+    private String readXmlSetting(String path) {
+       try {
+          XPathFactory xpathFactory = XPathFactory.newInstance();
+          XPath xpath = xpathFactory.newXPath();
+          XPathExpression expr = xpath.compile(path);
+          FileInputStream settingsIn = new FileInputStream(SETTINGS_FILE_NAME);
+          InputSource source = new InputSource(settingsIn);
+          String value = expr.evaluate(source);
+          settingsIn.close();
+          
+          // System.out.println(path + " = " + value);
+          
+          return value;
+       } catch (Exception e) {
+          e.printStackTrace();
+          return null;
+       }
+    }
+   
+    /**
+     * Gets the xml document containing settings
+     * @return
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     */
+    private Document getSettingsDocument() throws ParserConfigurationException, SAXException, IOException {
+       DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+       DocumentBuilder builder= dbFactory.newDocumentBuilder();
+       
+        Document doc = builder.parse(new File(SETTINGS_FILE_NAME));
+       
+        return doc;
+    }
+   
+    /**
+     * Write settings in file
+     * @param doc Settings as XML document
+     */
+    private void writeSettings(Document doc) {
+       try {
+            FileOutputStream fileOutputStream = new FileOutputStream(SETTINGS_FILE_NAME);
+            TransformerFactory tFactory = TransformerFactory.newInstance();
+            Transformer transformer = tFactory.newTransformer();
+            transformer.transform(new DOMSource(doc), new StreamResult(fileOutputStream));
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+         e.printStackTrace();
+      } catch (TransformerException e) {
+         e.printStackTrace();
+      }
+    }
 
     public int[] readWindowPos(String windowname) {
         int[] coord = new int[2];
         try {
-            // Build the document with SAX and Xerces, no validation
-            SAXBuilder builder = new SAXBuilder();
-            // Create the document
-            Document doc = builder.build(new File("Settings.xml"));
-            Element webapp = doc.getRootElement();
-            coord[0] = Integer.parseInt(webapp.getChild("guisettings").getChild("windowspos").getChild(windowname).getChild("x").getText());
-            coord[1] = Integer.parseInt(webapp.getChild("guisettings").getChild("windowspos").getChild(windowname).getChild("y").getText());
+           String x = readXmlSetting("//guisettings/windowspos/" + windowname + "/x");
+           String y = readXmlSetting("//guisettings/windowspos/" + windowname + "/y");
+            coord[0] = x != null ? Integer.parseInt(x) : 0 ;
+            coord[1] = y != null ? Integer.parseInt(y) : 0 ;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return coord;
     }
-	
-	public int[] readWindowSize(String windowname) {
+   
+   public int[] readWindowSize(String windowname) {
         int[] dimension = new int[2];
         try {
-            // Build the document with SAX and Xerces, no validation
-            SAXBuilder builder = new SAXBuilder();
-            // Create the document
-            Document doc = builder.build(new File("Settings.xml"));
-            Element webapp = doc.getRootElement();
-            dimension[0] = Integer.parseInt(webapp.getChild("guisettings").getChild("windowsize").getChild(windowname).getChild("x").getText());
-            dimension[1] = Integer.parseInt(webapp.getChild("guisettings").getChild("windowsize").getChild(windowname).getChild("y").getText());
+           String x = readXmlSetting("//guisettings/windowsize/" + windowname + "/x");
+           String y = readXmlSetting("//guisettings/windowsize/" + windowname + "/y");
+           dimension[0] = x != null ? Integer.parseInt(x) : 0 ;
+           dimension[1] = y != null ? Integer.parseInt(y) : 0 ;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -83,96 +159,80 @@ public class Settings {
 
     public void writeWindowPos(String windowname, String[] pos) {
         try {
-
-            SAXBuilder builder = new SAXBuilder();
-            Document doc = builder.build(new File("Settings.xml"));
-            Element webapp = doc.getRootElement();
-            webapp.getChild("guisettings").getChild("windowspos").getChild(windowname).getChild("x").setText(pos[0]);
-            webapp.getChild("guisettings").getChild("windowspos").getChild(windowname).getChild("y").setText(pos[1]);
-            XMLOutputter xmloutputter = new XMLOutputter();
-            try {
-                FileOutputStream fileOutputStream = new FileOutputStream("Settings.xml");
-                xmloutputter.output(doc, fileOutputStream);
-                fileOutputStream.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+           Document doc = getSettingsDocument();
+           
+            XPathFactory xpathFactory = XPathFactory.newInstance();
+          XPath xpath = xpathFactory.newXPath();
+          
+            Element posX = (Element) xpath.evaluate("//guisettings/windowspos/" + windowname + "/x", doc, XPathConstants.NODE);
+            Element posY = (Element) xpath.evaluate("//guisettings/windowspos/" + windowname + "/y", doc, XPathConstants.NODE);
+           
+            posX.replaceChild(doc.createTextNode(pos[0]), posX.getFirstChild());
+            posY.replaceChild(doc.createTextNode(pos[1]), posY.getFirstChild());
+           
+            writeSettings(doc);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-	
-	public void writeWindowSize(String windowname, String[] dimension) {
-        try {
-
-            SAXBuilder builder = new SAXBuilder();
-            Document doc = builder.build(new File("Settings.xml"));
-            Element webapp = doc.getRootElement();
-            webapp.getChild("guisettings").getChild("windowsize").getChild(windowname).getChild("x").setText(dimension[0]);
-            webapp.getChild("guisettings").getChild("windowsize").getChild(windowname).getChild("y").setText(dimension[1]);
-            XMLOutputter xmloutputter = new XMLOutputter();
-            try {
-                FileOutputStream fileOutputStream = new FileOutputStream("Settings.xml");
-                xmloutputter.output(doc, fileOutputStream);
-                fileOutputStream.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+   
+   public void writeWindowSize(String windowname, String[] dimension) {
+      try {
+         Document doc = getSettingsDocument();
+           
+            XPathFactory xpathFactory = XPathFactory.newInstance();
+          XPath xpath = xpathFactory.newXPath();
+          
+            Element dimX = (Element) xpath.evaluate("//guisettings/windowsize/" + windowname + "/x", doc, XPathConstants.NODE);
+            Element dimY = (Element) xpath.evaluate("//guisettings/windowsize/" + windowname + "/y", doc, XPathConstants.NODE);
+           
+            dimX.replaceChild(doc.createTextNode(dimension[0]), dimX.getFirstChild());
+            dimY.replaceChild(doc.createTextNode(dimension[1]), dimY.getFirstChild());
+           
+            writeSettings(doc);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-	
+   
     public boolean readBoolEmuoptions(String option)
     {
         int value=0;
         try {
-            // Build the document with SAX and Xerces, no validation
-            SAXBuilder builder = new SAXBuilder();
-            // Create the document
-            Document doc = builder.build(new File("Settings.xml"));
-            Element webapp = doc.getRootElement();
-            value =Integer.parseInt(webapp.getChild("emuoptions").getChild(option).getText());
-            
+            String v = readXmlSetting("//emuoptions/" + option);
+            value = v != null ? Integer.parseInt(v) : 0;
+           
         } catch (Exception e) {
             e.printStackTrace();
         }
         if(value==1) return true;
         else return false;
     }
+   
     public void writeBoolEmuoptions(String option,boolean value)
     {
-            String state = "0";
-            if(value) state = "1";
+          String state = value ? "1" : "0";
+           
             try {
-
-            SAXBuilder builder = new SAXBuilder();
-            Document doc = builder.build(new File("Settings.xml"));
-            Element webapp = doc.getRootElement();
-            webapp.getChild("emuoptions").getChild(option).setText(state);  
-            XMLOutputter xmloutputter = new XMLOutputter();
-            try {
-                FileOutputStream fileOutputStream = new FileOutputStream("settings.xml");
-                xmloutputter.output(doc, fileOutputStream);
-                fileOutputStream.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+               Document doc = getSettingsDocument();
+               
+                XPathFactory xpathFactory = XPathFactory.newInstance();
+              XPath xpath = xpathFactory.newXPath();
+              
+                Element emuOption = (Element) xpath.evaluate("//emuoptions/" + option, doc, XPathConstants.NODE);
+               
+                emuOption.replaceChild(doc.createTextNode(state), emuOption.getFirstChild());
+               
+                writeSettings(doc);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+       
     }
-    
+   
     public HashMap<Integer, keyCode> loadKeys() {
         HashMap<Integer, keyCode> m = new HashMap<Integer, keyCode>(22);
-        
+       
         m.put(readKeyOption("up"), keyCode.UP);
         m.put(readKeyOption("down"), keyCode.DOWN);
         m.put(readKeyOption("left"), keyCode.LEFT);
@@ -195,17 +255,18 @@ public class Settings {
         m.put(readKeyOption("volMin"), keyCode.VOLMIN);
         m.put(readKeyOption("screen"), keyCode.SCREEN);
         m.put(readKeyOption("music"), keyCode.MUSIC);
-        
+       
         return m;
     }
-    
-    public void writeKeys(HashMap<Integer, keyCode> keys) {
+   
+    @SuppressWarnings("unchecked")
+   public void writeKeys(HashMap<Integer, keyCode> keys) {
         Iterator iter = keys.entrySet().iterator();
         while(iter.hasNext()) {
             Map.Entry<Integer, keyCode> entry = (Map.Entry)iter.next();
             keyCode key = (keyCode)entry.getValue();
             int value = (Integer)entry.getKey();
-            
+           
             switch (key) {
                 case DOWN:      writeKeyOption("down", value); break;
                 case UP:        writeKeyOption("up", value); break;
@@ -215,7 +276,7 @@ public class Settings {
                 case ANUP:      writeKeyOption("analogUp", value); break;
                 case ANLEFT:    writeKeyOption("analogLeft", value); break;
                 case ANRIGHT:   writeKeyOption("analogRight", value); break;
-            
+           
                 case TRIANGLE:  writeKeyOption("triangle", value); break;
                 case SQUARE:    writeKeyOption("square", value); break;
                 case CIRCLE:    writeKeyOption("circle", value); break;
@@ -224,56 +285,47 @@ public class Settings {
                 case R1:        writeKeyOption("rTrigger", value); break;
                 case START:     writeKeyOption("start", value); break;
                 case SELECT:    writeKeyOption("select", value); break;
-                
+               
                 case HOME:      writeKeyOption("home", value); break;
                 case HOLD:      writeKeyOption("hold", value); break;
                 case VOLMIN:    writeKeyOption("volMin", value); break;
                 case VOLPLUS:   writeKeyOption("volPlus", value); break;
                 case SCREEN:    writeKeyOption("screen", value); break;
                 case MUSIC:     writeKeyOption("music", value); break;
-                        
+                       
                 default: break;
             }
         }
     }
-    
+   
     private int readKeyOption(String keyName) {
         int r = KeyEvent.VK_UNDEFINED;
-        
+       
         try {
-            // Build the document with SAX and Xerces, no validation
-            SAXBuilder builder = new SAXBuilder();
-            // Create the document
-            Document doc = builder.build(new File("Settings.xml"));
-            Element webapp = doc.getRootElement();
-            r = Integer.parseInt(webapp.getChild("emuoptions").getChild("keys").getChild(keyName).getText());
-            
+            String rS = readXmlSetting("//emuoptions/keys/" + keyName);
+            r = rS != null ? Integer.parseInt(rS) : null;
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+       
         return r;
     }
-    
+   
     public void writeKeyOption(String keyName, int key) {
         try {
-            SAXBuilder builder = new SAXBuilder();
-            Document doc = builder.build(new File("Settings.xml"));
-            Element webapp = doc.getRootElement();
-            webapp.getChild("emuoptions").getChild("keys").getChild(keyName).setText(Integer.toString(key));
-            XMLOutputter xmloutputter = new XMLOutputter();
-            try {
-                FileOutputStream fileOutputStream = new FileOutputStream("Settings.xml");
-                xmloutputter.output(doc, fileOutputStream);
-                fileOutputStream.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+           Document doc = getSettingsDocument();
+           
+            XPathFactory xpathFactory = XPathFactory.newInstance();
+          XPath xpath = xpathFactory.newXPath();
+          
+            Element keyNameEl = (Element) xpath.evaluate("//emuoptions/keys/" + keyName, doc, XPathConstants.NODE);
+           
+            keyNameEl.replaceChild(doc.createTextNode(String.valueOf(key)), keyNameEl.getFirstChild());
+           
+            writeSettings(doc);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+   
 }
