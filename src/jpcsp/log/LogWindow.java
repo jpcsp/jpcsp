45bcbce83dd79017dcb7a9ca369bb7b4bae5e77f
Florent Castelli <orphis@users.noreply.github.com>
2008-09-24 17:30:45 +0000

    Introducing advanced logging based on log4j. To edit the logging settings, you can update the LogSettings.xml file. Currently, only the Eclipse project file includes the log4j library, NetBeans will follow soon.

---
+++ src/jpcsp/log/LogWindow.java
@@ -0,0 +1,148 @@
+package jpcsp.log;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+
+import jpcsp.Settings;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class LogWindow extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9105338140096798954L;
+	private JTextPane textPane;
+
+	public LogWindow() {
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setTitle("Logger");
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				if (Settings.get_instance().readBoolOptions("guisettings/saveWindowPos"))
+			        Settings.get_instance().writeWindowPos("logwindow", getLocation());
+			}});
+				
+		textPane = new JTextPane();
+		JScrollPane scrollPane = new JScrollPane(textPane);
+
+		TextPaneAppender textPaneAppender = (TextPaneAppender)Logger.getRootLogger().getAppender("JpcspAppender");
+		textPaneAppender.setTextPane(textPane);
+		
+		JButton clearButton = new JButton("Clear");
+		clearButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clearScreenMessages();
+			}});
+		JButton saveButton = new JButton("Save to file...");
+		saveButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser m_fileChooser = new JFileChooser();
+				m_fileChooser.setSelectedFile(new File("logoutput.txt"));
+				m_fileChooser.setDialogTitle("Save logging output");
+				m_fileChooser.setCurrentDirectory(new java.io.File("."));
+				int returnVal = m_fileChooser.showSaveDialog(LogWindow.this);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File f = m_fileChooser.getSelectedFile();
+
+					try {
+						if (f.exists()) {
+							int res = JOptionPane.showConfirmDialog(
+											LogWindow.this,
+											"File already Exists! Do you want to override?",
+											"Already Exists Message",
+											JOptionPane.YES_NO_OPTION,
+											JOptionPane.QUESTION_MESSAGE);
+
+							if (res != 0)
+								return;
+						}
+
+						BufferedWriter out = null;
+						try {
+							out = new BufferedWriter(new FileWriter(f));
+							out.write(textPane.getText());
+						} catch (IOException ex) {
+							throw ex;
+						} finally {
+							if (out != null) {
+								try {
+									out.close();
+								} catch (IOException ex) {
+									ex.printStackTrace();
+								}
+							}
+						}
+					} catch (Exception ex) {
+						ex.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		GroupLayout layout = new GroupLayout(getRootPane());
+		layout.setAutoCreateGaps(true);
+		//layout.setAutoCreateContainerGaps(true);
+		
+		layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+				.addComponent(scrollPane)
+				.addGroup(layout.createSequentialGroup()
+						.addComponent(saveButton)
+						.addComponent(clearButton)));
+
+		layout.setVerticalGroup(layout.createSequentialGroup()
+				.addComponent(scrollPane)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(saveButton)
+						.addComponent(clearButton)));
+
+		setSize(500, 300);
+		getRootPane().setLayout(layout);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.setProperty("log4j.properties", "LogSettings.xml");
+		DOMConfigurator.configure("LogSettings.xml");
+		
+		System.setOut(new PrintStream(new LoggingOutputStream(Logger.getLogger("sysout"), Level.INFO)));
+		new LogWindow().setVisible(true); 
+	}
+
+	public void clearScreenMessages() {
+		synchronized (textPane) {
+			textPane.setText("");
+		}
+	}
+
+}
---
+++ src/jpcsp/log/TextPaneAppender.java
@@ -0,0 +1,318 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jpcsp.log;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.StringTokenizer;
+
+import javax.swing.ImageIcon;
+import javax.swing.JTextPane;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Level;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * <b>Experimental</b> TextPaneAppender. <br>
+ * 
+ * 
+ * Created: Sat Feb 26 18:50:27 2000 <br>
+ * 
+ * @author Sven Reimers
+ */
+
+public class TextPaneAppender extends AppenderSkeleton {
+
+	JTextPane textpane;
+	StyledDocument doc;
+	StringWriter sw;
+	Hashtable<Level, MutableAttributeSet> attributes;
+	Hashtable<Level, ImageIcon> icons;
+
+	private String label;
+
+	private boolean fancy;
+
+	final String LABEL_OPTION = "Label";
+	final String COLOR_OPTION_FATAL = "Color.Emerg";
+	final String COLOR_OPTION_ERROR = "Color.Error";
+	final String COLOR_OPTION_WARN = "Color.Warn";
+	final String COLOR_OPTION_INFO = "Color.Info";
+	final String COLOR_OPTION_DEBUG = "Color.Debug";
+	final String COLOR_OPTION_BACKGROUND = "Color.Background";
+	final String FANCY_OPTION = "Fancy";
+	final String FONT_NAME_OPTION = "Font.Name";
+	final String FONT_SIZE_OPTION = "Font.Size";
+	static final Level levels[] = { Level.FATAL, Level.ERROR, Level.WARN, Level.INFO, Level.DEBUG, Level.TRACE };
+
+	public static Image loadIcon(String path) {
+		Image img = null;
+		try {
+			URL url = ClassLoader.getSystemResource(path);
+			img = (Image) (Toolkit.getDefaultToolkit()).getImage(url);
+		} catch (Exception e) {
+			System.out.println("Exception occured: " + e.getMessage() + " - "
+					+ e);
+		}
+		return (img);
+	}
+
+	public TextPaneAppender(Layout layout, String name) {
+		this();
+		this.layout = layout;
+		this.name = name;
+		setTextPane(new JTextPane());
+		createAttributes();
+		createIcons();
+	}
+
+	public TextPaneAppender() {
+		super();
+		setTextPane(new JTextPane());
+		createAttributes();
+		createIcons();
+		this.label = "";
+		this.sw = new StringWriter();
+		this.fancy = false;
+	}
+
+	public void close() {
+
+	}
+
+	private void createAttributes() {
+		attributes = new Hashtable<Level, MutableAttributeSet>();
+		for (int i = 0; i < levels.length; i++) {
+			MutableAttributeSet att = new SimpleAttributeSet();
+			attributes.put(levels[i], att);
+			StyleConstants.setFontSize(att, 12);
+		}
+		StyleConstants.setForeground(attributes.get(Level.ERROR), Color.red);
+		StyleConstants.setForeground(attributes.get(Level.WARN), Color.orange);
+		StyleConstants.setForeground(attributes.get(Level.INFO), Color.gray);
+		StyleConstants.setForeground(attributes.get(Level.DEBUG), Color.black);
+	}
+
+	private void createIcons() {
+		icons = new Hashtable<Level, ImageIcon>();
+		/*for (int i = 0; i < levels.length; i++) {
+			if (levels[i].equals(Level.FATAL))
+				icons
+						.put(levels[i], new ImageIcon(
+								loadIcon("icons/RedFlag.gif")));
+			if (levels[i].equals(Level.ERROR))
+				icons
+						.put(levels[i], new ImageIcon(
+								loadIcon("icons/RedFlag.gif")));
+			if (levels[i].equals(Level.WARN))
+				icons.put(levels[i],
+						new ImageIcon(loadIcon("icons/BlueFlag.gif")));
+			if (levels[i].equals(Level.INFO))
+				icons.put(levels[i], new ImageIcon(
+						loadIcon("icons/GreenFlag.gif")));
+			if (levels[i].equals(Level.DEBUG))
+				icons.put(levels[i], new ImageIcon(
+						loadIcon("icons/GreenFlag.gif")));
+		}*/
+	}
+
+	public void append(LoggingEvent event) {
+		String text = this.layout.format(event);
+		String trace = "";
+		if (event.getThrowableInformation() != null) {
+			String[] ts = event.getThrowableStrRep();
+			for(String s : ts)
+				sw.write(s);
+			for (int i = 0; i < sw.getBuffer().length(); i++) {
+				if (sw.getBuffer().charAt(i) == '\t')
+					sw.getBuffer().replace(i, i + 1, "        ");
+			}
+			trace = sw.toString();
+			sw.getBuffer().delete(0, sw.getBuffer().length());
+		}
+		try {
+			synchronized(textpane) {
+				if (fancy) {
+					textpane.setEditable(true);
+					textpane.insertIcon(icons.get(event.getLevel()));
+					textpane.setEditable(false);
+				}
+				doc.insertString(doc.getLength(), text + trace, attributes.get(event.getLevel()));
+			}
+		} catch (BadLocationException badex) {
+			System.err.println(badex);
+		}
+		textpane.setCaretPosition(doc.getLength());
+	}
+
+	public JTextPane getTextPane() {
+		return textpane;
+	}
+
+	private static Color parseColor(String v) {
+		StringTokenizer st = new StringTokenizer(v, ",");
+		int val[] = { 255, 255, 255, 255 };
+		int i = 0;
+		while (st.hasMoreTokens()) {
+			val[i] = Integer.parseInt(st.nextToken());
+			i++;
+		}
+		return new Color(val[0], val[1], val[2], val[3]);
+	}
+
+	private static String colorToString(Color c) {
+		// alpha component emitted only if not default (255)
+		String res = "" + c.getRed() + "," + c.getGreen() + "," + c.getBlue();
+		return c.getAlpha() >= 255 ? res : res + "," + c.getAlpha();
+	}
+
+	public void setLayout(Layout layout) {
+		this.layout = layout;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setTextPane(JTextPane textpane) {
+		this.textpane = textpane;
+		textpane.setEditable(false);
+		this.doc = textpane.getStyledDocument();
+	}
+
+	private void setColor(Level p, String v) {
+		StyleConstants.setForeground(attributes.get(p),
+				parseColor(v));
+	}
+
+	private String getColor(Level p) {
+		Color c = StyleConstants.getForeground(attributes.get(p));
+		return c == null ? null : colorToString(c);
+	}
+
+	// ///////////////////////////////////////////////////////////////////
+	// option setters and getters
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setColorEmerg(String color) {
+		setColor(Level.FATAL, color);
+	}
+
+	public String getColorEmerg() {
+		return getColor(Level.FATAL);
+	}
+
+	public void setColorError(String color) {
+		setColor(Level.ERROR, color);
+	}
+
+	public String getColorError() {
+		return getColor(Level.ERROR);
+	}
+
+	public void setColorWarn(String color) {
+		setColor(Level.WARN, color);
+	}
+
+	public String getColorWarn() {
+		return getColor(Level.WARN);
+	}
+
+	public void setColorInfo(String color) {
+		setColor(Level.INFO, color);
+	}
+
+	public String getColorInfo() {
+		return getColor(Level.INFO);
+	}
+
+	public void setColorDebug(String color) {
+		setColor(Level.DEBUG, color);
+	}
+
+	public String getColorDebug() {
+		return getColor(Level.DEBUG);
+	}
+
+	public void setColorBackground(String color) {
+		textpane.setBackground(parseColor(color));
+	}
+
+	public String getColorBackground() {
+		return colorToString(textpane.getBackground());
+	}
+
+	public void setFancy(boolean fancy) {
+		this.fancy = fancy;
+	}
+
+	public boolean getFancy() {
+		return fancy;
+	}
+
+	public void setFontSize(int size) {
+		Enumeration<MutableAttributeSet> e = attributes.elements();
+		while (e.hasMoreElements()) {
+			StyleConstants.setFontSize(e.nextElement(), size);
+		}
+		return;
+	}
+
+	public int getFontSize() {
+		AttributeSet attrSet = attributes.get(Level.INFO);
+		return StyleConstants.getFontSize(attrSet);
+	}
+
+	public void setFontName(String name) {
+		Enumeration<MutableAttributeSet> e = attributes.elements();
+		while (e.hasMoreElements()) {
+			StyleConstants.setFontFamily(e.nextElement(), name);
+		}
+		return;
+	}
+
+	public String getFontName() {
+		AttributeSet attrSet = attributes.get(Level.INFO);
+		return StyleConstants.getFontFamily(attrSet);
+	}
+
+	public boolean requiresLayout() {
+		return true;
+	}
+} // TextPaneAppender
+
