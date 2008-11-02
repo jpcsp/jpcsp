4e3c8315eaa6025a352aea0c1216be847cd26513
pfft.whatever <pfft.whatever@gmail.com>
2008-11-02 00:06:41 +0000

    Added some devctl support for mstick sectors/clusters (incomplete).

--- src/jpcsp/HLE/kernel/managers/UidManager.java
+++ src/jpcsp/HLE/kernel/managers/UidManager.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
 package jpcsp.HLE.kernel.managers;
 
@@ -16,7 +28,7 @@ public class UidManager {
 
     private static HashMap<Integer, SceKernelUid> uidMap;
     private static int uidNext;
-    
+
     public int addObject(SceKernelUid object) {
         while (uidMap.containsKey(uidNext)) {
             uidNext = (uidNext + 1) & 0x7FFFFFFF;
@@ -32,8 +44,8 @@ public class UidManager {
         }
         uidMap.remove(uid);
         return true;
-    }    
-        
+    }
+
     public void reset() {
         uidMap = new HashMap<Integer, SceKernelUid>();
         uidNext = 0x1000;
@@ -43,10 +55,10 @@ public class UidManager {
 
     private UidManager() {
     }
-    
+
     static {
         singleton = new UidManager();
         singleton.reset();
     }
-    
+
 }
