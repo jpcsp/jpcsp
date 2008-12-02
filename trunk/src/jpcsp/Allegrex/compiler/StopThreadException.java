f1bae17053860a9f8bb98d29fb1a0fd2cb7025aa
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2008-12-02 16:51:31 +0000

    Forgot file :-(

---
+++ trunk/src/jpcsp/Allegrex/compiler/StopThreadException.java
@@ -0,0 +1,9 @@
+package jpcsp.Allegrex.compiler;
+
+public class StopThreadException extends Exception {
+	private static final long serialVersionUID = 3001829140250270415L;
+
+	public StopThreadException(String message) {
+		super(message);
+	}
+}
