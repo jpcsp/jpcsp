79ed8b1fdaf2ff6ac95a2303907f7a4f8f904935
gid15@web.de <gid15@users.noreply.github.com>
2009-05-10 07:48:24 +0000

    Improved Texture cache performance (keep combination of texture address and clut address in the cache). Improved Texture reading performance (MemoryReader)

---
+++ src/jpcsp/memory/MemoryReader.java
@@ -0,0 +1,193 @@
+/*
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.memory;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import jpcsp.Memory;
+
+/**
+ * @author gid15
+ *
+ */
+public class MemoryReader {
+	public static IMemoryReader getMemoryReader(int address, int length, int step) {
+		Buffer buffer = Memory.getInstance().getBuffer(address, length);
+
+		if (buffer instanceof IntBuffer) {
+			IntBuffer intBuffer = (IntBuffer) buffer;
+			switch (step) {
+			case 1: return new MemoryReaderInt8(intBuffer);
+			case 2: return new MemoryReaderInt16(intBuffer);
+			case 4: return new MemoryReaderInt32(intBuffer);
+			}
+		} else if (buffer instanceof ByteBuffer) {
+			ByteBuffer byteBuffer = (ByteBuffer) buffer;
+			switch (step) {
+			case 1: return new MemoryReaderByte8(byteBuffer);
+			case 2: return new MemoryReaderByte16(byteBuffer);
+			case 4: return new MemoryReaderByte32(byteBuffer);
+			}
+		}
+
+		// Default (generic) MemoryReader
+		return new MemoryReaderGeneric(address, length, step);
+	}
+
+	private static class MemoryReaderGeneric implements IMemoryReader {
+		private Memory mem;
+		private int address;
+		private int length;
+		private int step;
+
+		public MemoryReaderGeneric(int address, int length, int step) {
+			this.address = address;
+			this.length = length;
+			this.step = step;
+			mem = Memory.getInstance();
+		}
+
+		@Override
+		public int readNext() {
+			int n;
+
+			if (length <= 0) {
+				return 0;
+			}
+
+			switch (step) {
+			case 1: n = mem.read8(address);  break;
+			case 2: n = mem.read16(address); break;
+			case 4: n = mem.read32(address); break;
+			default: n = 0; break;
+			}
+
+			address += step;
+			length -= step;
+
+			return n;
+		}
+	}
+
+	private static class MemoryReaderInt8 implements IMemoryReader {
+		private int index;
+		private int value;
+		private IntBuffer buffer;
+
+		public MemoryReaderInt8(IntBuffer buffer) {
+			this.buffer = buffer;
+			index = 0;
+			value = buffer.get();
+		}
+
+		@Override
+		public int readNext() {
+			int n;
+
+			if (index == 4) {
+				index = 0;
+				value = buffer.get();
+			}
+			n = value & 0xFF;
+			value >>= 8;
+			index++;
+
+			return n;
+		}
+	}
+
+	private static class MemoryReaderInt16 implements IMemoryReader {
+		private int index;
+		private int value;
+		private IntBuffer buffer;
+
+		public MemoryReaderInt16(IntBuffer buffer) {
+			this.buffer = buffer;
+			index = 0;
+		}
+
+		@Override
+		public int readNext() {
+			int n;
+
+			if (index == 0) {
+				value = buffer.get();
+				n = value & 0xFFFF;
+				index++;
+			} else {
+				index = 0;
+				n = value >>> 16;
+			}
+
+			return n;
+		}
+	}
+
+	private static class MemoryReaderInt32 implements IMemoryReader {
+		private IntBuffer buffer;
+
+		public MemoryReaderInt32(IntBuffer buffer) {
+			this.buffer = buffer;
+		}
+
+		@Override
+		public int readNext() {
+			return buffer.get();
+		}
+	}
+
+	private static class MemoryReaderByte8 implements IMemoryReader {
+		private ByteBuffer buffer;
+
+		public MemoryReaderByte8(ByteBuffer buffer) {
+			this.buffer = buffer;
+		}
+
+		@Override
+		public int readNext() {
+			return ((int) buffer.get()) & 0xFF;
+		}
+	}
+
+	private static class MemoryReaderByte16 implements IMemoryReader {
+		private ByteBuffer buffer;
+
+		public MemoryReaderByte16(ByteBuffer buffer) {
+			this.buffer = buffer;
+		}
+
+		@Override
+		public int readNext() {
+			return ((int) buffer.getShort()) & 0xFFFF;
+		}
+	}
+
+	private static class MemoryReaderByte32 implements IMemoryReader {
+		private ByteBuffer buffer;
+
+		public MemoryReaderByte32(ByteBuffer buffer) {
+			this.buffer = buffer;
+		}
+
+		@Override
+		public int readNext() {
+			return buffer.getInt();
+		}
+	}
+}
