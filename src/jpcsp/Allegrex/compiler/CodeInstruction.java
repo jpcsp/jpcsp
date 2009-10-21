jpcsp/jpcsp

commit 5ffac7b3baab19e35c46f7f7395fd3fb257f306e

Small performance improvement in Compiler: - no memory access check for reference to $sp - optimized addi, addiu, or, ori and branching instructions to handle simplified cases where one the register is $zr
Tuned debug logging.

git-svn-id: http://jpcsp.googlecode.com/svn/trunk@1220 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/Allegrex/compiler/CodeInstruction.java
@@ -268,12 +268,61 @@ public class CodeInstruction {
         return Opcodes.NOP;
     }
 
+    private int loadRegistersForBranchingOpcodeBranch2(CompilerContext context, MethodVisitor mv, int branchingOpcode) {
+    	boolean loadRs = true;
+    	boolean loadRt = true;
+
+    	if (context.getRsRegisterIndex() == context.getRtRegisterIndex()) {
+    		if (branchingOpcode == Opcodes.IF_ICMPEQ) {
+    			loadRs = false;
+    			loadRt = false;
+    			if (false) {
+    				branchingOpcode = Opcodes.GOTO;
+    			} else {
+    				// The ASM library has problems with small frames having no
+    				// stack (NullPointerException). Generate a dummy stack requirement.
+    				// "IFEQ 0" is equivalent to GOTO
+    				context.loadImm(0);
+    				branchingOpcode = Opcodes.IFEQ;
+    			}
+    		} else if (branchingOpcode == Opcodes.IF_ICMPNE) {
+    			loadRs = false;
+    			loadRt = false;
+    			branchingOpcode = Opcodes.NOP;
+    		}
+    	} else if (context.isRsRegister0()) {
+    		if (branchingOpcode == Opcodes.IF_ICMPEQ) {
+    			loadRs = false;
+    			branchingOpcode = Opcodes.IFEQ;
+    		} else if (branchingOpcode == Opcodes.IF_ICMPNE) {
+    			loadRs = false;
+    			branchingOpcode = Opcodes.IFNE;
+    		}
+    	} else if (context.isRtRegister0()) {
+    		if (branchingOpcode == Opcodes.IF_ICMPEQ) {
+    			loadRt = false;
+    			branchingOpcode = Opcodes.IFEQ;
+    		} else if (branchingOpcode == Opcodes.IF_ICMPNE) {
+    			loadRt = false;
+    			branchingOpcode = Opcodes.IFNE;
+    		}
+    	}
+
+    	if (loadRs) {
+    		context.loadRs();
+    	}
+    	if (loadRt) {
+    		context.loadRt();
+    	}
+
+    	return branchingOpcode;
+    }
+
     private int getBranchingOpcodeBranch2(CompilerContext context, MethodVisitor mv, int branchingOpcode, int notBranchingOpcode) {
-    	context.loadRs();
-    	context.loadRt();
+    	branchingOpcode = loadRegistersForBranchingOpcodeBranch2(context, mv, branchingOpcode);
 		compileDelaySlot(context, mv);
 
-    	if (branchingOpcode == Opcodes.IF_ICMPEQ && context.getRsRegisterIndex() == context.getRtRegisterIndex() && getBranchingTo() == getAddress()) {
+    	if (branchingOpcode == Opcodes.GOTO && getBranchingTo() == getAddress()) {
     		context.visitLogInfo(mv, "Pausing emulator - branch to self (death loop)");
     		context.visitPauseEmuWithStatus(mv, Emulator.EMU_STATUS_JUMPSELF);
     	}
@@ -282,8 +331,7 @@ public class CodeInstruction {
     }
 
     private int getBranchingOpcodeBranch2L(CompilerContext context, MethodVisitor mv, int branchingOpcode, int notBranchingOpcode) {
-        context.loadRs();
-        context.loadRt();
+    	notBranchingOpcode = loadRegistersForBranchingOpcodeBranch2(context, mv, notBranchingOpcode);
         CodeInstruction afterDelaySlotCodeInstruction = getAfterDelaySlotCodeInstruction(context);
         context.visitJump(notBranchingOpcode, afterDelaySlotCodeInstruction);
         compileDelaySlot(context, mv);
@@ -393,6 +441,15 @@ public class CodeInstruction {
     }
 
     public String toString() {
-        return "0x" + Integer.toHexString(getAddress()).toUpperCase() + " - " + getInsn().disasm(getAddress(), getOpcode());
+    	StringBuffer result = new StringBuffer();
+
+    	result.append(isBranching()    ? "<" : " ");
+    	result.append(isBranchTarget() ? ">" : " ");
+    	result.append(" 0x");
+    	result.append(Integer.toHexString(getAddress()).toUpperCase());
+    	result.append(" - ");
+    	result.append(getInsn().disasm(getAddress(), getOpcode()));
+
+    	return result.toString();
     }
 }

src/jpcsp/Allegrex/Instructions.java
@@ -724,16 +724,7 @@ public void interpret(Processor processor, int insn) {
 }
 @Override
 public void compile(ICompilerContext context, int insn) {
-	if (!context.isRtRegister0()) {
-		context.prepareRtForStore();
-		context.loadRs();
-		int imm = context.getImm16(true);
-		if (imm != 0) {
-			context.loadImm(imm);
-			context.getMethodVisitor().visitInsn(Opcodes.IADD);
-		}
-		context.storeRt();
-	}
+	ADDIU.compile(context, insn);
 }
 @Override
 public String disasm(int address, int insn) {
@@ -766,11 +757,15 @@ public void interpret(Processor processor, int insn) {
 public void compile(ICompilerContext context, int insn) {
 	if (!context.isRtRegister0()) {
 		context.prepareRtForStore();
-		context.loadRs();
 		int imm = context.getImm16(true);
-		if (imm != 0) {
+		if (context.isRsRegister0()) {
 			context.loadImm(imm);
-			context.getMethodVisitor().visitInsn(Opcodes.IADD);
+		} else {
+			context.loadRs();
+			if (imm != 0) {
+				context.loadImm(imm);
+				context.getMethodVisitor().visitInsn(Opcodes.IADD);
+			}
 		}
 		context.storeRt();
 	}
@@ -920,9 +915,19 @@ public void interpret(Processor processor, int insn) {
 public void compile(ICompilerContext context, int insn) {
 	if (!context.isRdRegister0()) {
 		context.prepareRdForStore();
-		context.loadRs();
-		context.loadRt();
-		context.getMethodVisitor().visitInsn(Opcodes.IOR);
+		if (context.isRsRegister0()) {
+			if (context.isRtRegister0()) {
+				context.loadImm(0);
+			} else {
+				context.loadRt();
+			}
+		} else {
+			context.loadRs();
+			if (!context.isRtRegister0()) {
+				context.loadRt();
+				context.getMethodVisitor().visitInsn(Opcodes.IOR);
+			}
+		}
 		context.storeRd();
 	}
 }
@@ -957,11 +962,15 @@ public void interpret(Processor processor, int insn) {
 public void compile(ICompilerContext context, int insn) {
 	if (!context.isRtRegister0()) {
 		context.prepareRtForStore();
-		context.loadRs();
 		int imm = context.getImm16(false);
-		if (imm != 0) {
+		if (context.isRsRegister0()) {
 			context.loadImm(imm);
-			context.getMethodVisitor().visitInsn(Opcodes.IOR);
+		} else {
+			context.loadRs();
+			if (imm != 0) {
+				context.loadImm(imm);
+				context.getMethodVisitor().visitInsn(Opcodes.IOR);
+			}
 		}
 		context.storeRt();
 	}

src/jpcsp/Allegrex/compiler/CodeBlock.java
@@ -59,8 +59,8 @@ public class CodeBlock {
 	}
 
 	public void addInstruction(int address, int opcode, Instruction insn, boolean isBranchTarget, boolean isBranching, int branchingTo) {
-		if (Compiler.log.isDebugEnabled()) {
-			Compiler.log.debug("CodeBlock.addInstruction 0x" + Integer.toHexString(address).toUpperCase() + " - " + insn.disasm(address, opcode));
+		if (Compiler.log.isTraceEnabled()) {
+			Compiler.log.trace("CodeBlock.addInstruction 0x" + Integer.toHexString(address).toUpperCase() + " - " + insn.disasm(address, opcode));
 		}
 
 		CodeInstruction codeInstruction = new CodeInstruction(address, opcode, insn, isBranchTarget, isBranching, branchingTo);
@@ -90,8 +90,8 @@ public class CodeBlock {
 	}
 
 	public void setIsBranchTarget(int address) {
-		if (Compiler.log.isDebugEnabled()) {
-			Compiler.log.debug("CodeBlock.setIsBranchTarget 0x" + Integer.toHexString(address).toUpperCase());
+		if (Compiler.log.isTraceEnabled()) {
+			Compiler.log.trace("CodeBlock.setIsBranchTarget 0x" + Integer.toHexString(address).toUpperCase());
 		}
 
 		CodeInstruction codeInstruction = getCodeInstruction(address);
@@ -366,7 +366,7 @@ public class CodeBlock {
     		cv = new CheckClassAdapter(cv);
     	}
         StringWriter debugOutput = null;
-    	if (Compiler.log.isDebugEnabled()) {
+    	if (Compiler.log.isTraceEnabled()) {
     	    debugOutput = new StringWriter();
     	    PrintWriter debugPrintWriter = new PrintWriter(debugOutput);
     	    cv = new TraceClassVisitor(cv, debugPrintWriter);
@@ -412,10 +412,14 @@ public class CodeBlock {
         cv.visitEnd();
 
     	if (debugOutput != null) {
-    	    Compiler.log.debug(debugOutput.toString());
+    	    Compiler.log.trace(debugOutput.toString());
     	}
 
-	    compiledClass = loadExecutable(context, className, cw.toByteArray());
+    	try {
+    		compiledClass = loadExecutable(context, className, cw.toByteArray());
+    	} catch (NullPointerException e) {
+    		Compiler.log.error("Error while compiling " + className + ": " + e);
+    	}
 
     	return compiledClass;
 	}

src/jpcsp/Allegrex/compiler/Compiler.java
@@ -201,8 +201,8 @@ public class Compiler implements ICompiler {
 	}
 
 	private IExecutable analyse(CompilerContext context, int startAddress, boolean recursive) {
-        if (log.isDebugEnabled()) {
-            log.debug("Compiler.analyse Block 0x" + Integer.toHexString(startAddress));
+        if (log.isTraceEnabled()) {
+            log.trace("Compiler.analyse Block 0x" + Integer.toHexString(startAddress));
         }
         startAddress = startAddress & Memory.addressMask;
         CodeBlock codeBlock = new CodeBlock(startAddress);
@@ -255,8 +255,8 @@ public class Compiler implements ICompiler {
         }
 
         IExecutable executable = codeBlock.getExecutable(context);
-        if (log.isDebugEnabled()) {
-            log.debug("Executable: " + executable);
+        if (log.isTraceEnabled()) {
+            log.trace("Executable: " + executable);
         }
 
         return executable;

src/jpcsp/Allegrex/compiler/CompilerClassLoader.java
@@ -44,8 +44,8 @@ public class CompilerClassLoader extends ClassLoader {
         Class<?> loadedClass = findLoadedClass(name);
 
         if (loadedClass == null && compiler != null) {
-        	if (log.isDebugEnabled()) {
-        		log.debug("ClassLoader creating class " + name);
+        	if (log.isTraceEnabled()) {
+        		log.trace("ClassLoader creating class " + name);
         	}
             IExecutable executable = compiler.compile(name);
             if (executable != null) {

src/jpcsp/Allegrex/compiler/CompilerContext.java
@@ -64,6 +64,7 @@ public class CompilerContext implements ICompilerContext {
     private static final int LOCAL_TMP = 7;
     private static final int LOCAL_MAX = 8;
     private static final int STACK_MAX = 10;
+    private static final int spRegisterIndex = 29;
     public Set<Integer> analysedAddresses = new HashSet<Integer>();
     public Stack<Integer> blocksToBeAnalysed = new Stack<Integer>();
     private int currentInstructionCount;
@@ -805,7 +806,7 @@ public class CompilerContext implements ICompilerContext {
 			mv.visitInsn(Opcodes.IADD);
 		}
 
-		if (RuntimeContext.debugMemoryRead && (!RuntimeContext.debugMemoryReadWriteNoSP || registerIndex != 29)) {
+		if (RuntimeContext.debugMemoryRead && (!RuntimeContext.debugMemoryReadWriteNoSP || registerIndex != spRegisterIndex)) {
 			mv.visitInsn(Opcodes.DUP);
 			loadImm(0);
             mv.visitLdcInsn(codeInstruction.getAddress());
@@ -817,7 +818,11 @@ public class CompilerContext implements ICompilerContext {
 		if (RuntimeContext.memoryInt == null) {
 	        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, memoryInternalName, "read32", "(I)I");
 		} else {
-            if (checkMemoryAccess()) {
+			if (registerIndex == spRegisterIndex) {
+				// No need to check for a valid memory access when referencing the $sp register
+				loadImm(2);
+    			mv.visitInsn(Opcodes.IUSHR);
+			} else if (checkMemoryAccess()) {
                 mv.visitLdcInsn(codeInstruction.getAddress());
                 mv.visitMethodInsn(Opcodes.INVOKESTATIC, runtimeContextInternalName, "checkMemoryRead32", "(II)I");
                 loadImm(2);
@@ -954,7 +959,11 @@ public class CompilerContext implements ICompilerContext {
 		}
 
 		if (RuntimeContext.memoryInt != null) {
-	        if (checkMemoryAccess()) {
+			if (registerIndex == spRegisterIndex) {
+				// No need to check for a valid memory access when referencing the $sp register
+				loadImm(2);
+    			mv.visitInsn(Opcodes.IUSHR);
+			} else if (checkMemoryAccess()) {
 	            mv.visitLdcInsn(codeInstruction.getAddress());
 	            mv.visitMethodInsn(Opcodes.INVOKESTATIC, runtimeContextInternalName, "checkMemoryWrite32", "(II)I");
                 loadImm(2);
@@ -993,7 +1002,7 @@ public class CompilerContext implements ICompilerContext {
 			mv.visitInsn(Opcodes.SWAP);
 		}
 
-		if (RuntimeContext.debugMemoryWrite && (!RuntimeContext.debugMemoryReadWriteNoSP || registerIndex != 29)) {
+		if (RuntimeContext.debugMemoryWrite && (!RuntimeContext.debugMemoryReadWriteNoSP || registerIndex != spRegisterIndex)) {
 			mv.visitInsn(Opcodes.DUP2);
 			mv.visitInsn(Opcodes.SWAP);
 			loadImm(2);

src/jpcsp/graphics/VideoEngine.java
@@ -1945,7 +1945,9 @@ public class VideoEngine {
                 Memory mem = Memory.getInstance();
                 vertexStatistics.start();
 
-                if (vinfo.index == 0 && type != PRIM_SPRITES && mem.isAddressGood(vinfo.ptr_vertex)) {
+                // Do not use optimized VertexInfo reading when tracing is enabled,
+                // it doesn't produce any trace information
+                if (vinfo.index == 0 && type != PRIM_SPRITES && mem.isAddressGood(vinfo.ptr_vertex) && !log.isTraceEnabled()) {
                 	// Optimized VertexInfo reading:
                 	// - do not copy the info already available in the OpenGL format
                 	//   (native format), load it into nativeBuffer (a direct buffer

commit 2388785adb1deaea1e9f49066a0ec29a30e80367

Fixed a compiler problem introduced by r1220

src/jpcsp/Allegrex/compiler/CodeInstruction.java
@@ -332,8 +332,10 @@ public class CodeInstruction {
 
     private int getBranchingOpcodeBranch2L(CompilerContext context, MethodVisitor mv, int branchingOpcode, int notBranchingOpcode) {
     	notBranchingOpcode = loadRegistersForBranchingOpcodeBranch2(context, mv, notBranchingOpcode);
-        CodeInstruction afterDelaySlotCodeInstruction = getAfterDelaySlotCodeInstruction(context);
-        context.visitJump(notBranchingOpcode, afterDelaySlotCodeInstruction);
+    	if (notBranchingOpcode != Opcodes.NOP) {
+    		CodeInstruction afterDelaySlotCodeInstruction = getAfterDelaySlotCodeInstruction(context);
+    		context.visitJump(notBranchingOpcode, afterDelaySlotCodeInstruction);
+    	}
         compileDelaySlot(context, mv);
 
         return Opcodes.GOTO;