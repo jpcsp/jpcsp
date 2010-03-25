bdda6ee4f0fcca15c5f7b6edad1d097d0c34147c
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2010-03-25 16:43:10 +0000

    Implemented sceKernelReleaseWaitThread()

--- trunk/src/jpcsp/HLE/SyscallHandler.java
+++ trunk/src/jpcsp/HLE/SyscallHandler.java
@@ -360,7 +360,9 @@ public class SyscallHandler {
                 case 0x2079:
                     ThreadMan.getInstance().ThreadMan_sceKernelRotateThreadReadyQueue(gpr[4]);
                     break;
-		// sceKernelReleaseWaitThread(0x207a),
+                case 0x207a:
+                	ThreadMan.getInstance().ThreadMan_sceKernelReleaseWaitThread(gpr[4]);
+                	break;
                 case 0x207b:
                     ThreadMan.getInstance().ThreadMan_sceKernelGetThreadId();
                     break;
