543c933a85a46d5ff5d43b15fa01bb38725772f6
Florent.Castelli <Florent.Castelli@4ab978e6-3352-0410-996c-0b42db0051c8>
2010-04-27 12:18:26 +0000

    Reorganization of lib directory with a folder per supported platform containing all the .dll/.so files in order to clean the root directory and allow them to be in the same directory without replacing the previous shared objects. Edited the classpath file for Eclipse, it should work then for the windows-x86 platform by default and I've tried the same change for Netbeans, but is untested. For external launchers, don't forget to add the -Djava.library.path=lib/"platform" to the JVM options or it won't load. Created two bat scripts to run the emulator that checks the environment and runs the correct JVM for the requested build (x86/amd64). Added an ant script to build a jar for the project or run jpcsp for windows-x86 platform (99% of users anyway). If "svnversion" is available, it will even create the title.txt file automatically. Removed the CUSTOM_NAME variable, if we want a custom name, just edit the regular name and don't bother anymore !

---
+++ trunk/start-windows-amd64.bat
@@ -0,0 +1,18 @@
+@echo off
+if "%programfiles(x86)%XXX"=="XXX" goto JAVA32
+if not exist "%programfiles%\Java\jre6\bin" goto JAVAMISSING
+echo Running Jpcsp 64bit...
+"%programfiles%\Java\jre6\bin\java" -Xmx512m -Djava.library.path=lib/windows-amd64 -jar bin/jpcsp.jar
+goto END
+
+:JAVA32
+echo Unable to run a 64bit build on a 32bit platform. Install a 64bit version of Windows first.
+pause
+goto END
+
+:JAVAMISSING
+echo The required version of Java has not been installed.
+echo Go to http://java.sun.com to install the 64bit Java JRE.
+pause
+
+:END
---
+++ trunk/start-windows-x86.bat
@@ -0,0 +1,19 @@
+@echo off
+if "%programfiles(x86)%XXX"=="XXX" goto JAVA32
+if not exist "%programfiles(x86)%\Java\jre6\bin" goto JAVAMISSING
+echo Running Jpcsp 32bit...
+"%programfiles(x86)%\Java\jre6\bin\java" -Xmx512m -Djava.library.path=lib/windows-x86 -jar bin/jpcsp.jar
+goto END
+
+:JAVA32
+if not exist "%programfiles%\Java\jre6\bin" goto JAVAMISSING
+echo Running Jpcsp 32bit...
+"%programfiles%\Java\jre6\bin\java" -Xmx512m -Djava.library.path=lib/windows-x86 -jar bin/jpcsp.jar
+goto END
+
+:JAVAMISSING
+echo The required version of Java has not been installed or isn't recognized.
+echo Go to http://java.sun.com to install the 32bit Java JRE.
+pause
+
+:END
