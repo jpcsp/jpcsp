8dfdc9511f581ef0b18800a4ccf22ab79c854d25
hykem.the.demon <hykem.the.demon@4ab978e6-3352-0410-996c-0b42db0051c8>
2010-06-27 23:43:35 +0000

    Changed default heap size (in Windows batch files) to 1024Mb.
    Added small new findings in sceAtrac3plus.
    
    Added small improvements to sceMpeg:
    - Improved Media Engine's timestamp handling (based on packet's DTS);
    - Allow Media Engine to load PSP_O_NBUF video files.

--- trunk/start-windows-amd64.bat
+++ trunk/start-windows-amd64.bat
@@ -2,7 +2,7 @@
 if "%programfiles(x86)%XXX"=="XXX" goto JAVA32
 if not exist "%programfiles%\Java\jre6\bin" goto JAVAMISSING
 echo Running Jpcsp 64bit...
-"%programfiles%\Java\jre6\bin\java" -Xmx512m -Djava.library.path=lib/windows-amd64 -jar bin/jpcsp.jar
+"%programfiles%\Java\jre6\bin\java" -Xmx1024m -Djava.library.path=lib/windows-amd64 -jar bin/jpcsp.jar
 goto END
 
 :JAVA32
--- trunk/start-windows-x86.bat
+++ trunk/start-windows-x86.bat
@@ -3,13 +3,13 @@ set PATH=%PATH%;lib\;lib\windows-x86\
 if "%programfiles(x86)%XXX"=="XXX" goto JAVA32
 if not exist "%programfiles(x86)%\Java\jre6\bin" goto JAVAMISSING
 echo Running Jpcsp 32bit...
-"%programfiles(x86)%\Java\jre6\bin\java" -Xmx512m -Djava.library.path=lib/windows-x86 -jar bin/jpcsp.jar
+"%programfiles(x86)%\Java\jre6\bin\java" -Xmx1024m -Djava.library.path=lib/windows-x86 -jar bin/jpcsp.jar
 goto END
 
 :JAVA32
 if not exist "%programfiles%\Java\jre6\bin" goto JAVAMISSING
 echo Running Jpcsp 32bit...
-"%programfiles%\Java\jre6\bin\java" -Xmx512m -Djava.library.path=lib/windows-x86 -jar bin/jpcsp.jar
+"%programfiles%\Java\jre6\bin\java" -Xmx1024m -Djava.library.path=lib/windows-x86 -jar bin/jpcsp.jar
 goto END
 
 :JAVAMISSING
