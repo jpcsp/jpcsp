bb0eb13b77b3d77811bf4ede43d759a23588dc91
i30817 <i30817@users.noreply.github.com>
2010-09-27 22:13:46 +0000

    remove dead code

--- src/jpcsp/format/FunctionLibrary.java
+++ src/jpcsp/format/FunctionLibrary.java
@@ -17,7 +17,6 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
 package jpcsp.format;
 
 import java.util.Hashtable;
-import java.util.List;
 
 /**
  *
@@ -51,55 +50,5 @@ class Module {
 
     public void addFunction(String nid, String name) {
         m_functions.put(nid, name);
-    }
-}
-
-public class FunctionLibrary {
-
-    private int m_moduleCount = 0;
-    private int m_functionCount = 0;
-    private Hashtable<String, Module> m_moduleTable;
-    private List<Module> m_moduleList;
-
-    public String getFunctionFromLibrary(String library, String nid) {
-        String ret;
-        try {
-            ret = (m_moduleTable.get(library)).getFunctionFromModule(nid);
-            return ret;
-        } catch (NullPointerException dummy1) {
-            try {
-                Module that = m_moduleTable.get(library);
-                ret = library + "_Unknown_" + nid.substring(2);
-                addFunctionToLibrary(library, nid, ret);
-                return ret;
-            } catch (NullPointerException dummy2) {
-                ret = null;
-                addModule(library);
-                addFunctionToLibrary(library, nid, library + "_Unknown_" + nid.substring(2));
-                return ret;
-            }
-        }
-    }
-
-    public void addModule(String module) {
-        m_moduleTable.put(module, new Module(module));
-        m_moduleCount++;
-    }
-
-    public void addFunctionToLibrary(String module, String nid, String name) {
-        Module that = m_moduleTable.get(module);
-        that.addFunction(nid, name);
-        m_functionCount++;
-    }
-
-    public int getModuleCount() {
-        return m_moduleCount;
-    }
-
-    public int getFunctionCount() {
-        return m_functionCount;
-    }
-
-    FunctionLibrary(String fw) throws Exception {
     }
 }
