31f04626508dca9be1641a1aa95d7adfc00b3194
gid15@web.de <gid15@users.noreply.github.com>
2010-12-19 15:51:34 +0000

    Mute the system volume while calling the external decoder to mute the noise produced by HIMDRender, as suggested by Itaru.

--- lib/windows-x86/DecodeAudio.bat
+++ lib/windows-x86/DecodeAudio.bat
@@ -1,5 +1,10 @@
 @echo off
 
-rem TODO HIMDRender is producing some noise, how to avoid it?
+rem Muting the system volume because HIMDRender is producing some noise
+lib\windows-x86\nircmd\nircmdc.exe mutesysvolume 1
 
 lib\windows-x86\HIMDRender.exe -e -i %1 -o %2
+
+
+rem un-Muting the system volume
+lib\windows-x86\nircmd\nircmdc.exe mutesysvolume 0
