244009c8b5e7d56fca5b560573ffd367dabca8db
gid15@web.de <gid15@users.noreply.github.com>
2010-12-23 19:54:50 +0000

    Fixed NegativeArraySizeException in __sceSasSetVoice() and __sceSasSetVoicePCM()

--- src/jpcsp/HLE/modules500/sceSasCore.java
+++ src/jpcsp/HLE/modules500/sceSasCore.java
@@ -18,6 +18,7 @@ package jpcsp.HLE.modules500;
 
 import jpcsp.Processor;
 import jpcsp.Allegrex.CpuState;
+import jpcsp.HLE.kernel.types.SceKernelErrors;
 import jpcsp.HLE.modules.HLEModuleFunction;
 import jpcsp.HLE.modules.HLEModuleManager;
 
@@ -55,7 +56,10 @@ public class sceSasCore extends jpcsp.HLE.modules150.sceSasCore {
             + String.format("sasCore=0x%08x voice=%d vagAddr=0x%08x size=0x%08x loopmode=%d",
             sasCore, voice, vagAddr, size, loopmode));
 
-        if (isSasHandleGood(sasCore, "__sceSasSetVoice", cpu) && isVoiceNumberGood(voice, "__sceSasSetVoice", cpu)) {
+        if (size <= 0 || (size & 0xF) != 0) {
+        	log.warn(String.format("__sceSasSetVoicePCM invalid size 0x%08X", size));
+        	cpu.gpr[2] = SceKernelErrors.ERROR_SAS_INVALID_PARAMETER;
+        } else if (isSasHandleGood(sasCore, "__sceSasSetVoice", cpu) && isVoiceNumberGood(voice, "__sceSasSetVoice", cpu)) {
             voices[voice].setSamples(decodeSamples(processor, vagAddr, size));
             voices[voice].setLoopMode(loopmode);
 
