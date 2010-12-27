16ca13995fc2e4cbae910221f2b77eed63ed93a0
gid15@web.de <gid15@users.noreply.github.com>
2010-12-27 17:09:27 +0000

    Updated version of HIMDRENDER provided by Hyakki (nircmd no longer needed).
    Comment from Hyakki:
    "here is a modified version of himdrender i made, compiled /w vs2008 see below for changes.
    I fixed the WER (windows error reporting) error some audio would get (the annoying "Himdrender has stopped working..." error) this way if himd gets a bad file, instead of crashing it won't do anything.
    Code:
    igraph->Connect(omgsrc_ipinout,omgtrns_ipinin)
    ->
    igraph->ConnectDirect(omgsrc_ipinout, omgtrns_ipinin, NULL)
    
    other changes made
    1. Replaced the connect function (it would cause an 'wer' error popup when trying to decode bad/non decode-able files)
    2. Removed some un-necessary dialogs (file browser popup, press a key to exit pause)
    3. Added in its own mute function so it will mute (mutes himdrender.exe only) when decoding starts, and un-mute when done, this way jpcsp can always have un-muted audio, and no noise on decode., no longer needs nircmd"

--- lib/windows-x86/DecodeAudio.bat
+++ lib/windows-x86/DecodeAudio.bat
@@ -1,10 +1,2 @@
 @echo off
-
-rem Muting the system volume because HIMDRender is producing some noise
-lib\windows-x86\nircmd\nircmdc.exe mutesysvolume 1
-
 lib\windows-x86\HIMDRender.exe -e -i %1 -o %2
-
-
-rem un-Muting the system volume
-lib\windows-x86\nircmd\nircmdc.exe mutesysvolume 0
