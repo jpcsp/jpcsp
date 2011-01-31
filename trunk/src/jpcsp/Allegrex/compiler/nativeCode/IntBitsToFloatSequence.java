79dcc32012500541384431763e310b224cf56ae1
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2011-01-31 20:21:09 +0000

    Fix for "intBitsToFloat" problem in "Kingdom Hearts: Birth by Sleep - US - ULUS10505"

--- trunk/src/jpcsp/Allegrex/compiler/nativeCode/IntBitsToFloatSequence.java
+++ trunk/src/jpcsp/Allegrex/compiler/nativeCode/IntBitsToFloatSequence.java
@@ -60,4 +60,56 @@ public class IntBitsToFloatSequence extends AbstractNativeCodeSequence {
 		mem.write32(v0 + 12, getGprA1());
 		setRegisterValue(2, v0);
 	}
+
+    // addu       $v0, $a0, $zr <=> move $v0, $a0
+    // MTV.s      $a1, S010.s
+    // vuc2i.s    S000.s, S010.s
+    // vi2f.q     C000.q, C000.q, 31
+    // sv.q       C000.q, 0($a0)
+    // jr         $ra
+    // nop
+	static public void callSequence3() {
+		Memory mem = getMemory();
+
+		int a0 = getGprA0();
+		setGprV0(a0);
+		int a1 = getGprA1();
+		mem.write32(a0, Float.floatToRawIntBits((a1 & 0xFF) / 255.f));
+		mem.write32(a0 + 4, Float.floatToRawIntBits(((a1 >> 8) & 0xFF) / 255.f));
+		mem.write32(a0 + 8, Float.floatToRawIntBits(((a1 >> 16) & 0xFF) / 255.f));
+		mem.write32(a0 + 12, Float.floatToRawIntBits(((a1 >> 24) & 0xFF) / 255.f));
+	}
+
+	// lv.s       S000.s, 0($t2)
+    // lv.s       S001.s, 4($t2)
+    // vs2i.p     C010.p, C000.p
+    // vi2f.q     C000.q, C010.q, 16
+    // sv.s       S000.s, 0($t0)
+    // sv.s       S001.s, 4($t0)
+    // sv.s       S002.s, 12($t0)
+	// sv.s       S001.s, 16($t0)
+	// sv.s       S000.s, 24($t0)
+	// sv.s       S003.s, 28($t0)
+	// sv.s       S002.s, 36($t0)
+	// sv.s       S003.s, 40($t0)
+	static public void callSequence4(int regSrc, int regDst) {
+		Memory mem = getMemory();
+
+		int src = getRegisterValue(regSrc);
+		int dst = getRegisterValue(regDst);
+		int n1 = mem.read32(src);
+		int n2 = mem.read32(src + 4);
+		int s000 = Float.floatToRawIntBits((float) (short) n1);
+		int s001 = Float.floatToRawIntBits((float) (short) (n1 >> 16));
+		int s002 = Float.floatToRawIntBits((float) (short) n2);
+		int s003 = Float.floatToRawIntBits((float) (short) (n2 >> 16));
+		mem.write32(dst, s000);
+		mem.write32(dst + 4, s001);
+		mem.write32(dst + 12, s002);
+		mem.write32(dst + 16, s001);
+		mem.write32(dst + 24, s000);
+		mem.write32(dst + 28, s003);
+		mem.write32(dst + 36, s002);
+		mem.write32(dst + 40, s003);
+	}
 }
