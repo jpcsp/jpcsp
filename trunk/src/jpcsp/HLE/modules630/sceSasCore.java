445c06cdf13d189fcf08fc978c128af066e4a24d
hykem.the.demon@gmail.com <hykem.the.demon@gmail.com@4ab978e6-3352-0410-996c-0b42db0051c8>
2011-04-04 19:58:04 +0000

    Reversed and implemented several newly discovered functions: - Implemented _sceAtracGetContextAddress in sceAtrac3plus; - Added drafts for __sceSasSetVoiceATRAC3, __sceSasConcatenateATRAC3 and __sceSasUnsetATRAC3; - Resolved previously unknown utility SavedataErr and added draft for GamedataInstall utility in sceUtility.

---
+++ trunk/src/jpcsp/HLE/modules630/sceSasCore.java
@@ -0,0 +1,144 @@
+/*
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.HLE.modules630;
+
+import jpcsp.Processor;
+import jpcsp.Allegrex.CpuState;
+import jpcsp.HLE.kernel.managers.IntrManager;
+import jpcsp.HLE.kernel.types.SceKernelErrors;
+import jpcsp.HLE.modules.HLEModuleFunction;
+import jpcsp.HLE.modules.HLEModuleManager;
+
+public class sceSasCore extends jpcsp.HLE.modules500.sceSasCore {
+
+    @Override
+    public void installModule(HLEModuleManager mm, int version) {
+        super.installModule(mm, version);
+
+        if (version >= 630) {
+            mm.addFunction(0x4AA9EAD6, __sceSasSetVoiceATRAC3Function);
+            mm.addFunction(0x7497EA85, __sceSasConcatenateATRAC3Function);
+            mm.addFunction(0xF6107F00, __sceSasUnsetATRAC3Function);
+        }
+    }
+
+    @Override
+    public void uninstallModule(HLEModuleManager mm, int version) {
+        super.uninstallModule(mm, version);
+
+        if (version >= 630) {
+            mm.removeFunction(__sceSasSetVoiceATRAC3Function);
+            mm.removeFunction(__sceSasConcatenateATRAC3Function);
+            mm.removeFunction(__sceSasUnsetATRAC3Function);
+        }
+    }
+
+    public void __sceSasSetVoiceATRAC3(Processor processor) {
+        CpuState cpu = processor.cpu;
+
+        int sasCore = cpu.gpr[4];
+        int voice = cpu.gpr[5];
+        int atrac3Addr = cpu.gpr[6];
+
+        log.warn("UNIMPLEMENTED: __sceSasSetVoiceATRAC3: sasCore=0x" + Integer.toHexString(sasCore)
+                + ", voice=" + voice
+                + ", atrac3Addr=0x" + Integer.toHexString(atrac3Addr));
+
+        if (IntrManager.getInstance().isInsideInterrupt()) {
+            cpu.gpr[2] = SceKernelErrors.ERROR_KERNEL_CANNOT_BE_CALLED_FROM_INTERRUPT;
+            return;
+        }
+        cpu.gpr[2] = 0;
+    }
+
+    public void __sceSasConcatenateATRAC3(Processor processor) {
+        CpuState cpu = processor.cpu;
+
+        int sasCore = cpu.gpr[4];
+        int voice = cpu.gpr[5];
+        int atrac3Addr = cpu.gpr[6];
+        int unk1 = cpu.gpr[7];
+
+        log.warn("UNIMPLEMENTED: __sceSasConcatenateATRAC3: sasCore=0x" + Integer.toHexString(sasCore)
+                + ", voice=" + voice
+                + ", atrac3Addr=0x" + Integer.toHexString(atrac3Addr)
+                + ", unk1=0x" + Integer.toHexString(unk1));
+
+        if (IntrManager.getInstance().isInsideInterrupt()) {
+            cpu.gpr[2] = SceKernelErrors.ERROR_KERNEL_CANNOT_BE_CALLED_FROM_INTERRUPT;
+            return;
+        }
+        cpu.gpr[2] = 0;
+    }
+
+    public void __sceSasUnsetATRAC3(Processor processor) {
+        CpuState cpu = processor.cpu;
+
+        int sasCore = cpu.gpr[4];
+        int voice = cpu.gpr[5];
+        int atrac3Addr = cpu.gpr[6];
+
+        log.warn("UNIMPLEMENTED: __sceSasUnsetATRAC3: sasCore=0x" + Integer.toHexString(sasCore)
+                + ", voice=" + voice
+                + ", atrac3Addr=0x" + Integer.toHexString(atrac3Addr));
+
+        if (IntrManager.getInstance().isInsideInterrupt()) {
+            cpu.gpr[2] = SceKernelErrors.ERROR_KERNEL_CANNOT_BE_CALLED_FROM_INTERRUPT;
+            return;
+        }
+        cpu.gpr[2] = 0;
+    }
+
+    public final HLEModuleFunction __sceSasSetVoiceATRAC3Function = new HLEModuleFunction("sceSasCore", "__sceSasSetVoiceATRAC3") {
+
+        @Override
+        public final void execute(Processor processor) {
+            __sceSasSetVoiceATRAC3(processor);
+        }
+
+        @Override
+        public final String compiledString() {
+            return "jpcsp.HLE.Modules.sceSasCoreModule.__sceSasSetVoiceATRAC3(processor);";
+        }
+    };
+
+    public final HLEModuleFunction __sceSasConcatenateATRAC3Function = new HLEModuleFunction("sceSasCore", "__sceSasConcatenateATRAC3") {
+
+        @Override
+        public final void execute(Processor processor) {
+            __sceSasConcatenateATRAC3(processor);
+        }
+
+        @Override
+        public final String compiledString() {
+            return "jpcsp.HLE.Modules.sceSasCoreModule.__sceSasConcatenateATRAC3(processor);";
+        }
+    };
+
+    public final HLEModuleFunction __sceSasUnsetATRAC3Function = new HLEModuleFunction("sceSasCore", "__sceSasUnsetATRAC3") {
+
+        @Override
+        public final void execute(Processor processor) {
+            __sceSasUnsetATRAC3(processor);
+        }
+
+        @Override
+        public final String compiledString() {
+            return "jpcsp.HLE.Modules.sceSasCoreModule.__sceSasUnsetATRAC3(processor);";
+        }
+    };
+}
