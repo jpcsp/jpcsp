75ebcb6c9c462c098a0a690625d78c957190de9b
gid15@web.de <gid15@users.noreply.github.com>
2011-05-27 18:59:36 +0000

    Added draft version for sceUtilityInstallGetStatus(), sceUtilityInstallInitStart(), sceUtilityInstallShutdownStart() and sceUtilityInstallUpdate().

---
+++ src/jpcsp/HLE/kernel/types/SceUtilityInstallParams.java
@@ -0,0 +1,59 @@
+/*
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.HLE.kernel.types;
+
+public class SceUtilityInstallParams extends pspAbstractMemoryMappedStructure {
+	public pspUtilityDialogCommon base;
+	public int unknown1;
+	public String gameName; // DISCID
+	byte[] key = new byte[0x10];
+
+	@Override
+	protected void read() {
+		base = new pspUtilityDialogCommon();
+		read(base);
+		setMaxSize(base.size);
+
+		unknown1 = read32();
+		gameName = readStringNZ(13);
+		readUnknown(3);
+		readUnknown(16);
+		read8Array(key);
+	}
+
+	@Override
+	protected void write() {
+        setMaxSize(base.size);
+	    write(base);
+
+	    write32(unknown1);
+	    writeStringNZ(13, gameName);
+	    writeUnknown(3);
+	    writeUnknown(16);
+	    write8Array(key);
+	}
+
+	@Override
+	public int sizeof() {
+		return base.size;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Address 0x%08X, unknown1=%d, gameName=%s", getBaseAddress(), unknown1, gameName);
+	}
+}
