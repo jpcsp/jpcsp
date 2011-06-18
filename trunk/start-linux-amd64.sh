2662fe268eeb635c325a7143f480a3272e9e0da2
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2011-06-18 14:43:09 +0000

    Increased MaxPermSize from 64 to 128m. It seems that several games require this setting. Hoping this will not cause problems for computers low on memory.

--- trunk/start-linux-amd64.sh
+++ trunk/start-linux-amd64.sh
@@ -1,2 +1,2 @@
 #!/bin/sh\n
-java -Xmx1024m -XX:MaxPermSize=64m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/amd-64 -jar bin/jpcsp.jar
+java -Xmx1024m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/amd-64 -jar bin/jpcsp.jar
