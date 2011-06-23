b7adecab98ca0b204eb46b5cfefc95b86cb79505
i30817 <i30817@users.noreply.github.com>
2011-06-23 13:42:52 +0000

    making exec path bin for the build bot.

--- start-linux-amd64.sh
+++ start-linux-amd64.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx1024m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/linux-amd64 -jar dist/Jpcsp.jar
+java -Xmx1024m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/linux-amd64 -jar bin/Jpcsp.jar
