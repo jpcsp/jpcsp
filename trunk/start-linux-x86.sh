124483de7c3eab379d94682c09d2854e3bfcd4fd
i30817 <i30817@4ab978e6-3352-0410-996c-0b42db0051c8>
2011-06-23 13:42:52 +0000

    making exec path bin for the build bot.

--- trunk/start-linux-x86.sh
+++ trunk/start-linux-x86.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx1024m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/linux-x86 -jar dist/Jpcsp.jar
+java -Xmx1024m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/linux-x86 -jar bin/Jpcsp.jar
