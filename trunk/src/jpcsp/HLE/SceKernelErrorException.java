456976e9fe0a81c4342174f49d7d71a42ac4c692
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2011-08-29 17:58:03 +0000

    Fixed "Error calling ... sceKernelTerminateThread...": return value is always an "int" value when a SceKernelErrorException is raised by an HLE method (even if the HLE method is returning "void").

--- trunk/src/jpcsp/HLE/SceKernelErrorException.java
+++ trunk/src/jpcsp/HLE/SceKernelErrorException.java
@@ -3,8 +3,13 @@ package jpcsp.HLE;
 @SuppressWarnings("serial")
 public class SceKernelErrorException extends RuntimeException {
 	public int errorCode;
-	
+
 	public SceKernelErrorException(int errorCode) {
 		this.errorCode = errorCode;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("SceKernelErrorException(errorCode=0x%08X)", errorCode);
 	}
 }
