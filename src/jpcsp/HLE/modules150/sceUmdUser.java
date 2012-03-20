jpcsp/jpcsp

commit fae5b3ddeaaa80801451adff81e7860326d5fc03

Fixed sceKernelFreeVpl(): manages a list of free VPL entries. Fixed error handling in sceKernelSuspendThread. Code clean-up in thread wait states including several small fixes in managers.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@2209 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/modules150/sceUmdUser.java
@@ -19,6 +19,7 @@ package jpcsp.HLE.modules150;
 import static jpcsp.HLE.kernel.types.SceKernelErrors.ERROR_KERNEL_WAIT_CANCELLED;
 import static jpcsp.HLE.kernel.types.SceKernelErrors.ERROR_KERNEL_WAIT_STATUS_RELEASED;
 import static jpcsp.HLE.kernel.types.SceKernelErrors.ERROR_KERNEL_WAIT_TIMEOUT;
+import static jpcsp.HLE.kernel.types.SceKernelThreadInfo.JPCSP_WAIT_UMD;
 import static jpcsp.util.Utilities.readStringZ;
 
 import java.util.LinkedList;
@@ -130,12 +131,9 @@ public class sceUmdUser implements HLEModule, HLEStartModule {
     public int getUmdStat() {
         int stat;
         if (iso != null) {
-            stat = PSP_UMD_PRESENT;
+            stat = PSP_UMD_PRESENT | PSP_UMD_READY;
             if (umdActivated) {
-                stat |= PSP_UMD_READY;
                 stat |= PSP_UMD_READABLE;
-            } else {
-                stat |= PSP_UMD_NOT_READY;
             }
         } else {
             stat = PSP_UMD_NOT_PRESENT;
@@ -163,8 +161,6 @@ public class sceUmdUser implements HLEModule, HLEStartModule {
 
     public void onThreadWaitTimeout(SceKernelThreadInfo thread) {
         log.info("UMD stat timedout");
-        // Untrack
-        thread.wait.waitingOnUmd = false;
         removeWaitingThread(thread);
         // Return WAIT_TIMEOUT
         thread.cpuContext.gpr[2] = ERROR_KERNEL_WAIT_TIMEOUT;
@@ -172,15 +168,13 @@ public class sceUmdUser implements HLEModule, HLEStartModule {
 
     public void onThreadWaitReleased(SceKernelThreadInfo thread) {
         log.info("UMD stat released");
-        // Untrack
-        thread.wait.waitingOnUmd = false;
         removeWaitingThread(thread);
         // Return ERROR_WAIT_STATUS_RELEASED
         thread.cpuContext.gpr[2] = ERROR_KERNEL_WAIT_STATUS_RELEASED;
     }
 
     public void onThreadDeleted(SceKernelThreadInfo thread) {
-        if (thread.wait.waitingOnUmd) {
+        if (thread.waitType == JPCSP_WAIT_UMD) {
             removeWaitingThread(thread);
         }
     }
@@ -190,10 +184,11 @@ public class sceUmdUser implements HLEModule, HLEStartModule {
             SceKernelThreadInfo waitingThread = lit.next();
             if (waitingThread.status == SceKernelThreadInfo.PSP_THREAD_WAITING) {
                 int wantedUmdStat = waitingThread.wait.wantedUmdStat;
-                if (waitingThread.wait.waitingOnUmd && checkDriveStat(wantedUmdStat)) {
-                    log.debug("sceUmdUser - checkWaitingThreads waking " + Integer.toHexString(waitingThread.uid) + " thread:'" + waitingThread.name + "'");
-                    // Untrack
-                    waitingThread.wait.waitingOnUmd = false;
+                if (waitingThread.waitType == JPCSP_WAIT_UMD &&
+                        checkDriveStat(wantedUmdStat)) {
+                	if (log.isDebugEnabled()) {
+                		log.debug("sceUmdUser - checkWaitingThreads waking " + Integer.toHexString(waitingThread.uid) + " thread:'" + waitingThread.name + "'");
+                	}
                     lit.remove();
                     // Return success
                     waitingThread.cpuContext.gpr[2] = 0;
@@ -230,7 +225,17 @@ public class sceUmdUser implements HLEModule, HLEStartModule {
         }
         umdActivated = true;
         cpu.gpr[2] = 0;
-        checkWaitingThreads();
+
+        SceKernelThreadInfo thread = Modules.ThreadManForUserModule.getCurrentThread();
+    	thread.doCallbacks = true;
+        if (iso != null) {
+        	Modules.ThreadManForUserModule.hleKernelNotifyCallback(SceKernelThreadInfo.THREAD_CALLBACK_UMD, PSP_UMD_PRESENT | PSP_UMD_READABLE);
+        } else {
+        	Modules.ThreadManForUserModule.hleKernelNotifyCallback(SceKernelThreadInfo.THREAD_CALLBACK_UMD, PSP_UMD_NOT_PRESENT | PSP_UMD_NOT_READY);
+        }
+    	thread.doCallbacks = false;
+
+    	checkWaitingThreads();
     }
 
     public void sceUmdDeactivate(Processor processor) {
@@ -247,9 +252,21 @@ public class sceUmdUser implements HLEModule, HLEStartModule {
             cpu.gpr[2] = SceKernelErrors.ERROR_KERNEL_CANNOT_BE_CALLED_FROM_INTERRUPT;
             return;
         }
+        // Trigger the callback only if the UMD was already activated
+        boolean triggerCallback = umdActivated;
         umdActivated = false;
         umdDeactivateCalled = true;
         cpu.gpr[2] = 0;
+        if (triggerCallback) {
+        	SceKernelThreadInfo thread = Modules.ThreadManForUserModule.getCurrentThread();
+        	thread.doCallbacks = true;
+	        if (iso != null) {
+	        	Modules.ThreadManForUserModule.hleKernelNotifyCallback(SceKernelThreadInfo.THREAD_CALLBACK_UMD, PSP_UMD_PRESENT | PSP_UMD_READY);
+	        } else {
+	        	Modules.ThreadManForUserModule.hleKernelNotifyCallback(SceKernelThreadInfo.THREAD_CALLBACK_UMD, PSP_UMD_NOT_PRESENT | PSP_UMD_NOT_READY);
+	        }
+        	thread.doCallbacks = false;
+        }
         checkWaitingThreads();
     }
 
@@ -262,10 +279,9 @@ public class sceUmdUser implements HLEModule, HLEStartModule {
         } else {
             SceKernelThreadInfo currentThread = threadMan.getCurrentThread();
             // Wait on a specific umdStat.
-            currentThread.wait.waitingOnUmd = true;
             currentThread.wait.wantedUmdStat = wantedStat;
             waitingThreads.add(currentThread);
-            threadMan.hleKernelThreadEnterWaitState(currentThread, SceKernelThreadInfo.PSP_WAIT_EVENTFLAG, -1, null, timeout, !doTimeout, doCallbacks);
+            threadMan.hleKernelThreadEnterWaitState(currentThread, JPCSP_WAIT_UMD, -1, null, timeout, !doTimeout, doCallbacks);
         }
         threadMan.hleRescheduleCurrentThread(doCallbacks);
     }
@@ -328,14 +344,12 @@ public class sceUmdUser implements HLEModule, HLEStartModule {
 
         for (ListIterator<SceKernelThreadInfo> lit = waitingThreads.listIterator(); lit.hasNext();) {
             SceKernelThreadInfo waitingThread = lit.next();
-            if (!waitingThread.wait.waitingOnUmd || waitingThread.status != SceKernelThreadInfo.PSP_THREAD_WAITING) {
+            if (!waitingThread.isWaiting() || waitingThread.waitType != JPCSP_WAIT_UMD) {
                 log.error("sceUmdCancelWaitDriveStat thread " + Integer.toHexString(waitingThread.uid)
                         + " '" + waitingThread.name + "' not waiting on umd");
             } else {
                 log.debug("sceUmdCancelWaitDriveStat waking thread " + Integer.toHexString(waitingThread.uid)
                         + " '" + waitingThread.name + "'");
-                // Untrack.
-                waitingThread.wait.waitingOnUmd = false;
                 lit.remove();
                 // Return WAIT_CANCELLED.
                 waitingThread.cpuContext.gpr[2] = ERROR_KERNEL_WAIT_CANCELLED;
@@ -406,7 +420,6 @@ public class sceUmdUser implements HLEModule, HLEStartModule {
         }
         ThreadManForUser threadMan = Modules.ThreadManForUserModule;
         if (threadMan.hleKernelRegisterCallback(SceKernelThreadInfo.THREAD_CALLBACK_UMD, uid)) {
-            Modules.ThreadManForUserModule.hleKernelNotifyCallback(SceKernelThreadInfo.THREAD_CALLBACK_UMD, getUmdStat());
             cpu.gpr[2] = 0;
         } else {
             cpu.gpr[2] = -1;

src/jpcsp/HLE/kernel/managers/EventFlagManager.java
@@ -63,11 +63,9 @@ public class EventFlagManager {
         eventFlagWaitStateChecker = new EventFlagWaitStateChecker();
     }
 
-    /** Don't call this unless thread.wait.waitingOnEventFlag == true
+    /** Don't call this unless thread.waitType == PSP_WAIT_EVENTFLAG
      * @return true if the thread was waiting on a valid event flag */
     private boolean removeWaitingThread(SceKernelThreadInfo thread) {
-        // Untrack
-        thread.wait.waitingOnEventFlag = false;
         // Update numWaitThreads
         SceKernelEventFlagInfo event = eventMap.get(thread.wait.EventFlag_id);
         if (event != null) {
@@ -107,7 +105,7 @@ public class EventFlagManager {
     }
 
     public void onThreadDeleted(SceKernelThreadInfo thread) {
-        if (thread.wait.waitingOnEventFlag) {
+        if (thread.waitType == PSP_WAIT_EVENTFLAG) {
             // decrement numWaitThreads
             removeWaitingThread(thread);
         }
@@ -120,9 +118,7 @@ public class EventFlagManager {
         for (Iterator<SceKernelThreadInfo> it = threadMan.iterator(); it.hasNext();) {
             SceKernelThreadInfo thread = it.next();
             if (thread.waitType == PSP_WAIT_EVENTFLAG &&
-                    thread.wait.waitingOnEventFlag &&
                     thread.wait.EventFlag_id == evid) {
-                thread.wait.waitingOnEventFlag = false;
                 thread.cpuContext.gpr[2] = result;
                 threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
                 reschedule = true;
@@ -149,14 +145,12 @@ public class EventFlagManager {
         for (Iterator<SceKernelThreadInfo> it = threadMan.iterator(); it.hasNext();) {
             SceKernelThreadInfo thread = it.next();
             if (thread.waitType == PSP_WAIT_EVENTFLAG &&
-                    thread.wait.waitingOnEventFlag &&
                     thread.wait.EventFlag_id == event.uid) {
                 if (checkEventFlag(event, thread.wait.EventFlag_bits, thread.wait.EventFlag_wait, thread.wait.EventFlag_outBits_addr)) {
                     if (log.isDebugEnabled()) {
                         log.debug("onEventFlagModified waking thread 0x" + Integer.toHexString(thread.uid) + " name:'" + thread.name + "'");
                     }
                     event.numWaitThreads--;
-                    thread.wait.waitingOnEventFlag = false;
                     thread.cpuContext.gpr[2] = 0;
                     threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
                     reschedule = true;
@@ -301,7 +295,6 @@ public class EventFlagManager {
                 event.numWaitThreads++;
                 SceKernelThreadInfo currentThread = threadMan.getCurrentThread();
                 // Wait on a specific event flag
-                currentThread.wait.waitingOnEventFlag = true;
                 currentThread.wait.EventFlag_id = uid;
                 currentThread.wait.EventFlag_bits = bits;
                 currentThread.wait.EventFlag_wait = wait;

src/jpcsp/HLE/kernel/managers/FplManager.java
@@ -67,8 +67,6 @@ public class FplManager {
     }
 
     private boolean removeWaitingThread(SceKernelThreadInfo thread) {
-        // Untrack
-        thread.wait.waitingOnEventFlag = false;
         // Update numWaitThreads
         SceKernelFplInfo fpl = fplMap.get(thread.wait.Fpl_id);
         if (fpl != null) {
@@ -107,7 +105,7 @@ public class FplManager {
     }
 
     public void onThreadDeleted(SceKernelThreadInfo thread) {
-        if (thread.wait.waitingOnFpl) {
+        if (thread.waitType == PSP_WAIT_FPL) {
             removeWaitingThread(thread);
         }
     }
@@ -119,9 +117,7 @@ public class FplManager {
         for (Iterator<SceKernelThreadInfo> it = threadMan.iterator(); it.hasNext();) {
             SceKernelThreadInfo thread = it.next();
             if (thread.waitType == PSP_WAIT_FPL &&
-                    thread.wait.waitingOnFpl &&
                     thread.wait.Fpl_id == fid) {
-                thread.wait.waitingOnFpl = false;
                 thread.cpuContext.gpr[2] = result;
                 threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
                 reschedule = true;
@@ -149,13 +145,11 @@ public class FplManager {
             for (Iterator<SceKernelThreadInfo> it = threadMan.iterator(); it.hasNext();) {
                 SceKernelThreadInfo thread = it.next();
                 if (thread.waitType == PSP_WAIT_FPL &&
-                        thread.wait.waitingOnFpl &&
                         thread.wait.Fpl_id == info.uid) {
                     if (log.isDebugEnabled()) {
                         log.debug(String.format("onFplFree waking thread %s", thread.toString()));
                     }
                     info.numWaitThreads--;
-                    thread.wait.waitingOnFpl = false;
                     thread.cpuContext.gpr[2] = 0;
                     threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
                     reschedule = true;
@@ -165,13 +159,11 @@ public class FplManager {
             for (Iterator<SceKernelThreadInfo> it = threadMan.iteratorByPriority(); it.hasNext();) {
                 SceKernelThreadInfo thread = it.next();
                 if (thread.waitType == PSP_WAIT_FPL &&
-                        thread.wait.waitingOnFpl &&
                         thread.wait.Fpl_id == info.uid) {
                     if (log.isDebugEnabled()) {
                         log.debug(String.format("onFplFree waking thread %s", thread.toString()));
                     }
                     info.numWaitThreads--;
-                    thread.wait.waitingOnFpl = false;
                     thread.cpuContext.gpr[2] = 0;
                     threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
                     reschedule = true;
@@ -297,7 +289,6 @@ public class FplManager {
                     fpl.numWaitThreads++;
                     // Go to wait state
                     SceKernelThreadInfo currentThread = threadMan.getCurrentThread();
-                    currentThread.wait.waitingOnFpl = true;
                     currentThread.wait.Fpl_id = uid;
                     currentThread.wait.Fpl_dataAddr = data_addr;
                     threadMan.hleKernelThreadEnterWaitState(PSP_WAIT_FPL, uid, fplWaitStateChecker, timeout_addr, doCallbacks);

src/jpcsp/HLE/kernel/managers/LwMutexManager.java
@@ -61,8 +61,6 @@ public class LwMutexManager {
     }
 
     private boolean removeWaitingThread(SceKernelThreadInfo thread) {
-        // Untrack
-        thread.wait.waitingOnLwMutex = false;
         // Update numWaitThreads
         SceKernelLwMutexInfo info = lwMutexMap.get(thread.wait.LwMutex_id);
         if (info != null) {
@@ -101,7 +99,7 @@ public class LwMutexManager {
     }
 
     public void onThreadDeleted(SceKernelThreadInfo thread) {
-        if (thread.wait.waitingOnMutex) {
+        if (thread.waitType == PSP_WAIT_LWMUTEX) {
             // decrement numWaitThreads
             removeWaitingThread(thread);
         }
@@ -114,9 +112,7 @@ public class LwMutexManager {
         for (Iterator<SceKernelThreadInfo> it = threadMan.iterator(); it.hasNext();) {
             SceKernelThreadInfo thread = it.next();
             if (thread.waitType == PSP_WAIT_LWMUTEX &&
-                    thread.wait.waitingOnLwMutex &&
                     thread.wait.LwMutex_id == lwmid) {
-                thread.wait.waitingOnLwMutex = false;
                 thread.cpuContext.gpr[2] = ERROR_KERNEL_WAIT_DELETE;
                 threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
                 reschedule = true;
@@ -136,15 +132,12 @@ public class LwMutexManager {
             for (Iterator<SceKernelThreadInfo> it = Modules.ThreadManForUserModule.iterator(); it.hasNext();) {
                 SceKernelThreadInfo thread = it.next();
                 if (thread.waitType == PSP_WAIT_LWMUTEX &&
-                        thread.wait.waitingOnLwMutex &&
                         thread.wait.LwMutex_id == info.uid &&
                         tryLockLwMutex(info, thread.wait.LwMutex_count, thread)) {
                     // New thread is taking control of LwMutex.
                     info.threadid = thread.uid;
                     // Update numWaitThreads
                     info.numWaitThreads--;
-                    // Untrack
-                    thread.wait.waitingOnLwMutex = false;
                     // Return success or failure
                     thread.cpuContext.gpr[2] = 0;
                     // Wakeup
@@ -156,15 +149,12 @@ public class LwMutexManager {
             for (Iterator<SceKernelThreadInfo> it = Modules.ThreadManForUserModule.iteratorByPriority(); it.hasNext();) {
                 SceKernelThreadInfo thread = it.next();
                 if (thread.waitType == PSP_WAIT_LWMUTEX &&
-                        thread.wait.waitingOnLwMutex &&
                         thread.wait.LwMutex_id == info.uid &&
                         tryLockLwMutex(info, thread.wait.LwMutex_count, thread)) {
                     // New thread is taking control of LwMutex.
                     info.threadid = thread.uid;
                     // Update numWaitThreads
                     info.numWaitThreads--;
-                    // Untrack
-                    thread.wait.waitingOnLwMutex = false;
                     // Return success or failure
                     thread.cpuContext.gpr[2] = 0;
                     // Wakeup
@@ -216,7 +206,6 @@ public class LwMutexManager {
                     // Failed, but it's ok, just wait a little
                     info.numWaitThreads++;
                     // Wait on a specific lwmutex
-                    currentThread.wait.waitingOnLwMutex = true;
                     currentThread.wait.LwMutex_id = uid;
                     currentThread.wait.LwMutex_count = count;
                     threadMan.hleKernelThreadEnterWaitState(PSP_WAIT_LWMUTEX, uid, lwMutexWaitStateChecker, timeout_addr, doCallbacks);

src/jpcsp/HLE/kernel/managers/MbxManager.java
@@ -61,7 +61,6 @@ public class MbxManager {
     }
 
     private boolean removeWaitingThread(SceKernelThreadInfo thread) {
-        thread.wait.waitingOnMbxReceive = false;
         SceKernelMbxInfo info = mbxMap.get(thread.wait.Mbx_id);
         if (info != null) {
             info.numWaitThreads--;
@@ -96,7 +95,9 @@ public class MbxManager {
     }
 
     public void onThreadDeleted(SceKernelThreadInfo thread) {
-        removeWaitingThread(thread);
+    	if (thread.waitType == PSP_WAIT_MBX) {
+    		removeWaitingThread(thread);
+    	}
     }
 
     private void onMbxDeletedCancelled(int mbxid, int result) {
@@ -106,9 +107,7 @@ public class MbxManager {
         for (Iterator<SceKernelThreadInfo> it = threadMan.iterator(); it.hasNext();) {
             SceKernelThreadInfo thread = it.next();
             if (thread.waitType == PSP_WAIT_MBX &&
-                    thread.wait.waitingOnMbxReceive &&
                     thread.wait.Mbx_id == mbxid) {
-                thread.wait.waitingOnMbxReceive = false;
                 thread.cpuContext.gpr[2] = result;
                 threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
                 reschedule = true;
@@ -136,7 +135,6 @@ public class MbxManager {
             for (Iterator<SceKernelThreadInfo> it = threadMan.iterator(); it.hasNext();) {
                 SceKernelThreadInfo thread = it.next();
                 if (thread.waitType == PSP_WAIT_MBX &&
-                        thread.wait.waitingOnMbxReceive &&
                         thread.wait.Mbx_id == info.uid &&
                         info.hasMessage()) {
                     if (log.isDebugEnabled()) {
@@ -146,7 +144,6 @@ public class MbxManager {
                     int msgAddr = info.removeMsg(mem);
                     mem.write32(thread.wait.Mbx_resultAddr, msgAddr);
                     info.numWaitThreads--;
-                    thread.wait.waitingOnMbxReceive = false;
                     thread.cpuContext.gpr[2] = 0;
                     threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
                     reschedule = true;
@@ -156,7 +153,6 @@ public class MbxManager {
             for (Iterator<SceKernelThreadInfo> it = threadMan.iteratorByPriority(); it.hasNext();) {
                 SceKernelThreadInfo thread = it.next();
                 if (thread.waitType == PSP_WAIT_MBX &&
-                        thread.wait.waitingOnMbxReceive &&
                         thread.wait.Mbx_id == info.uid &&
                         info.hasMessage()) {
                     if (log.isDebugEnabled()) {
@@ -166,7 +162,6 @@ public class MbxManager {
                     int msgAddr = info.removeMsg(mem);
                     mem.write32(thread.wait.Mbx_resultAddr, msgAddr);
                     info.numWaitThreads--;
-                    thread.wait.waitingOnMbxReceive = false;
                     thread.cpuContext.gpr[2] = 0;
                     threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
                     reschedule = true;
@@ -278,7 +273,6 @@ public class MbxManager {
                     }
                     info.numWaitThreads++;
                     SceKernelThreadInfo currentThread = threadMan.getCurrentThread();
-                    currentThread.wait.waitingOnMbxReceive = true;
                     currentThread.wait.Mbx_id = uid;
                     currentThread.wait.Mbx_resultAddr = addr_msg_addr;
                     threadMan.hleKernelThreadEnterWaitState(PSP_WAIT_MBX, uid, mbxWaitStateChecker, timeout_addr, doCallbacks);

src/jpcsp/HLE/kernel/managers/MsgPipeManager.java
@@ -73,30 +73,23 @@ public class MsgPipeManager {
     }
 
     private boolean removeWaitingThread(SceKernelThreadInfo thread) {
-        if (thread.wait.waitingOnMsgPipeSend) {
-            // Untrack
-            thread.wait.waitingOnMsgPipeSend = false;
+        if (thread.waitType == PSP_WAIT_MSGPIPE) {
             // Update numSendWaitThreads.
             SceKernelMppInfo info = msgMap.get(thread.wait.MsgPipe_id);
             if (info != null) {
-                info.numSendWaitThreads--;
-                if (info.numSendWaitThreads < 0) {
-                    log.warn("removing waiting thread " + Integer.toHexString(thread.uid) + ", MsgPipe " + Integer.toHexString(info.uid) + " numSendWaitThreads underflowed");
-                    info.numSendWaitThreads = 0;
-                }
-                return true;
-            }
-        } else if (thread.wait.waitingOnMsgPipeSend) {
-            // Untrack
-            thread.wait.waitingOnMsgPipeReceive = false;
-            // Update numReceiveWaitThreads.
-            SceKernelMppInfo info = msgMap.get(thread.wait.MsgPipe_id);
-            if (info != null) {
-                info.numReceiveWaitThreads--;
-                if (info.numReceiveWaitThreads < 0) {
-                    log.warn("removing waiting thread " + Integer.toHexString(thread.uid) + ", MsgPipe " + Integer.toHexString(info.uid) + " numReceiveWaitThreads underflowed");
-                    info.numReceiveWaitThreads = 0;
-                }
+            	if (thread.wait.MsgPipe_isSend) {
+	                info.numSendWaitThreads--;
+	                if (info.numSendWaitThreads < 0) {
+	                    log.warn("removing waiting thread " + Integer.toHexString(thread.uid) + ", MsgPipe " + Integer.toHexString(info.uid) + " numSendWaitThreads underflowed");
+	                    info.numSendWaitThreads = 0;
+	                }
+            	} else {
+                    info.numReceiveWaitThreads--;
+                    if (info.numReceiveWaitThreads < 0) {
+                        log.warn("removing waiting thread " + Integer.toHexString(thread.uid) + ", MsgPipe " + Integer.toHexString(info.uid) + " numReceiveWaitThreads underflowed");
+                        info.numReceiveWaitThreads = 0;
+                    }
+            	}
                 return true;
             }
         }
@@ -138,16 +131,7 @@ public class MsgPipeManager {
         for (Iterator<SceKernelThreadInfo> it = threadMan.iterator(); it.hasNext();) {
             SceKernelThreadInfo thread = it.next();
             if (thread.waitType == PSP_WAIT_MSGPIPE &&
-                    thread.wait.waitingOnMsgPipeSend &&
                     thread.wait.MsgPipe_id == msgpid) {
-                thread.wait.waitingOnMsgPipeSend = false;
-                thread.cpuContext.gpr[2] = result;
-                threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
-                reschedule = true;
-            } else if (thread.waitType == PSP_WAIT_MSGPIPE &&
-                    thread.wait.waitingOnMsgPipeReceive &&
-                    thread.wait.MsgPipe_id == msgpid) {
-                thread.wait.waitingOnMsgPipeReceive = false;
                 thread.cpuContext.gpr[2] = result;
                 threadMan.hleChangeThreadState(thread, PSP_THREAD_READY);
                 reschedule = true;
@@ -177,11 +161,9 @@ public class MsgPipeManager {
             for (Iterator<SceKernelThreadInfo> it = threadMan.iterator(); it.hasNext();) {
                 SceKernelThreadInfo thread = it.next();
                 if (thread.waitType == PSP_WAIT_MSGPIPE &&
-                        thread.wait.waitingOnMsgPipeSend &&
+                        thread.wait.MsgPipe_isSend &&
                         thread.wait.MsgPipe_id == info.uid &&
                         trySendMsgPipe(mem, info, thread.wait.MsgPipe_address, thread.wait.MsgPipe_size, thread.wait.MsgPipe_waitMode, thread.wait.MsgPipe_resultSize_addr)) {
-                    // Untrack.
-                    thread.wait.waitingOnMsgPipeSend = false;
                     // Adjust waiting threads.
                     info.numSendWaitThreads--;
                     // Return success.
@@ -195,11 +177,9 @@ public class MsgPipeManager {
             for (Iterator<SceKernelThreadInfo> it = threadMan.iteratorByPriority(); it.hasNext();) {
                 SceKernelThreadInfo thread = it.next();
                 if (thread.waitType == PSP_WAIT_MSGPIPE &&
-                        thread.wait.waitingOnMsgPipeSend &&
+                        thread.wait.MsgPipe_isSend &&
                         thread.wait.MsgPipe_id == info.uid &&
                         trySendMsgPipe(mem, info, thread.wait.MsgPipe_address, thread.wait.MsgPipe_size, thread.wait.MsgPipe_waitMode, thread.wait.MsgPipe_resultSize_addr)) {
-                    // Untrack.
-                    thread.wait.waitingOnMsgPipeSend = false;
                     // Adjust waiting threads.
                     info.numSendWaitThreads--;
                     // Return success.
@@ -225,11 +205,9 @@ public class MsgPipeManager {
             for (Iterator<SceKernelThreadInfo> it = threadMan.iterator(); it.hasNext();) {
                 SceKernelThreadInfo thread = it.next();
                 if (thread.waitType == PSP_WAIT_MSGPIPE &&
-                        thread.wait.waitingOnMsgPipeReceive &&
+                        !thread.wait.MsgPipe_isSend &&
                         thread.wait.MsgPipe_id == info.uid &&
                         tryReceiveMsgPipe(mem, info, thread.wait.MsgPipe_address, thread.wait.MsgPipe_size, thread.wait.MsgPipe_waitMode, thread.wait.MsgPipe_resultSize_addr)) {
-                    // Untrack.
-                    thread.wait.waitingOnMsgPipeReceive = false;
                     // Adjust waiting threads.
                     info.numReceiveWaitThreads--;
                     // Return success.
@@ -243,11 +221,9 @@ public class MsgPipeManager {
             for (Iterator<SceKernelThreadInfo> it = threadMan.iteratorByPriority(); it.hasNext();) {
                 SceKernelThreadInfo thread = it.next();
                 if (thread.waitType == PSP_WAIT_MSGPIPE &&
-                        thread.wait.waitingOnMsgPipeReceive &&
+                        !thread.wait.MsgPipe_isSend &&
                         thread.wait.MsgPipe_id == info.uid &&
                         tryReceiveMsgPipe(mem, info, thread.wait.MsgPipe_address, thread.wait.MsgPipe_size, thread.wait.MsgPipe_waitMode, thread.wait.MsgPipe_resultSize_addr)) {
-                    // Untrack.
-                    thread.wait.waitingOnMsgPipeReceive = false;
                     // Adjust waiting threads.
                     info.numReceiveWaitThreads--;
                     // Return success.
@@ -397,7 +373,7 @@ public class MsgPipeManager {
                     info.numSendWaitThreads++;
                     SceKernelThreadInfo currentThread = threadMan.getCurrentThread();
                     // Wait on a specific MsgPipe.
-                    currentThread.wait.waitingOnMsgPipeSend = true;
+                    currentThread.wait.MsgPipe_isSend = true;
                     currentThread.wait.MsgPipe_id = uid;
                     currentThread.wait.MsgPipe_address = msg_addr;
                     currentThread.wait.MsgPipe_size = size;
@@ -467,7 +443,7 @@ public class MsgPipeManager {
                     info.numReceiveWaitThreads++;
                     SceKernelThreadInfo currentThread = threadMan.getCurrentThread();
                     // Wait on a specific MsgPipe.
-                    currentThread.wait.waitingOnMsgPipeReceive = true;
+                    currentThread.wait.MsgPipe_isSend = false;
                     currentThread.wait.MsgPipe_id = uid;
                     currentThread.wait.MsgPipe_address = msg_addr;
                     currentThread.wait.MsgPipe_size = size;

commit 09ab05a8c214fc339e2d606c1bac8a396e871049

Fixed sceUmdActivate (was broken by r2209 for some games).

src/jpcsp/HLE/modules150/sceUmdUser.java
@@ -192,7 +192,7 @@ public class sceUmdUser extends HLEModule {
         // The callback will be executed at the next sceXXXXCB() syscall.
         int notifyArg;
         if (iso != null) {
-        	notifyArg = PSP_UMD_PRESENT | PSP_UMD_READABLE;
+        	notifyArg = PSP_UMD_PRESENT | PSP_UMD_READY | PSP_UMD_READABLE;
         } else {
         	notifyArg = PSP_UMD_NOT_PRESENT | PSP_UMD_NOT_READY;
         }