2156ca8a5958c5e649f0500740dd13a902c81876
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2012-08-09 16:38:24 +0000

    Trying to support Win7 systems where no "REG" command is available. Only implemented in the 32-bit windows start script as a test.

--- trunk/start-windows-x86.bat
+++ trunk/start-windows-x86.bat
@@ -9,19 +9,31 @@ goto JAVA
 set key=HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Runtime Environment
 
 :JAVA
+set JAVA_CMD=java.exe
+
+rem Checking if the "reg" command is available
+reg /? >NUL
+if ERRORLEVEL 1 goto RUN
+
 set JAVA_VERSION=
 set JAVA_HOME=
 for /f "tokens=3* skip=2" %%a in ('reg query "%key%" /v CurrentVersion') do set JAVA_VERSION=%%a
 for /f "tokens=2* skip=2" %%a in ('reg query "%key%\%JAVA_VERSION%" /v JavaHome') do set JAVA_HOME=%%b
 
-if not exist "%JAVA_HOME%\bin\java.exe" goto JAVAMISSING
+set JAVA_CMD=%JAVA_HOME%\bin\java.exe
+if not exist "%JAVA_CMD%" goto JAVAMISSING
+
+:RUN
 echo Running Jpcsp 32bit...
-"%JAVA_HOME%\bin\java" -Xmx1024m -Xss2m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/windows-x86 -jar bin/jpcsp.jar %*
+"%JAVA_CMD%" -Xmx1024m -Xss2m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/windows-x86 -jar bin/jpcsp.jar %*
+if ERRORLEVEL 1 goto PAUSE
 goto END
 
 :JAVAMISSING
 echo The required version of Java has not been installed or isn't recognized.
 echo Go to http://java.sun.com to install the 32bit Java JRE.
+
+:PAUSE
 pause
 
 :END
