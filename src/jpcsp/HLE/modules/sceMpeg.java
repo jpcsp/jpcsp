4f86c126a57d4d3423545b026042e8c597725c3b
gid15@web.de <gid15@users.noreply.github.com>
2012-08-13 15:30:29 +0000

    Small improvement in sceKernelSysClock2USec: allow NULL parameters. Enabled sceMpeg functions from modules600. New draft functions for sceMp4.

--- src/jpcsp/HLE/modules/sceMpeg.java
+++ src/jpcsp/HLE/modules/sceMpeg.java
@@ -18,7 +18,7 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
 
 package jpcsp.HLE.modules;
 
-public class sceMpeg extends jpcsp.HLE.modules352.sceMpeg {
+public class sceMpeg extends jpcsp.HLE.modules600.sceMpeg {
 	@Override
 	public final String getName() { return "sceMpeg"; }
 }
