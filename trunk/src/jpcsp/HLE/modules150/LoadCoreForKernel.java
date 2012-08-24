b613475b75d5665f120b116be296a48cc4bb203b
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2012-08-24 16:37:35 +0000

    More code clean-up for unimplemented syscall's.

--- trunk/src/jpcsp/HLE/modules150/LoadCoreForKernel.java
+++ trunk/src/jpcsp/HLE/modules150/LoadCoreForKernel.java
@@ -18,6 +18,7 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
 package jpcsp.HLE.modules150;
 
 import jpcsp.HLE.HLEFunction;
+import jpcsp.HLE.HLEUnimplemented;
 import jpcsp.HLE.PspString;
 import jpcsp.HLE.TPointer;
 import jpcsp.HLE.Modules;
@@ -33,52 +34,45 @@ public class LoadCoreForKernel extends HLEModule {
 	@Override
 	public String getName() { return "LoadCoreForKernel"; }
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0xACE23476, version = 150)
 	public int sceKernelCheckPspConfig() {
-		log.warn("Unimplemented sceKernelCheckPspConfig");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x7BE1421C, version = 150)
 	public int sceKernelCheckExecFile() {
-		log.warn("Unimplemented sceKernelCheckExecFile");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0xBF983EF2, version = 150)
 	public int sceKernelProbeExecutableObject() {
-		log.warn("Unimplemented sceKernelProbeExecutableObject");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x7068E6BA, version = 150)
 	public int sceKernelLoadExecutableObject() {
-		log.warn("Unimplemented sceKernelLoadExecutableObject");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0xB4D6FECC, version = 150)
 	public int sceKernelApplyElfRelSection() {
-		log.warn("Unimplemented sceKernelApplyElfRelSection");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x54AB2675, version = 150)
 	public int sceKernelApplyPspRelSection() {
-		log.warn("Unimplemented sceKernelApplyPspRelSection");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x2952F5AC, version = 150)
 	public int sceKernelDcacheWBinvAll() {
-		log.warn("Unimplemented sceKernelDcacheWBinvAll");
-
 		return 0;
 	}
 
@@ -89,115 +83,99 @@ public class LoadCoreForKernel extends HLEModule {
         return 0;
     }
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x99A695F0, version = 150)
 	public int sceKernelRegisterLibrary() {
-		log.warn("Unimplemented sceKernelRegisterLibrary");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x5873A31F, version = 150)
 	public int sceKernelRegisterLibraryForUser() {
-		log.warn("Unimplemented sceKernelRegisterLibraryForUser");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x0B464512, version = 150)
 	public int sceKernelReleaseLibrary() {
-		log.warn("Unimplemented sceKernelReleaseLibrary");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x9BAF90F6, version = 150)
 	public int sceKernelCanReleaseLibrary() {
-		log.warn("Unimplemented sceKernelCanReleaseLibrary");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x0E760DBA, version = 150)
 	public int sceKernelLinkLibraryEntries() {
-		log.warn("Unimplemented sceKernelLinkLibraryEntries");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x0DE1F600, version = 150)
 	public int sceKernelLinkLibraryEntriesForUser() {
-		log.warn("Unimplemented sceKernelLinkLibraryEntriesForUser");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0xDA1B09AA, version = 150)
 	public int sceKernelUnLinkLibraryEntries() {
-		log.warn("Unimplemented sceKernelUnLinkLibraryEntries");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0xC99DD47A, version = 150)
 	public int sceKernelQueryLoadCoreCB() {
-		log.warn("Unimplemented sceKernelQueryLoadCoreCB");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x616FCCCD, version = 150)
 	public int sceKernelSetBootCallbackLevel() {
-		log.warn("Unimplemented sceKernelSetBootCallbackLevel");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x52A86C21, version = 150)
 	public int sceKernelGetModuleFromUID() {
-		log.warn("Unimplemented sceKernelGetModuleFromUID");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0xCD0F3BAC, version = 150)
 	public int sceKernelCreateModule() {
-		log.warn("Unimplemented sceKernelCreateModule");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x6B2371C2, version = 150)
 	public int sceKernelDeleteModule() {
-		log.warn("Unimplemented sceKernelDeleteModule");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x8D8A8ACE, version = 150)
 	public int sceKernelAssignModule() {
-		log.warn("Unimplemented sceKernelAssignModule");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0xAFF947D4, version = 150)
 	public int sceKernelCreateAssignModule() {
-		log.warn("Unimplemented sceKernelCreateAssignModule");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0xAE7C6E76, version = 150)
 	public int sceKernelRegisterModule() {
-		log.warn("Unimplemented sceKernelRegisterModule");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x74CF001A, version = 150)
 	public int sceKernelReleaseModule() {
-		log.warn("Unimplemented sceKernelReleaseModule");
-
 		return 0;
 	}
 
@@ -266,17 +244,15 @@ public class LoadCoreForKernel extends HLEModule {
         return module.address;
     }
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x929B5C69, version = 150)
 	public int sceKernelGetModuleListWithAlloc() {
-		log.warn("Unimplemented sceKernelGetModuleListWithAlloc");
-
 		return 0;
 	}
 
+    @HLEUnimplemented
 	@HLEFunction(nid = 0x05D915DB, version = 150)
 	public int sceKernelGetModuleIdListForKernel() {
-		log.warn("Unimplemented sceKernelGetModuleIdListForKernel");
-
 		return 0;
 	}
 }
