5e9c1b6b25e91328774a3a74594116ec28972cbb
gid15@web.de <gid15@users.noreply.github.com>
2012-09-29 18:14:16 +0000

    Code clean-up: __sceSasSetVoiceATRAC3 is no longer logged as "Unimplemented"

--- src/jpcsp/HLE/modules600/sceSasCore.java
+++ src/jpcsp/HLE/modules600/sceSasCore.java
@@ -16,21 +16,28 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
 package jpcsp.HLE.modules600;
 
+import org.apache.log4j.Logger;
+
 import jpcsp.Memory;
 import jpcsp.HLE.HLEFunction;
+import jpcsp.HLE.HLELogging;
+import jpcsp.HLE.HLEUnimplemented;
 import jpcsp.HLE.Modules;
 import jpcsp.HLE.modules150.sceAtrac3plus.AtracID;
 
+@HLELogging
 public class sceSasCore extends jpcsp.HLE.modules500.sceSasCore {
+	public static Logger log = jpcsp.HLE.modules150.sceSasCore.log;
+
 	protected void setSasCoreAtrac3Context(int sasCore, int voice, int atrac3Context) {
 		Memory mem = Memory.getInstance();
         mem.write32(sasCore + 56 * voice + 20, atrac3Context);
 	}
 
+	@HLELogging(level="info")
 	@HLEFunction(nid = 0x4AA9EAD6, version = 600, checkInsideInterrupt = true)
     public int __sceSasSetVoiceATRAC3(int sasCore, int voice, int atrac3Context) {
 		// atrac3Context is the value returned by _sceAtracGetContextAddress
-        log.warn(String.format("Unimplemented __sceSasSetVoiceATRAC3 sasCore=0x%08X, voice=%d, atrac3Context=0x%08X", sasCore, voice, atrac3Context));
 
         checkSasAndVoiceHandlesGood(sasCore, voice);
 
@@ -45,10 +52,9 @@ public class sceSasCore extends jpcsp.HLE.modules500.sceSasCore {
         return 0;
     }
 
+	@HLEUnimplemented
     @HLEFunction(nid = 0x7497EA85, version = 600, checkInsideInterrupt = true)
     public int __sceSasConcatenateATRAC3(int sasCore, int voice, int atrac3Addr, int unkown) {
-        log.warn(String.format("Unimplemented __sceSasConcatenateATRAC3 sasCore=0x%08X, voice=%d, atrac3Addr=0x%08X, unknown=0x%08X", sasCore, voice, atrac3Addr, unkown));
-
         checkSasAndVoiceHandlesGood(sasCore, voice);
 
         return 0;
@@ -56,8 +62,6 @@ public class sceSasCore extends jpcsp.HLE.modules500.sceSasCore {
 
     @HLEFunction(nid = 0xF6107F00, version = 600, checkInsideInterrupt = true)
     public int __sceSasUnsetATRAC3(int sasCore, int voice) {
-        log.warn(String.format("Unimplemented __sceSasUnsetATRAC3 sasCore=0x%08X, voice=%d", sasCore, voice));
-
         checkSasAndVoiceHandlesGood(sasCore, voice);
 
         voices[voice].setAtracId(null);
