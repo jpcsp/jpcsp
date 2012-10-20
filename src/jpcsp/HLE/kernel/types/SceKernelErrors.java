b450130e2924c980721245fb749ef2f1a6c619c7
gid15@web.de <gid15@users.noreply.github.com>
2012-10-20 20:33:47 +0000

    __sceSasSetVoicePCM: fixed value check for PCM size parameter.

--- src/jpcsp/HLE/kernel/types/SceKernelErrors.java
+++ src/jpcsp/HLE/kernel/types/SceKernelErrors.java
@@ -382,6 +382,7 @@ public class SceKernelErrors {
     public final static int ERROR_SAS_INVALID_ADSR_CURVE_MODE                   = 0x80420013;
     public final static int ERROR_SAS_INVALID_PARAMETER                         = 0x80420014;
     public final static int ERROR_SAS_VOICE_PAUSED                              = 0x80420016;
+    public final static int ERROR_SAS_INVALID_SIZE                              = 0x8042001A;
     public final static int ERROR_SAS_BUSY                                      = 0x80420030;
     public final static int ERROR_SAS_NOT_INIT                                  = 0x80420100;
 
