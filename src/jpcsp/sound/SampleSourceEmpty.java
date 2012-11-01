9d1493d8e38d4eaf22a89a8cd4ba6bc7511d3bf2
gid15@web.de <gid15@users.noreply.github.com>
2012-11-01 06:01:14 +0000

    Avoid NullPointerException when no sceSas voice has been defined.

---
+++ src/jpcsp/sound/SampleSourceEmpty.java
@@ -0,0 +1,42 @@
+/*
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.sound;
+
+/**
+ * @author gid15
+ *
+ */
+public class SampleSourceEmpty implements ISampleSource {
+	@Override
+	public int getNextSample() {
+		return 0;
+	}
+
+	@Override
+	public void setSampleIndex(int index) {
+	}
+
+	@Override
+	public int getSampleIndex() {
+		return 0;
+	}
+
+	@Override
+	public int getNumberSamples() {
+		return 0;
+	}
+}
