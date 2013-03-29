c00a50a1a1ec217848cefc5afe6f18690b26fd94
gid15@web.de <gid15@users.noreply.github.com>
2013-03-29 15:07:25 +0000

    Improved DecodeAudio.bat to support all 3 decoding tools: - atrac3plus2wav.exe - at3tool.exe - HIMDRender.exe as suggested by several users.

--- lib/windows-amd64/DecodeAudio.bat
+++ lib/windows-amd64/DecodeAudio.bat
@@ -1,7 +1,18 @@
 @echo off
-if exist lib\windows-amd64\at3tool.exe goto at3tool
+if not exist lib\windows-amd64\atrac3plus2wav.exe goto at3tool
+lib\windows-amd64\atrac3plus2wav.exe %1
+if not exist "%1.wav" goto at3tool
+rename %1.wav %2
+goto end
+
+:at3tool
+if not exist lib\windows-amd64\at3tool.exe goto himdrender
+lib\windows-amd64\at3tool.exe -d -repeat 1 %3 %2
+if not exist "%2" goto himdrender
+goto end
+
+:himdrender
 lib\windows-amd64\HIMDRender.exe -e -i %1 -o %2
 goto end
-:at3tool
-lib\windows-amd64\at3tool.exe -d -repeat 0 %3 %2
+
 :end
--- lib/windows-x86/DecodeAudio.bat
+++ lib/windows-x86/DecodeAudio.bat
@@ -1,7 +1,18 @@
 @echo off
-if exist lib\windows-x86\at3tool.exe goto at3tool
+if not exist lib\windows-x86\atrac3plus2wav.exe goto at3tool
+lib\windows-x86\atrac3plus2wav.exe %1
+if not exist "%1.wav" goto at3tool
+rename %1.wav %2
+goto end
+
+:at3tool
+if not exist lib\windows-x86\at3tool.exe goto himdrender
+lib\windows-x86\at3tool.exe -d -repeat 1 %3 %2
+if not exist "%2" goto himdrender
+goto end
+
+:himdrender
 lib\windows-x86\HIMDRender.exe -e -i %1 -o %2
 goto end
-:at3tool
-lib\windows-x86\at3tool.exe -d -repeat 0 %3 %2
+
 :end
