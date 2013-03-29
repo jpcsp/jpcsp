3ec945199863f2e539eb8445bbac8f4bf3ed8a0f
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2013-03-29 11:49:47 +0000

    Added basic support for decoding Mono Atrac3+ using at3tool.exe (not included with Jpcsp distribution). at3tool.exe has to be placed in lib\windows-x86 or lib\windows-amd64

--- trunk/lib/windows-amd64/DecodeAudio.bat
+++ trunk/lib/windows-amd64/DecodeAudio.bat
@@ -1,2 +1,7 @@
 @echo off
+if exist lib\windows-amd64\at3tool.exe goto at3tool
 lib\windows-amd64\HIMDRender.exe -e -i %1 -o %2
+goto end
+:at3tool
+lib\windows-amd64\at3tool.exe -d -repeat 0 %3 %2
+:end
--- trunk/lib/windows-x86/DecodeAudio.bat
+++ trunk/lib/windows-x86/DecodeAudio.bat
@@ -1,2 +1,7 @@
 @echo off
+if exist lib\windows-x86\at3tool.exe goto at3tool
 lib\windows-x86\HIMDRender.exe -e -i %1 -o %2
+goto end
+:at3tool
+lib\windows-x86\at3tool.exe -d -repeat 0 %3 %2
+:end
