5ccee94e7335aeed1a506e78b4be2d952081f9d6
gid15@web.de <gid15@users.noreply.github.com>
2013-03-29 16:56:32 +0000

    Fix for atrac3plus2wav.exe decoder as suggested by serio

--- lib/windows-x86/DecodeAudio.bat
+++ lib/windows-x86/DecodeAudio.bat
@@ -2,7 +2,7 @@
 if not exist lib\windows-x86\atrac3plus2wav.exe goto at3tool
 lib\windows-x86\atrac3plus2wav.exe %1
 if not exist "%1.wav" goto at3tool
-rename %1.wav %2
+move %1.wav %2
 goto end
 
 :at3tool
