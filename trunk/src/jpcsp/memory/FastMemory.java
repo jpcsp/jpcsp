1e6c9b04f6ee4feac484c37a6eec48d0ce9bf345
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2013-04-05 19:57:11 +0000

    Small performance improvements in FastMemory.

--- trunk/src/jpcsp/memory/FastMemory.java
+++ trunk/src/jpcsp/memory/FastMemory.java
@@ -52,6 +52,15 @@ public class FastMemory extends Memory {
 	private static final boolean traceRead  = false;
 	private static final boolean traceWrite = false;
 
+	// Array containing only 0, for fast memset(addr, 0, length);
+	public static final int[] zero = new int[32768];
+
+	private static final int[] memory8Shift = { 0, 8, 16, 24 };
+	private static final int[] memory8Mask = { 0xFFFFFF00, 0xFFFF00FF, 0xFF00FFFF, 0x00FFFFFF };
+	private static final int[] memory16Shift = { 0, 0, 16, 16 };
+	private static final int[] memory16Mask = { 0xFFFF0000, 0xFFFF0000, 0x0000FFFF, 0x0000FFFF };
+	private static final boolean[] isIntAligned = { true, false, false, false };
+
 	@Override
 	public boolean allocate() {
 		// Free previously allocated memory
@@ -72,68 +81,52 @@ public class FastMemory extends Memory {
 
 	@Override
 	public void Initialise() {
+		Arrays.fill(zero, 0);
 		Arrays.fill(all, 0);
 	}
 
 	@Override
 	public int read8(int address) {
-		try {
-			address &= addressMask;
-			int data = all[address >> 2];
-			switch (address & 0x03) {
-				case 1: data >>=  8; break;
-				case 2: data >>= 16; break;
-				case 3: data >>= 24; break;
-			}
+		address &= addressMask;
+		int data = (all[address >> 2] >> memory8Shift[address & 0x03]) & 0xFF;
 
-			if (traceRead) {
-				if (log.isTraceEnabled()) {
-					log.trace("read8(0x" + Integer.toHexString(address).toUpperCase() + ")=0x" + Integer.toHexString(data & 0xFF).toUpperCase());
-				}
+		if (traceRead) {
+			if (log.isTraceEnabled()) {
+				log.trace(String.format("read8(0x%08X)=0x%02X", address, data));
 			}
-	
-			return data & 0xFF;
-		} catch (Exception e) {
-            invalidMemoryAddress(address, "read8", Emulator.EMU_STATUS_MEM_READ);
-			return 0;
 		}
+	
+		return data;
 	}
 
 	@Override
 	public int read16(int address) {
-		try {
-			address &= addressMask;
-			int data = all[address >> 2];
-			if ((address & 0x02) != 0) {
-				data >>= 16;
-			}
+		address &= addressMask;
+		int data = (all[address >> 2] >> memory16Shift[address & 0x02]) & 0xFFFF;
 
-			if (traceRead) {
-				if (log.isTraceEnabled()) {
-					log.trace("read16(0x" + Integer.toHexString(address).toUpperCase() + ")=0x" + Integer.toHexString(data & 0xFFFF).toUpperCase());
-				}
+		if (traceRead) {
+			if (log.isTraceEnabled()) {
+				log.trace(String.format("read16(0x%08X)=0x%04X", address, data));
 			}
-
-			return data & 0xFFFF;
-		} catch (Exception e) {
-            invalidMemoryAddress(address, "read16", Emulator.EMU_STATUS_MEM_READ);
-			return 0;
 		}
+
+		return data;
 	}
 
 	@Override
 	public int read32(int address) {
 		try {
 			address &= addressMask;
+			int data = all[address >> 2];
 
 			if (traceRead) {
 				if (log.isTraceEnabled()) {
-					log.trace("read32(0x" + Integer.toHexString(address).toUpperCase() + ")=0x" + Integer.toHexString(all[address >> 2]).toUpperCase() + " (" + Float.intBitsToFloat(all[address >> 2]) + ")");
+					log.trace(String.format("read32(0x%08X)=0x%08X (%f)", address, data, Float.intBitsToFloat(data)));
 				}
 			}
 
-			return all[address >> 2];
-		} catch (Exception e) {
+			return data;
+		} catch (ArrayIndexOutOfBoundsException e) {
 			if (read32AllowedInvalidAddress(address)) {
 				return 0;
 			}
@@ -145,106 +138,76 @@ public class FastMemory extends Memory {
 
 	@Override
 	public long read64(int address) {
-		try {
-			address &= addressMask;
-			long data = (((long) all[(address >> 2) + 1]) << 32) | (((long) all[address >> 2]) & 0xFFFFFFFFL);
+		address &= addressMask;
+		long data = (((long) all[(address >> 2) + 1]) << 32) | (((long) all[address >> 2]) & 0xFFFFFFFFL);
 
-			if (traceRead) {
-				if (log.isTraceEnabled()) {
-					log.trace("read64(0x" + Integer.toHexString(address).toUpperCase() + ")=0x" + Long.toHexString(data).toUpperCase());
-				}
+		if (traceRead) {
+			if (log.isTraceEnabled()) {
+				log.trace(String.format("read64(0x%08X)=0x%X", address, data));
 			}
-
-			return data;
-		} catch (Exception e) {
-            invalidMemoryAddress(address, "read64", Emulator.EMU_STATUS_MEM_READ);
-			return 0;
 		}
+
+		return data;
 	}
 
 	@Override
 	public void write8(int address, byte data) {
-		try {
-			address &= addressMask;
-			int memData = all[address >> 2];
-			switch (address & 0x03) {
-			case 0: memData = (memData & 0xFFFFFF00) | ((data & 0xFF)      ); break;
-			case 1: memData = (memData & 0xFFFF00FF) | ((data & 0xFF) <<  8); break;
-			case 2: memData = (memData & 0xFF00FFFF) | ((data & 0xFF) << 16); break;
-			case 3: memData = (memData & 0x00FFFFFF) | ((data & 0xFF) << 24); break;
-			}
+		address &= addressMask;
+		int index = address & 0x03;
+		int memData = (all[address >> 2] & memory8Mask[index]) | ((data & 0xFF) << memory8Shift[index]);
 
-			if (traceWrite) {
-				if (log.isTraceEnabled()) {
-					log.trace("write8(0x" + Integer.toHexString(address).toUpperCase() + ", 0x" + Integer.toHexString(data & 0xFF).toUpperCase() + ")");
-				}
+		if (traceWrite) {
+			if (log.isTraceEnabled()) {
+				log.trace(String.format("write8(0x%08X, 0x%02X)", address, data & 0xFF));
 			}
-
-			all[address >> 2] = memData;
-            Modules.sceDisplayModule.write8(address);
-		} catch (Exception e) {
-            invalidMemoryAddress(address, "write8", Emulator.EMU_STATUS_MEM_WRITE);
 		}
+
+		all[address >> 2] = memData;
+        Modules.sceDisplayModule.write8(address);
 	}
 
 	@Override
 	public void write16(int address, short data) {
-		try {
-			address &= addressMask;
-			int memData = all[address >> 2];
-			if ((address & 0x02) == 0) {
-				memData = (memData & 0xFFFF0000) | (data & 0xFFFF);
-			} else {
-				memData = (memData & 0x0000FFFF) | (data << 16);
-			}
+		address &= addressMask;
+		int index = address & 0x02;
+		int memData = (all[address >> 2] & memory16Mask[index]) | ((data & 0xFFFF) << memory16Shift[index]);
 
-			if (traceWrite) {
-				if (log.isTraceEnabled()) {
-					log.trace("write16(0x" + Integer.toHexString(address).toUpperCase() + ", 0x" + Integer.toHexString(data & 0xFFFF).toUpperCase() + ")");
-				}
+		if (traceWrite) {
+			if (log.isTraceEnabled()) {
+				log.trace(String.format("write16(0x%08X, 0x%04X)", address, data & 0xFFFF));
 			}
-
-			all[address >> 2] = memData;
-            Modules.sceDisplayModule.write16(address);
-		} catch (Exception e) {
-            invalidMemoryAddress(address, "write16", Emulator.EMU_STATUS_MEM_WRITE);
 		}
+
+		all[address >> 2] = memData;
+        Modules.sceDisplayModule.write16(address);
 	}
 
 	@Override
 	public void write32(int address, int data) {
-		try {
-			address &= addressMask;
-			all[address >> 2] = data;
+		address &= addressMask;
 
-			if (traceWrite) {
-				if (log.isTraceEnabled()) {
-					log.trace("write32(0x" + Integer.toHexString(address).toUpperCase() + ", 0x" + Integer.toHexString(data).toUpperCase() + " (" + Float.intBitsToFloat(data) + "))");
-				}
+		if (traceWrite) {
+			if (log.isTraceEnabled()) {
+				log.trace(String.format("write32(0x%08X, %08X (%f))", address, data, Float.intBitsToFloat(data)));
 			}
-
-			Modules.sceDisplayModule.write32(address);
-		} catch (Exception e) {
-            invalidMemoryAddress(address, "write32", Emulator.EMU_STATUS_MEM_WRITE);
 		}
+
+		all[address >> 2] = data;
+		Modules.sceDisplayModule.write32(address);
 	}
 
 	@Override
 	public void write64(int address, long data) {
-		try {
-			address &= addressMask;
+		address &= addressMask;
 
-			if (traceWrite) {
-				if (log.isTraceEnabled()) {
-					log.trace("write64(0x" + Integer.toHexString(address).toUpperCase() + ", 0x" + Long.toHexString(data).toUpperCase() + ")");
-				}
+		if (traceWrite) {
+			if (log.isTraceEnabled()) {
+				log.trace(String.format("write64(0x%08X, 0x%X", address, data));
 			}
-
-			all[address >> 2] = (int) data;
-			all[(address >> 2) + 1] = (int) (data >> 32);
-		} catch (Exception e) {
-            invalidMemoryAddress(address, "write64", Emulator.EMU_STATUS_MEM_WRITE);
 		}
+
+		all[address >> 2] = (int) data;
+		all[(address >> 2) + 1] = (int) (data >> 32);
 	}
 
 	@Override
@@ -263,8 +226,8 @@ public class FastMemory extends Memory {
 		return buffer.slice();
 	}
 
-	private boolean isIntAligned(int n) {
-		return (n & 0x03) == 0;
+	private static boolean isIntAligned(int n) {
+		return isIntAligned[n & 0x03];
 	}
 
 	@Override
@@ -279,9 +242,16 @@ public class FastMemory extends Memory {
 
 		int count4 = length >> 2;
 		if (count4 > 0) {
-			int data1 = data & 0xFF;
-			int data4 = (data1 << 24) | (data1 << 16) | (data1 << 8) | data1;
-			Arrays.fill(all, address >> 2, (address >> 2) + count4, data4);
+			if (data == 0) {
+				// Fast memset(addr, 0, length) using copy from "zero" array
+				for (int i = 0; i < count4; i += zero.length) {
+					System.arraycopy(zero, 0, all, (address >> 2) + i, Math.min(zero.length, count4 - i));
+				}
+			} else {
+				int data1 = data & 0xFF;
+				int data4 = (data1 << 24) | (data1 << 16) | (data1 << 8) | data1;
+				Arrays.fill(all, address >> 2, (address >> 2) + count4, data4);
+			}
 			address += count4 << 2;
 			length -= count4 << 2;
 		}
@@ -339,12 +309,11 @@ public class FastMemory extends Memory {
 			// Buffers are overlapping, but we have to copy as they would not overlap.
 			// Unfortunately, IntBuffer operation are always checking for overlapping buffers,
 			// so we have to copy manually...
-			IMemoryReader sourceReader = MemoryReader.getMemoryReader(source, length, 4);
-			IMemoryWriter destinationWriter = MemoryWriter.getMemoryWriter(destination, length, 4);
+			int src = source >> 2;
+			int dst = destination >> 2;
 			for (int i = 0; i < length; i += 4) {
-				destinationWriter.writeNext(sourceReader.readNext());
+				all[dst++] = all[src++];
 			}
-			destinationWriter.flush();
 		}
 	}
 
@@ -406,9 +375,11 @@ public class FastMemory extends Memory {
 					}
 				} else {
 					IMemoryReader sourceReader = MemoryReader.getMemoryReader(source, length, 1);
+					IMemoryWriter destinationWriter = MemoryWriter.getMemoryWriter(destination, length, 1);
 					for (int i = 0; i < length; i++) {
-						write8(destination + i, (byte) sourceReader.readNext());
+						destinationWriter.writeNext(sourceReader.readNext());
 					}
+					destinationWriter.flush();
 				}
 			} else {
 				for (int i = length - 1; i >= 0; i--) {
