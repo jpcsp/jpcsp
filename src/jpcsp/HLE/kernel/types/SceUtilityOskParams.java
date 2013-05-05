jpcsp/jpcsp

commit 095045ad204b9412f9100e1d71bb6732800d082a

Improved compatibility of sceUtilityOsk...: outTextLength contains the max size of the returned text.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3138 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/kernel/types/SceUtilityOskParams.java
@@ -116,7 +116,12 @@ public class SceUtilityOskParams extends pspAbstractMemoryMappedStructure {
 			writeStringUTF16Z(descAddr, desc);
 			write32(inTextAddr);
 			writeStringUTF16Z(inTextAddr, inText);
-			outTextLength = writeStringUTF16Z(outTextAddr, outText);
+			int length = Math.min(outText.length(), outTextLength - 1);
+			if (outTextLimit != 0) {
+				length = Math.min(length, outTextLimit);
+			}
+			int bytesLength = writeStringUTF16Z(outTextAddr, outText.substring(0, length));
+			mem.memset(outTextAddr + length, (byte) 0, outTextLength * 2 - bytesLength);
 			write32(outTextLength);
 			write32(outTextAddr);
 			write32(result);
@@ -168,6 +173,6 @@ public class SceUtilityOskParams extends pspAbstractMemoryMappedStructure {
 
 	@Override
 	public String toString() {
-		return String.format("desc=%s, inText=%s, outText=%s", oskData.desc, oskData.inText, oskData.outText);
+		return String.format("desc='%s', inText='%s', outText='%s', inputMode=%d, inputAttr=0x%X, outTextLength=%d, outTextLimit=%d", oskData.desc, oskData.inText, oskData.outText, oskData.inputMode, oskData.inputAttr, oskData.outTextLength, oskData.outTextLimit);
 	}
 }

src/jpcsp/HLE/kernel/types/pspAbstractMemoryMappedStructure.java
@@ -21,7 +21,9 @@ import java.nio.charset.Charset;
 import jpcsp.Memory;
 import jpcsp.HLE.ITPointerBase;
 import jpcsp.memory.IMemoryReader;
+import jpcsp.memory.IMemoryWriter;
 import jpcsp.memory.MemoryReader;
+import jpcsp.memory.MemoryWriter;
 import jpcsp.util.Utilities;
 
 public abstract class pspAbstractMemoryMappedStructure {
@@ -258,7 +260,6 @@ public abstract class pspAbstractMemoryMappedStructure {
     	return s.toString();
     }
 
-    // Write a string in UTF16
     /**
      * Write a string in UTF16, including a trailing '\0\0'
      * @param addr address where to write the string
@@ -275,13 +276,15 @@ public abstract class pspAbstractMemoryMappedStructure {
     		return 0;
     	}
 
+    	IMemoryWriter memoryWriter = MemoryWriter.getMemoryWriter(addr, bytes.length + 2, 1);
     	for (int i = 0; i < bytes.length; i++) {
-    		mem.write8(addr + i, bytes[i]);
+    		memoryWriter.writeNext(bytes[i] & 0xFF);
     	}
 
     	// Write trailing '\0\0'
-    	mem.write8(addr + bytes.length    , (byte) 0);
-    	mem.write8(addr + bytes.length + 1, (byte) 0);
+    	memoryWriter.writeNext(0);
+    	memoryWriter.writeNext(0);
+    	memoryWriter.flush();
 
     	return bytes.length;
     }

commit 0430aad73528b453a397bedc88d6c4e6ac3ccd70

Fixed problem introduced by r3138 in sceUtilityOsk: avoid truncation of entered text.

src/jpcsp/HLE/kernel/types/SceUtilityOskParams.java
@@ -121,7 +121,7 @@ public class SceUtilityOskParams extends pspAbstractMemoryMappedStructure {
 				length = Math.min(length, outTextLimit);
 			}
 			int bytesLength = writeStringUTF16Z(outTextAddr, outText.substring(0, length));
-			mem.memset(outTextAddr + length, (byte) 0, outTextLength * 2 - bytesLength);
+			mem.memset(outTextAddr + bytesLength, (byte) 0, outTextLength * 2 - bytesLength);
 			write32(outTextLength);
 			write32(outTextAddr);
 			write32(result);