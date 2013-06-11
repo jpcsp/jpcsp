jpcsp/jpcsp

commit 42856a9d6569f49df11c67b2d58f2763c84dc647

New findings on error codes returned by sceUtilitySavedata mode=AUTOLOAD, LOAD, LISTLOAD, SIZES, WRITE, WRITESECURE, ERASESECURE: different error codes are returned depending if the game directory is present or not. For some modes, the return code also depends on the structure size (i.e. the version it was compiled for).
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@2974 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/kernel/types/SceUtilitySavedataParam.java
@@ -30,6 +30,7 @@ import jpcsp.memory.MemoryReader;
 import jpcsp.memory.IMemoryWriter;
 import jpcsp.memory.MemoryWriter;
 import jpcsp.HLE.Modules;
+import jpcsp.HLE.VFS.IVirtualFileSystem;
 import jpcsp.crypto.CryptoEngine;
 import jpcsp.filesystems.SeekableDataInput;
 import jpcsp.filesystems.SeekableRandomFile;
@@ -236,13 +237,13 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
         read(base);
         setMaxSize(base.totalSizeof());
 
-        mode = read32();
-        bind = read32();
-        overwrite = read32() == 0 ? false : true;
-        gameName = readStringNZ(13);
+        mode = read32(); // Offset 48
+        bind = read32(); // Offset 52
+        overwrite = read32() == 0 ? false : true; // Offset 56
+        gameName = readStringNZ(13); // Offset 60
         readUnknown(3);
-        saveName = readStringNZ(20);
-        saveNameListAddr = read32();
+        saveName = readStringNZ(20); // Offset 76
+        saveNameListAddr = read32(); // Offset 96
         if (Memory.isAddressGood(saveNameListAddr)) {
             List<String> newSaveNameList = new ArrayList<String>();
             boolean endOfList = false;
@@ -256,41 +257,41 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
             }
             saveNameList = newSaveNameList.toArray(new String[newSaveNameList.size()]);
         }
-        fileName = readStringNZ(13);
+        fileName = readStringNZ(13); // Offset 100
         readUnknown(3);
-        dataBuf = read32();
-        dataBufSize = read32();
-        dataSize = read32();
+        dataBuf = read32(); // Offset 116
+        dataBufSize = read32(); // Offset 120
+        dataSize = read32(); // Offset 124
 
         sfoParam = new PspUtilitySavedataSFOParam();
-        read(sfoParam);
+        read(sfoParam); // Offset 128
         icon0FileData = new PspUtilitySavedataFileData();
-        read(icon0FileData);
+        read(icon0FileData); // Offset 1412
         icon1FileData = new PspUtilitySavedataFileData();
-        read(icon1FileData);
+        read(icon1FileData); // Offset 1428
         pic1FileData = new PspUtilitySavedataFileData();
-        read(pic1FileData);
+        read(pic1FileData); // Offset 1444
         snd0FileData = new PspUtilitySavedataFileData();
-        read(snd0FileData);
+        read(snd0FileData); // Offset 1460
 
-        newDataAddr = read32();
+        newDataAddr = read32(); // Offset 1476
         if (newDataAddr != 0) {
             newData = new PspUtilitySavedataListSaveNewData();
             newData.read(mem, newDataAddr);
         } else {
             newData = null;
         }
-        focus = read32();
-        abortStatus = read32();
-        msFreeAddr = read32();
-        msDataAddr = read32();
-        utilityDataAddr = read32();
-        read8Array(key);
-        secureVersion = read32();
-        multiStatus = read32();
-        idListAddr = read32();
-        fileListAddr = read32();
-        sizeAddr = read32();
+        focus = read32(); // Offset 1480
+        abortStatus = read32(); // Offset 1484
+        msFreeAddr = read32(); // Offset 1488
+        msDataAddr = read32(); // Offset 1492
+        utilityDataAddr = read32(); // Offset 1496
+        read8Array(key); // Offset 1500
+        secureVersion = read32(); // Offset 1516
+        multiStatus = read32(); // Offset 1520
+        idListAddr = read32(); // Offset 1524
+        fileListAddr = read32(); // Offset 1528
+        sizeAddr = read32(); // Offset 1532
     }
 
     @Override
@@ -335,10 +336,14 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
     }
 
     public String getBasePath() {
-        return getBasePath(saveName);
+        return getBasePath(gameName, saveName);
     }
 
     public String getBasePath(String saveName) {
+    	return getBasePath(gameName, saveName);
+    }
+
+    public String getBasePath(String gameName, String saveName) {
         String path = savedataPath + gameName;
         if (saveName != null && !anyFileName.equals(saveName)) {
             path += saveName;
@@ -351,28 +356,26 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
         return getBasePath(saveName) + fileName;
     }
 
-    private int getFileSize(String fileName) {
-        int size = 0;
-
-        if (fileName != null && fileName.length() > 0) {
-            SceIoStat fileStat = Modules.IoFileMgrForUserModule.statFile(getFileName(saveName, fileName));
-            if (fileStat != null) {
-                size = (int) fileStat.size;
-            }
-        }
-
-        return size;
-    }
-
     public int getSizeKb(String gameName, String saveName) {
-        int sizeKb;
+        int sizeKb = 0;
 
-        sizeKb = Utilities.getSizeKb(getFileSize(fileName));
-        sizeKb += Utilities.getSizeKb(getFileSize(icon0FileName));
-        sizeKb += Utilities.getSizeKb(getFileSize(icon1FileName));
-        sizeKb += Utilities.getSizeKb(getFileSize(pic1FileName));
-        sizeKb += Utilities.getSizeKb(getFileSize(snd0FileName));
-        sizeKb += Utilities.getSizeKb(getFileSize(paramSfoFileName));
+        String path = getBasePath(saveName);
+        StringBuilder localFileName = new StringBuilder();
+        IVirtualFileSystem vfs = Modules.IoFileMgrForUserModule.getVirtualFileSystem(path, localFileName);
+        if (vfs != null) {
+        	String[] fileNames = vfs.ioDopen(localFileName.toString());
+        	if (fileNames != null) {
+        		for (int i = 0; i < fileNames.length; i++) {
+            		SceIoStat stat = new SceIoStat();
+            		SceIoDirent dirent = new SceIoDirent(stat, fileNames[i]);
+            		int result = vfs.ioDread(localFileName.toString(), dirent);
+            		if (result > 0) {
+            			sizeKb += Utilities.getSizeKb((int) stat.size);
+            		}
+        		}
+        		vfs.ioDclose(localFileName.toString());
+        	}
+        }
 
         return sizeKb;
     }
@@ -625,6 +628,16 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
         fileInput.close();
 
         return true;
+    }
+
+    public boolean isDirectoryPresent(String gameName, String saveName) {
+    	String path = getBasePath(saveName);
+    	SceIoStat stat = Modules.IoFileMgrForUserModule.statFile(path);
+    	if (stat != null && (stat.attr & 0x20) == 0) {
+    		return true;
+    	}
+
+    	return false;
     }
 
     public boolean isPresent(String gameName, String saveName) {

src/jpcsp/HLE/modules150/sceUtility.java
@@ -563,7 +563,14 @@ public class sceUtility extends HLEModule {
 	                    savedataParams.base.result = 0;
 	                    savedataParams.write(mem);
 	                } catch (IOException e) {
-	                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_LOAD_NO_DATA;
+	                	if (!savedataParams.isGameDirectoryPresent()) {
+		                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_LOAD_NO_DATA;
+	                	} else if (savedataParams.base.totalSizeof() < 1536) {
+		                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_LOAD_NO_DATA;
+	                	} else {
+	                		// The PSP is returning a different return code based on the size of the savedataParams input structure.
+		                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_LOAD_NO_UMD;
+	                	}
 	                } catch (Exception e) {
 	                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_LOAD_ACCESS_ERROR;
 	                    log.error(e);
@@ -600,7 +607,14 @@ public class sceUtility extends HLEModule {
 		                        savedataParams.base.result = 0;
 		                        savedataParams.write(mem);
 		                    } catch (IOException e) {
-		                        savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_LOAD_NO_DATA;
+			                	if (!savedataParams.isGameDirectoryPresent()) {
+				                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_LOAD_NO_DATA;
+			                	} else if (savedataParams.base.totalSizeof() < 1536) {
+				                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_LOAD_NO_DATA;
+			                	} else {
+			                		// The PSP is returning a different return code based on the size of the savedataParams input structure.
+				                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_LOAD_NO_UMD;
+			                	}
 		                    } catch (Exception e) {
 		                        savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_LOAD_ACCESS_ERROR;
 		                        log.error(e);
@@ -754,87 +768,75 @@ public class sceUtility extends HLEModule {
 	                //   size (32KB string) : "416 KB"
 	                // error: SCE_UTILITY_SAVEDATA_TYPE_SIZES return 801103c7
 	                //
-                    int baseResult = 0;
-	                String gameName = savedataParams.gameName;
-	                String saveName = savedataParams.saveName;
+                    int result = 0;
 
-	                // MS free size.
-                    // Gets the ammount of free space in the Memory Stick. If null,
-                    // the size is ignored and no error is returned.
-	                int buffer1Addr = savedataParams.msFreeAddr;
-	                if (Memory.isAddressGood(buffer1Addr)) {
+                    if (log.isDebugEnabled()) {
+                    	log.debug(String.format("MODE_SIZES: msFreeAddr=0x%08X-0x%08X, msDataAddr=0x%08X-0x%08X, utilityDataAddr=0x%08X-0x%08X", savedataParams.msFreeAddr, savedataParams.msFreeAddr + 20, savedataParams.msDataAddr, savedataParams.msDataAddr + 64, savedataParams.utilityDataAddr, savedataParams.utilityDataAddr + 28));
+                    }
+
+                    // Gets the amount of free space on the Memory Stick.
+	                int msFreeAddr = savedataParams.msFreeAddr;
+	                if (msFreeAddr != 0) {
 	                    String memoryStickFreeSpaceString = MemoryStick.getSizeKbString(MemoryStick.getFreeSizeKb());
 
-	                    mem.write32(buffer1Addr + 0, MemoryStick.getSectorSize());
-	                    mem.write32(buffer1Addr + 4, MemoryStick.getFreeSizeKb() / MemoryStick.getSectorSizeKb());
-	                    mem.write32(buffer1Addr + 8, MemoryStick.getFreeSizeKb());
-	                    Utilities.writeStringNZ(mem, buffer1Addr + 12, 8, memoryStickFreeSpaceString);
+	                    mem.write32(msFreeAddr + 0, MemoryStick.getSectorSize());
+	                    mem.write32(msFreeAddr + 4, MemoryStick.getFreeSizeKb() / MemoryStick.getSectorSizeKb());
+	                    mem.write32(msFreeAddr + 8, MemoryStick.getFreeSizeKb());
+	                    Utilities.writeStringNZ(mem, msFreeAddr + 12, 8, memoryStickFreeSpaceString);
 
 	                    log.debug("Memory Stick Free Space = " + memoryStickFreeSpaceString);
 	                }
 
-	                // MS data size.
-                    // Gets the size of the data already saved in the Memory Stick.
-                    // If null, the size is ignored and no error is returned.
-	                int buffer2Addr = savedataParams.msDataAddr;
-	                if (Memory.isAddressGood(buffer2Addr)) {
-	                    gameName = Utilities.readStringNZ(mem, buffer2Addr, 13);
-	                    saveName = Utilities.readStringNZ(mem, buffer2Addr + 16, 20);
+                    // Gets the size of the data already saved on the Memory Stick.
+	                int msDataAddr = savedataParams.msDataAddr;
+	                if (msDataAddr != 0) {
+	                    String gameName = Utilities.readStringNZ(mem, msDataAddr, 13);
+	                    String saveName = Utilities.readStringNZ(mem, msDataAddr + 16, 20);
 
-                        if (savedataParams.isPresent(gameName, saveName)) {
+                        if (savedataParams.isDirectoryPresent(gameName, saveName)) {
                             int savedataSizeKb = savedataParams.getSizeKb(gameName, saveName);
                             int savedataSize32Kb = MemoryStick.getSize32Kb(savedataSizeKb);
 
-                            mem.write32(buffer2Addr + 36, savedataSizeKb / MemoryStick.getSectorSizeKb()); // Number of sectors.
-                            mem.write32(buffer2Addr + 40, savedataSizeKb); // Size in Kb.
-                            Utilities.writeStringNZ(mem, buffer2Addr + 44, 8, MemoryStick.getSizeKbString(savedataSizeKb));
-                            mem.write32(buffer2Addr + 52, savedataSize32Kb);
-                            Utilities.writeStringNZ(mem, buffer2Addr + 56, 8, MemoryStick.getSizeKbString(savedataSize32Kb));
+                            mem.write32(msDataAddr + 36, savedataSizeKb / MemoryStick.getSectorSizeKb()); // Number of sectors.
+                            mem.write32(msDataAddr + 40, savedataSizeKb); // Size in Kb.
+                            Utilities.writeStringNZ(mem, msDataAddr + 44, 8, MemoryStick.getSizeKbString(savedataSizeKb));
+                            mem.write32(msDataAddr + 52, savedataSize32Kb);
+                            Utilities.writeStringNZ(mem, msDataAddr + 56, 8, MemoryStick.getSizeKbString(savedataSize32Kb));
 
-                            log.debug("Memory Stick Full Space = " +  MemoryStick.getSizeKbString(savedataSizeKb));
+                            log.debug("Memory Stick Used Space = " +  MemoryStick.getSizeKbString(savedataSizeKb));
 	                    } else {
-                            mem.write32(buffer2Addr + 36, 0); // Number of sectors.
-                            mem.write32(buffer2Addr + 40, 0); // Size in Kb.
-                            Utilities.writeStringNZ(mem, buffer2Addr + 44, 8, "");
-                            mem.write32(buffer2Addr + 52, 0);
-                            Utilities.writeStringNZ(mem, buffer2Addr + 56, 8, "");
-
                             log.debug(String.format("Savedata MODE_SIZES directory not found, gameName='%s', saveName='%s'", gameName, saveName));
-	                        baseResult = SceKernelErrors.ERROR_SAVEDATA_SIZES_NO_DATA;
+	                        result = SceKernelErrors.ERROR_SAVEDATA_SIZES_NO_DATA;
 	                    }
 	                }
 
-	                // Utility data size.
-                    // Gets the size of the data to be saved in the Memory Stick.
-                    // If null, the size is ignored and no error is returned.
-	                int buffer3Addr = savedataParams.utilityDataAddr;
-	                if (Memory.isAddressGood(buffer3Addr)) {
+                    // Gets the size of the data to be saved on the Memory Stick.
+	                int utilityDataAddr = savedataParams.utilityDataAddr;
+	                if (utilityDataAddr != 0) {
 	                    int memoryStickRequiredSpaceKb = 0;
 	                    memoryStickRequiredSpaceKb += MemoryStick.getSectorSizeKb(); // Assume 1 sector for SFO-Params
-                        // In overwrite mode, the dataSize params are ignored.
-                        if (!savedataParams.overwrite) {
-                            // The main binary dataSize depends on the fileName existance.
-                            if (savedataParams.fileName != null) {
-                                memoryStickRequiredSpaceKb += computeMemoryStickRequiredSpaceKb(savedataParams.dataSize);
-                            }
-                            memoryStickRequiredSpaceKb += computeMemoryStickRequiredSpaceKb(savedataParams.icon0FileData.size);
-                            memoryStickRequiredSpaceKb += computeMemoryStickRequiredSpaceKb(savedataParams.icon1FileData.size);
-                            memoryStickRequiredSpaceKb += computeMemoryStickRequiredSpaceKb(savedataParams.pic1FileData.size);
-                            memoryStickRequiredSpaceKb += computeMemoryStickRequiredSpaceKb(savedataParams.snd0FileData.size);
+                        // Add the dataSize only if a fileName has been provided
+                        if (savedataParams.fileName != null && savedataParams.fileName.length() > 0) {
+                            memoryStickRequiredSpaceKb += computeMemoryStickRequiredSpaceKb(savedataParams.dataSize + 15);
                         }
-	                    String memoryStickRequiredSpaceString = MemoryStick.getSizeKbString(memoryStickRequiredSpaceKb);
+                        memoryStickRequiredSpaceKb += computeMemoryStickRequiredSpaceKb(savedataParams.icon0FileData.size);
+                        memoryStickRequiredSpaceKb += computeMemoryStickRequiredSpaceKb(savedataParams.icon1FileData.size);
+                        memoryStickRequiredSpaceKb += computeMemoryStickRequiredSpaceKb(savedataParams.pic1FileData.size);
+                        memoryStickRequiredSpaceKb += computeMemoryStickRequiredSpaceKb(savedataParams.snd0FileData.size);
+
+                        String memoryStickRequiredSpaceString = MemoryStick.getSizeKbString(memoryStickRequiredSpaceKb);
 	                    int memoryStickRequiredSpace32Kb = MemoryStick.getSize32Kb(memoryStickRequiredSpaceKb);
 	                    String memoryStickRequiredSpace32KbString = MemoryStick.getSizeKbString(memoryStickRequiredSpace32Kb);
 
-	                    mem.write32(buffer3Addr + 0, memoryStickRequiredSpaceKb / MemoryStick.getSectorSizeKb());
-	                    mem.write32(buffer3Addr + 4, memoryStickRequiredSpaceKb);
-	                    Utilities.writeStringNZ(mem, buffer3Addr + 8, 8, memoryStickRequiredSpaceString);
-	                    mem.write32(buffer3Addr + 16, memoryStickRequiredSpace32Kb);
-	                    Utilities.writeStringNZ(mem, buffer3Addr + 20, 8, memoryStickRequiredSpace32KbString);
+	                    mem.write32(utilityDataAddr + 0, memoryStickRequiredSpaceKb / MemoryStick.getSectorSizeKb());
+	                    mem.write32(utilityDataAddr + 4, memoryStickRequiredSpaceKb);
+	                    Utilities.writeStringNZ(mem, utilityDataAddr + 8, 8, memoryStickRequiredSpaceString);
+	                    mem.write32(utilityDataAddr + 16, memoryStickRequiredSpace32Kb);
+	                    Utilities.writeStringNZ(mem, utilityDataAddr + 20, 8, memoryStickRequiredSpace32KbString);
 
 	                    log.debug("Memory Stick Required Space = " + memoryStickRequiredSpaceString);
 	                }
-                    savedataParams.base.result = baseResult;
+                    savedataParams.base.result = result;
 	                break;
 	            }
 
@@ -1042,7 +1044,11 @@ public class sceUtility extends HLEModule {
 	                    savedataParams.singleWrite(mem);
 	                    savedataParams.base.result = 0;
 	                } catch (IOException e) {
-	                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_RW_ACCESS_ERROR;
+	                	if (!savedataParams.isGameDirectoryPresent()) {
+		                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_RW_NO_DATA;
+	                	} else {
+	                		savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_RW_ACCESS_ERROR;
+	                	}
 	                } catch (Exception e) {
 	                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_RW_ACCESS_ERROR;
                         log.error(e);
@@ -1147,8 +1153,13 @@ public class sceUtility extends HLEModule {
 	            case SceUtilitySavedataParam.MODE_ERASESECURE:
 	                if (savedataParams.fileName != null) {
 	                    String save = savedataParams.getFileName(savedataParams.saveName, savedataParams.fileName);
-	                    Modules.IoFileMgrForUserModule.deleteFile(save);
-	                    savedataParams.base.result = 0;
+	                    if (Modules.IoFileMgrForUserModule.deleteFile(save)) {
+	                    	savedataParams.base.result = 0;
+	                    } else if (savedataParams.isGameDirectoryPresent()) {
+	                    	savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_RW_NO_DATA;
+	                    } else {
+	                    	savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_RW_FILE_NOT_FOUND;
+	                    }
 	                } else {
 	                    log.warn("Savedata MODE_ERASESECURE no fileName specified!");
 	                    savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_RW_NO_DATA;

src/jpcsp/HLE/kernel/types/SceKernelErrors.java
@@ -311,6 +311,7 @@ public class SceKernelErrors {
     public static final int ERROR_SAVEDATA_SAVE_NO_SPACE                        = 0x80110383;
     public static final int ERROR_SAVEDATA_SAVE_MEMSTICK_PROTECTED              = 0x80110384;
     public static final int ERROR_SAVEDATA_SAVE_ACCESS_ERROR                    = 0x80110385;
+    public static final int ERROR_SAVEDATA_SAVE_DATA_BROKEN                     = 0x80110386;
     public static final int ERROR_SAVEDATA_SAVE_BAD_PARAMS                      = 0x80110388;
     public static final int ERROR_SAVEDATA_SAVE_NO_UMD                          = 0x80110389;
     public static final int ERROR_SAVEDATA_SAVE_WRONG_UMD                       = 0x8011038a;

src/jpcsp/HLE/modules150/IoFileMgrForUser.java
@@ -1411,6 +1411,13 @@ public class IoFileMgrForUser extends HLEModule {
     	}
     }
 
+    public IVirtualFileSystem getVirtualFileSystem(String pspfilename, StringBuilder localFileName) {
+        String absoluteFileName = getAbsoluteFileName(pspfilename);
+        IVirtualFileSystem vfs = vfsManager.getVirtualFileSystem(absoluteFileName, localFileName);
+
+        return vfs;
+    }
+
     /*
      * HLE functions.
      */

commit 09cf646b5296acf45c8ac905a924784bbf8fd7af

Improved sceUtilitySavedata MODE_SIZES: empty saveName has to be interpreted as "any" saveName. No error has to be returned if any save can be found under the gameName. Was broken by r2974.

src/jpcsp/HLE/kernel/types/SceUtilitySavedataParam.java
@@ -657,8 +657,29 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
         return true;
     }
 
+    public String getAnySaveName(String gameName, String saveName) {
+        // NULL can also be sent in saveName (seen in MODE_SIZES).
+        // It means any save from the current game, since all saves share a common
+        // save data file.
+        if (saveName == null || saveName.length() <= 0 || anyFileName.equals(saveName)) {
+            File f = new File(savedataFilePath);
+            String[] entries = f.list();
+            if (entries != null) {
+	            for (int i = 0; i < f.list().length; i++) {
+	                if (entries[i].startsWith(gameName)) {
+	                    saveName = entries[i].replace(gameName, "");
+	                    break;
+	                }
+	            }
+            }
+        }
+
+        return saveName;
+    }
+
     public boolean isDirectoryPresent(String gameName, String saveName) {
-    	String path = getBasePath(gameName, saveName);
+    	saveName = getAnySaveName(gameName, saveName);
+        String path = getBasePath(gameName, saveName);
     	SceIoStat stat = Modules.IoFileMgrForUserModule.statFile(path);
     	if (stat != null && (stat.attr & 0x20) == 0) {
     		return true;
@@ -668,32 +689,18 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
     }
 
     public boolean isPresent(String gameName, String saveName) {
-        // NULL can also be sent in saveName (seen in MODE_SIZES).
-        // It means any save from the current game, since all saves share a common
-        // save data file.
-        if (saveName == null || saveName.length() <= 0 || anyFileName.equals(saveName)) {
-            File f = new File(savedataFilePath);
-            String[] entries = f.list();
-            if (entries == null) {
-                return false;
-            }
-            for (int i = 0; i < f.list().length; i++) {
-                if (entries[i].startsWith(gameName)) {
-                    saveName = entries[i].replace(gameName, "");
-                    break;
-                }
-            }
-        }
+    	saveName = getAnySaveName(gameName, saveName);
+        String path = getBasePath(gameName, saveName);
+
         // When NULL is sent in fileName, it means any file inside the savedata folder.
         if (fileName == null || fileName.length() <= 0) {
-            File f = new File(savedataFilePath + gameName + saveName);
+            File f = new File(path);
             if (f.list() == null) {
                 return false;
             }
             return true;
         }
 
-        String path = getBasePath(gameName, saveName);
         try {
             SeekableDataInput fileInput = getDataInput(path, fileName);
             if (fileInput != null) {

src/jpcsp/HLE/modules150/sceUtility.java
@@ -913,6 +913,7 @@ public class sceUtility extends HLEModule {
                         String gameName = Utilities.readStringNZ(mem, msDataAddr, 13);
                         String saveName = Utilities.readStringNZ(mem, msDataAddr + 16, 20);
 
+                        saveName = savedataParams.getAnySaveName(gameName, saveName);
                         if (savedataParams.isDirectoryPresent(gameName, saveName)) {
                             int savedataSizeKb = savedataParams.getSizeKb(gameName, saveName);
                             int savedataSize32Kb = MemoryStick.getSize32Kb(savedataSizeKb);