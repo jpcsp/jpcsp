jpcsp/jpcsp

commit 3482d4120c9492e9106f2d65630dc7802beafa31

Improved sceUtilitySavedata MODE_SIZES: empty saveName has to be interpreted as "any" saveName. No error has to be returned if any save can be found under the gameName. Was broken by r2974.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3229 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/kernel/types/SceUtilitySavedataParam.java
@@ -657,8 +657,29 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
         return true;
     }
 
+    public String getAnySaveName(String gameName, String saveName) {
+        // NULL can also be sent in saveName (seen in MODE_SIZES).
+        // It means any save from the current game, since all saves share a common
+        // save data file.
+        if (saveName == null || saveName.length() <= 0 || anyFileName.equals(saveName)) {
+            File f = new File(savedataFilePath);
+            String[] entries = f.list();
+            if (entries != null) {
+	            for (int i = 0; i < f.list().length; i++) {
+	                if (entries[i].startsWith(gameName)) {
+	                    saveName = entries[i].replace(gameName, "");
+	                    break;
+	                }
+	            }
+            }
+        }
+
+        return saveName;
+    }
+
     public boolean isDirectoryPresent(String gameName, String saveName) {
-    	String path = getBasePath(gameName, saveName);
+    	saveName = getAnySaveName(gameName, saveName);
+        String path = getBasePath(gameName, saveName);
     	SceIoStat stat = Modules.IoFileMgrForUserModule.statFile(path);
     	if (stat != null && (stat.attr & 0x20) == 0) {
     		return true;
@@ -668,32 +689,18 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
     }
 
     public boolean isPresent(String gameName, String saveName) {
-        // NULL can also be sent in saveName (seen in MODE_SIZES).
-        // It means any save from the current game, since all saves share a common
-        // save data file.
-        if (saveName == null || saveName.length() <= 0 || anyFileName.equals(saveName)) {
-            File f = new File(savedataFilePath);
-            String[] entries = f.list();
-            if (entries == null) {
-                return false;
-            }
-            for (int i = 0; i < f.list().length; i++) {
-                if (entries[i].startsWith(gameName)) {
-                    saveName = entries[i].replace(gameName, "");
-                    break;
-                }
-            }
-        }
+    	saveName = getAnySaveName(gameName, saveName);
+        String path = getBasePath(gameName, saveName);
+
         // When NULL is sent in fileName, it means any file inside the savedata folder.
         if (fileName == null || fileName.length() <= 0) {
-            File f = new File(savedataFilePath + gameName + saveName);
+            File f = new File(path);
             if (f.list() == null) {
                 return false;
             }
             return true;
         }
 
-        String path = getBasePath(gameName, saveName);
         try {
             SeekableDataInput fileInput = getDataInput(path, fileName);
             if (fileInput != null) {

src/jpcsp/HLE/modules150/sceUtility.java
@@ -913,6 +913,7 @@ public class sceUtility extends HLEModule {
                         String gameName = Utilities.readStringNZ(mem, msDataAddr, 13);
                         String saveName = Utilities.readStringNZ(mem, msDataAddr + 16, 20);
 
+                        saveName = savedataParams.getAnySaveName(gameName, saveName);
                         if (savedataParams.isDirectoryPresent(gameName, saveName)) {
                             int savedataSizeKb = savedataParams.getSizeKb(gameName, saveName);
                             int savedataSize32Kb = MemoryStick.getSize32Kb(savedataSizeKb);

commit 1d3895fa0573661f680b8fa604bc9f280c650873

Fixed savedata issue introduced by r3229.

src/jpcsp/HLE/kernel/types/SceUtilitySavedataParam.java
@@ -714,17 +714,17 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
 
     public boolean isPresent(String gameName, String saveName) {
     	saveName = getAnySaveName(gameName, saveName);
-        String path = getBasePath(gameName, saveName);
 
         // When NULL is sent in fileName, it means any file inside the savedata folder.
         if (fileName == null || fileName.length() <= 0) {
-            File f = new File(path);
+            File f = new File(savedataFilePath + gameName + saveName);
             if (f.list() == null) {
                 return false;
             }
             return true;
         }
 
+        String path = getBasePath(gameName, saveName);
         try {
             SeekableDataInput fileInput = getDataInput(path, fileName);
             if (fileInput != null) {
@@ -759,6 +759,22 @@ public class SceUtilitySavedataParam extends pspAbstractMemoryMappedStructure {
         }
 
         return 0;
+    }
+
+    public Calendar getSavedTime() {
+    	return getSavedTime(saveName);
+    }
+
+    public Calendar getSavedTime(String saveName) {
+		String sfoFileName = getFileName(saveName, SceUtilitySavedataParam.paramSfoFileName);
+        SceIoStat sfoStat = Modules.IoFileMgrForUserModule.statFile(sfoFileName);
+        ScePspDateTime pspTime = sfoStat.mtime;
+
+		Calendar savedTime = Calendar.getInstance();
+        // pspTime.month has a value in range [1..12], Calendar requires a value in range [0..11]
+        savedTime.set(pspTime.year, pspTime.month - 1, pspTime.day, pspTime.hour, pspTime.minute, pspTime.second);
+
+        return savedTime;
     }
 
     @Override