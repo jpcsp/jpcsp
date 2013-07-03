jpcsp/jpcsp

commit 885b55f572274a6d823061b9d39d9f9326be709b

Implemented circular buffer for sceAtrac3plus. Improved handling of bufferSize with a negative value in sceAtracSetData
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3096 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/kernel/types/pspFileBuffer.java
@@ -1,0 +1,134 @@
+/*
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.HLE.kernel.types;
+
+import static jpcsp.util.Utilities.min;
+
+/**
+ * Implements a circular buffer in PSP memory that can
+ * be fed from a file.
+ * Used by sceAtrac3plus and sceMp3 modules.
+ * 
+ * @author gid15
+ *
+ */
+public class pspFileBuffer {
+	private int addr;
+	private int maxSize;
+	private int currentSize;
+	private int readPosition;
+	private int writePosition;
+	private int filePosition;
+	private int fileMaxSize;
+
+	public pspFileBuffer() {
+	}
+
+	public pspFileBuffer(int addr, int maxSize) {
+		this.addr = addr;
+		this.maxSize = maxSize;
+	}
+
+	public pspFileBuffer(int addr, int maxSize, int readSize) {
+		this.addr = addr;
+		this.maxSize = maxSize;
+		notifyWrite(readSize);
+	}
+
+	public void setFileMaxSize(int fileMaxSize) {
+		this.fileMaxSize = fileMaxSize;
+	}
+
+	public boolean isFileEnd() {
+		return filePosition >= fileMaxSize;
+	}
+
+	public int getWriteAddr() {
+		return addr + writePosition;
+	}
+
+	public int getWriteSize() {
+		return min(maxSize - currentSize, maxSize - writePosition, fileMaxSize - filePosition);
+	}
+
+	public int getFilePosition() {
+		return filePosition;
+	}
+
+	public int getReadAddr() {
+		return addr + readPosition;
+	}
+
+	public int getReadSize() {
+		return min(currentSize, maxSize - readPosition);
+	}
+
+	public void reset(int readSize, int filePosition) {
+		currentSize = 0;
+		readPosition = 0;
+		writePosition = 0;
+		notifyWrite(readSize);
+		this.filePosition = filePosition;
+	}
+
+	public void notifyRead(int size) {
+		if (size > 0) {
+			size = min(size, currentSize);
+			readPosition = incrementPosition(readPosition, size);
+			currentSize -= size;
+		}
+	}
+
+	public void notifyReadAll() {
+		notifyRead(currentSize);
+	}
+
+	public void notifyWrite(int size) {
+		if (size > 0) {
+			size = min(size, getMaxSize() - currentSize);
+			writePosition = incrementPosition(writePosition, size);
+			filePosition += size;
+			currentSize += size;
+		}
+	}
+
+	private int incrementPosition(int position, int size) {
+		position += size;
+		if (position >= maxSize) {
+			position -= maxSize;
+		}
+
+		return position;
+	}
+
+	public int getMaxSize() {
+		return maxSize;
+	}
+
+	public int getAddr() {
+		return addr;
+	}
+
+	public boolean isEmpty() {
+		return currentSize == 0;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("pspFileBuffer(addr=0x%08X, maxSize=0x%X, currentSize=0x%X, filePosition=0x%X)", getAddr(), getMaxSize(), currentSize, getFilePosition());
+	}
+}

src/jpcsp/HLE/modules250/sceAtrac3plus.java
@@ -16,7 +16,6 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
 package jpcsp.HLE.modules250;
 
-import jpcsp.HLE.CanBeNull;
 import jpcsp.HLE.CheckArgument;
 import jpcsp.HLE.HLEFunction;
 import jpcsp.HLE.HLELogging;
@@ -24,6 +23,7 @@ import jpcsp.HLE.HLEUnimplemented;
 import jpcsp.HLE.TPointer;
 import jpcsp.HLE.TPointer32;
 import jpcsp.HLE.kernel.types.SceKernelErrors;
+import jpcsp.util.Utilities;
 
 @HLELogging
 public class sceAtrac3plus extends jpcsp.HLE.modules150.sceAtrac3plus {
@@ -70,7 +70,7 @@ public class sceAtrac3plus extends jpcsp.HLE.modules150.sceAtrac3plus {
 
     @HLEUnimplemented
     @HLEFunction(nid = 0xF6837A1A, version = 250, checkInsideInterrupt = true)
-    public int sceAtracSetMOutData(int unknown1, int unknown2, int unknown3, int unknown4, int unknown5, int unknown6) {
+    public int sceAtracSetMOutData(@CheckArgument("checkAtracID") int atID, int unknown2, int unknown3, int unknown4, int unknown5, int unknown6) {
         return 0;
     }
 
@@ -85,21 +85,21 @@ public class sceAtrac3plus extends jpcsp.HLE.modules150.sceAtrac3plus {
     	return hleSetHalfwayBufferAndGetID(MOutHalfBuffer, readSize, MOutHalfBufferSize, true);
     }
 
+    @HLEUnimplemented
     @HLEFunction(nid = 0x5622B7C1, version = 250, checkInsideInterrupt = true)
-    public int sceAtracSetAA3DataAndGetID(TPointer buffer, int bufferSize, int fileSize, @CanBeNull TPointer32 metadataSizeAddr) {
-    	int codecType = getCodecType(buffer.getAddress());
-        int atID = hleCreateAtracID(codecType);
-        if (atracIDs.containsKey(atID)) {
-            atracIDs.get(atID).setData(buffer.getAddress(), bufferSize, bufferSize, false);
-        }
-        metadataSizeAddr.setValue(0x400); // Dummy common value found in most .AA3 files.
-
-        return atID;
+    public int sceAtracSetAA3DataAndGetID(TPointer buffer, int bufferSize, int fileSize, int unused) {
+    	if (log.isDebugEnabled()) {
+    		log.debug(String.format("sceAtracSetAA3DataAndGetID buffer:%s", Utilities.getMemoryDump(buffer.getAddress(), bufferSize)));
+    	}
+    	return hleSetHalfwayBufferAndGetID(buffer, bufferSize, bufferSize, false);
     }
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x5DD66588, version = 250)
-    public int sceAtracSetAA3HalfwayBufferAndGetID(int unknown1, int unknown2, int unknown3, int unknown4, int unknown5, int unknown6) {
-        return 0;
+    public int sceAtracSetAA3HalfwayBufferAndGetID(TPointer buffer, int readSize, int bufferSize, int fileSize, int unused) {
+    	if (log.isDebugEnabled()) {
+    		log.debug(String.format("sceAtracSetAA3HalfwayBufferAndGetID buffer:%s", Utilities.getMemoryDump(buffer.getAddress(), readSize)));
+    	}
+    	return hleSetHalfwayBufferAndGetID(buffer, readSize, bufferSize, false);
     }
 }

src/jpcsp/HLE/modules620/sceAtrac3plus.java
@@ -55,7 +55,7 @@ public class sceAtrac3plus extends jpcsp.HLE.modules600.sceAtrac3plus {
         }
 
         int sourceBytesConsumed = 0;
-        if (id.getInputBufferAddr() == 0) {
+        if (id.getInputBuffer() == null) {
         	int headerAddr = findRIFFHeader(sourceAddr.getAddress());
         	if (headerAddr != 0) {
         		sourceBytesConsumed = id.getSourceBufferLength();
@@ -69,7 +69,7 @@ public class sceAtrac3plus extends jpcsp.HLE.modules600.sceAtrac3plus {
         } else {
         	// Estimate source bytes to be read based on current sample position
         	int estimatedFileOffset = (int) (((long) id.getInputFileSize()) * id.getAtracCurrentSample() / id.getAtracEndSample());
-        	sourceBytesConsumed = Math.max(0, estimatedFileOffset - id.getInputFileOffset());
+        	sourceBytesConsumed = Math.max(0, estimatedFileOffset - id.getInputBuffer().getFilePosition());
         	sourceBytesConsumed = Math.min(sourceBytesConsumed, id.getSourceBufferLength());
         	id.addStreamData(sourceAddr.getAddress(), sourceBytesConsumed);
         }
@@ -108,7 +108,7 @@ public class sceAtrac3plus extends jpcsp.HLE.modules600.sceAtrac3plus {
 	        sampleBytesAddr.setValue(sampleBytes);
 
 	        if (log.isDebugEnabled()) {
-	        	log.debug(String.format("sceAtracLowLevelDecode returning %d samples (0x%X bytes), 0x%X source bytes consumed, sample position %d/%d, file position %d/%d", samples, sampleBytes, sourceBytesConsumed, id.getAtracCurrentSample(), id.getAtracEndSample(), id.getInputFileOffset(), id.getInputFileSize()));
+	        	log.debug(String.format("sceAtracLowLevelDecode returning %d samples (0x%X bytes), 0x%X source bytes consumed, sample position %d/%d, file position %d/%d", samples, sampleBytes, sourceBytesConsumed, id.getAtracCurrentSample(), id.getAtracEndSample(), id.getInputBuffer().getFilePosition(), id.getInputFileSize()));
 	        	if (log.isTraceEnabled()) {
 	        		log.trace(Utilities.getMemoryDump(samplesAddr.getAddress(), sampleBytes));
 	        	}

src/jpcsp/connector/AtracCodec.java
@@ -61,7 +61,7 @@ public class AtracCodec {
     protected static final String decodedAtracSuffix = atracSuffix + decodedSuffix;
     protected RandomAccessFile decodedStream;
     protected OutputStream atracStream;
-    protected int atracEnd;
+    protected boolean atracEnd;
     protected int atracEndSample;
     protected int atracMaxSamples;
     protected int atracFileSize;
@@ -351,7 +351,7 @@ public class AtracCodec {
 
     public int atracDecodeData(int atracID, int address, int channels) {
         int samples = 0;
-        boolean isEnded = false;
+        atracEnd = false;
 
         if (checkMediaEngineState()) {
         	if (me.getContainer() == null && atracChannel != null) {
@@ -397,7 +397,7 @@ public class AtracCodec {
             	samples = copySamplesToMem(address);
             }
         	if (samples == 0) {
-        		isEnded = true;
+        		atracEnd = true;
         	}
         } else if (decodedStream != null) {
             try {
@@ -407,23 +407,17 @@ public class AtracCodec {
                     Memory.getInstance().copyToMemory(address, ByteBuffer.wrap(atracDecodeBuffer, 0, length), length);
                     long restLength = decodedStream.length() - decodedStream.getFilePointer();
                     if (restLength <= 0) {
-                    	isEnded = true;
+                    	atracEnd = true;
                     }
                 } else {
-                	isEnded = true;
+                	atracEnd = true;
                 }
             } catch (IOException e) {
                 Modules.log.warn(e);
             }
         } else {
             samples = -1;
-            isEnded = true;
-        }
-
-        if (isEnded) {
-        	atracEnd = 1;
-        } else {
-        	atracEnd = 0;
+            atracEnd = true;
         }
 
         return samples;
@@ -465,7 +459,7 @@ public class AtracCodec {
     	atracChannel.reset();
     }
 
-    public int getAtracEnd() {
+    public boolean getAtracEnd() {
         return atracEnd;
     }
 

src/jpcsp/sound/SampleSourceAtrac3.java
@@ -55,15 +55,15 @@ public class SampleSourceAtrac3 implements ISampleSource {
 	private void decode() {
 		bufferedSamples = codec.atracDecodeData(id.getAtracId(), buffer, id.getAtracOutputChannels());
 
-		if (id.getInputFileOffset() < id.getInputFileSize()) {
-			int requestedSize = min(id.getInputFileSize() - id.getInputFileOffset(), id.getInputBufferSize());
+		if (!id.getInputBuffer().isFileEnd()) {
+			int requestedSize = min(id.getInputFileSize() - id.getInputBuffer().getFilePosition(), id.getInputBuffer().getMaxSize());
 			id.setContextDecodeResult(-1, requestedSize);
 		} else {
 			id.setContextDecodeResult(0, 0);
 		}
 
 		if (log.isDebugEnabled()) {
-			log.debug(String.format("SampleSourceAtrac3 decode: bufferedSamples=%d, currentSample=%d, endSample=%d, isEnd=%d", bufferedSamples, getSampleIndex(), id.getAtracEndSample(), codec.getAtracEnd()));
+			log.debug(String.format("SampleSourceAtrac3 decode: bufferedSamples=%d, currentSample=%d, endSample=%d, isEnd=%b", bufferedSamples, getSampleIndex(), id.getAtracEndSample(), codec.getAtracEnd()));
 		}
 
 		sampleIndex = 0;

commit 234dcab14cdc127175f9e583a8f8691a2ff74bec

Fixed sceAtracDecode: remaining frames were incorrectly estimated (was broken by r3096).

src/jpcsp/HLE/kernel/types/pspFileBuffer.java
@@ -77,6 +77,10 @@ public class pspFileBuffer {
 		return min(currentSize, maxSize - readPosition);
 	}
 
+	public int getCurrentSize() {
+		return currentSize;
+	}
+
 	public void reset(int readSize, int filePosition) {
 		currentSize = 0;
 		readPosition = 0;

src/jpcsp/HLE/modules150/sceAtrac3plus.java
@@ -245,7 +245,7 @@ public class sceAtrac3plus extends HLEModule {
             Memory mem = Memory.getInstance();
 
             int currentAddr = inputBuffer.getReadAddr();
-            int bufferSize = inputBuffer.getReadSize();
+            int bufferSize = inputBuffer.getCurrentSize();
             atracEndSample = -1;
             atracCurrentSample = 0;
             isSecondBufferNeeded = false;
@@ -548,7 +548,7 @@ public class sceAtrac3plus extends HLEModule {
                 return inputBuffer.getMaxSize() / atracBytesPerFrame;
             }
 
-            int remainFrames = inputBuffer.getReadSize() / atracBytesPerFrame;
+            int remainFrames = inputBuffer.getCurrentSize() / atracBytesPerFrame;
 
             return remainFrames;
         }