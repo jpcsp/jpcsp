4dacc1ec36e9faade454421e37abcfb2dfcd8130
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2014-03-25 20:37:25 +0000

    New "IO Test" file for testing sceIoDread

---
+++ trunk/demos/src/io/main.c
@@ -0,0 +1,171 @@
+#include <pspkernel.h>
+#include <pspdebug.h>
+#include <pspctrl.h>
+#include <pspdisplay.h>
+#include <pspgu.h>
+#include <pspgum.h>
+#include <psppower.h>
+
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+PSP_MODULE_INFO("IO Tests", 0, 1, 0);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | PSP_THREAD_ATTR_VFPU);
+
+int done = 0;
+
+
+void runTest()
+{
+	int dfd = sceIoDopen("ms0:/tmp/");
+	if (dfd <= 0) {
+		pspDebugScreenPrintf("sceIoDopen = 0x%08X\n", dfd);
+		return;
+	}
+
+	SceIoDirent dir;
+	u8 buffer[1044];
+	while (1) {
+		memset(&dir, 0, sizeof(dir));
+		memset(buffer, 0, sizeof(buffer));
+		((u32 *) buffer)[0] = sizeof(buffer);
+		dir.d_private = buffer;
+		int result = sceIoDread(dfd, &dir);
+		pspDebugScreenPrintf("sceIoDread = 0x%08X\n", result);
+		if (result <= 0) {
+			break;
+		}
+		int i;
+		for (i = 0; i < sizeof(buffer); i++) {
+			if (buffer[i] != 0) {
+				pspDebugScreenPrintf("[%2d] = 0x%02X('%c') ", i, buffer[i], buffer[i]);
+			}
+		}
+		pspDebugScreenPrintf("\n");
+	}
+	sceIoDclose(dfd);
+}
+
+
+int main(int argc, char *argv[])
+{
+	SceCtrlData pad;
+	int oldButtons = 0;
+#define SECOND	   1000000
+#define REPEAT_START (1 * SECOND)
+#define REPEAT_DELAY (SECOND / 5)
+	struct timeval repeatStart;
+	struct timeval repeatDelay;
+
+	repeatStart.tv_sec = 0;
+	repeatStart.tv_usec = 0;
+	repeatDelay.tv_sec = 0;
+	repeatDelay.tv_usec = 0;
+
+	pspDebugScreenInit();
+	pspDebugScreenPrintf("Press Cross to start the IO Test\n");
+
+	while(!done)
+	{
+		sceCtrlReadBufferPositive(&pad, 1);
+		int buttonDown = (oldButtons ^ pad.Buttons) & pad.Buttons;
+
+		if (pad.Buttons == oldButtons)
+		{
+			struct timeval now;
+			gettimeofday(&now, NULL);
+			if (repeatStart.tv_sec == 0)
+			{
+				repeatStart.tv_sec = now.tv_sec;
+				repeatStart.tv_usec = now.tv_usec;
+				repeatDelay.tv_sec = 0;
+				repeatDelay.tv_usec = 0;
+			}
+			else
+			{
+				long usec = (now.tv_sec - repeatStart.tv_sec) * SECOND;
+				usec += (now.tv_usec - repeatStart.tv_usec);
+				if (usec >= REPEAT_START)
+				{
+					if (repeatDelay.tv_sec != 0)
+					{
+						usec = (now.tv_sec - repeatDelay.tv_sec) * SECOND;
+						usec += (now.tv_usec - repeatDelay.tv_usec);
+						if (usec >= REPEAT_DELAY)
+						{
+							repeatDelay.tv_sec = 0;
+						}
+					}
+
+					if (repeatDelay.tv_sec == 0)
+					{
+						buttonDown = pad.Buttons;
+						repeatDelay.tv_sec = now.tv_sec;
+						repeatDelay.tv_usec = now.tv_usec;
+					}
+				}
+			}
+		}
+		else
+		{
+			repeatStart.tv_sec = 0;
+		}
+
+		if (buttonDown & PSP_CTRL_CROSS)
+		{
+			sceKernelSetCompiledSdkVersion(0x307FFFF);
+			runTest();
+			sceKernelSetCompiledSdkVersion(0x307FFFF + 1);
+			runTest();
+		}
+
+		if (buttonDown & PSP_CTRL_TRIANGLE)
+		{
+			done = 1;
+		}
+
+		oldButtons = pad.Buttons;
+	}
+
+	sceGuTerm();
+
+	sceKernelExitGame();
+	return 0;
+}
+
+/* Exit callback */
+int exit_callback(int arg1, int arg2, void *common)
+{
+	done = 1;
+	return 0;
+}
+
+/* Callback thread */
+int CallbackThread(SceSize args, void *argp)
+{
+	int cbid;
+
+	cbid = sceKernelCreateCallback("Exit Callback", exit_callback, (void*)0);
+	sceKernelRegisterExitCallback(cbid);
+
+	sceKernelSleepThreadCB();
+
+	return 0;
+}
+
+/* Sets up the callback thread and returns its thread id */
+int SetupCallbacks(void)
+{
+	int thid = 0;
+
+	thid = sceKernelCreateThread("CallbackThread", CallbackThread, 0x11, 0xFA0, 0, 0);
+	if(thid >= 0)
+	{
+		sceKernelStartThread(thid, 0, 0);
+	}
+
+	return thid;
+}
+
