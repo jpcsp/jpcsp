715cb78668df868b64f8902d2a6fdbb7ca5df716
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2014-03-26 20:31:58 +0000

    Added empty implementation for sceMp3LowLevelInit, sceMp3LowLevelDecode

--- trunk/src/jpcsp/HLE/modules/sceMp3.java
+++ trunk/src/jpcsp/HLE/modules/sceMp3.java
@@ -16,5 +16,5 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
 package jpcsp.HLE.modules;
 
-public class sceMp3 extends jpcsp.HLE.modules150.sceMp3 {
+public class sceMp3 extends jpcsp.HLE.modules620.sceMp3 {
 }
