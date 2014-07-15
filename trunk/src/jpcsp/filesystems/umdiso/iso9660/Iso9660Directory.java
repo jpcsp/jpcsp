899354b282ea8c85830e373fb47950bde6b28c1f
gid15@web.de <gid15@web.de@4ab978e6-3352-0410-996c-0b42db0051c8>
2014-07-15 20:52:05 +0000

    Fixed reading of large files within ISO (file size >= 0x80000000).

--- trunk/src/jpcsp/filesystems/umdiso/iso9660/Iso9660Directory.java
+++ trunk/src/jpcsp/filesystems/umdiso/iso9660/Iso9660Directory.java
@@ -31,7 +31,7 @@ import jpcsp.filesystems.umdiso.UmdIsoReader;
 public class Iso9660Directory {
     private final List<Iso9660File> files;
 
-    public Iso9660Directory(UmdIsoReader r, int directorySector, int directorySize) throws IOException {
+    public Iso9660Directory(UmdIsoReader r, int directorySector, long directorySize) throws IOException {
         // parse directory sector
         UmdIsoFile dataStream = new UmdIsoFile(r, directorySector, directorySize, null, null);
 
