jpcsp/jpcsp

commit 5f0147eb0e0504ce1973a5f6b62ec65ab4eef444

Always store the GE and FB in a 32-bit texture, 16-bit textures are causing color artifacts.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3588 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/graphics/RE/REShader.java
@@ -16,6 +16,7 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
 package jpcsp.graphics.RE;
 
+import static jpcsp.HLE.modules150.sceDisplay.getTexturePixelFormat;
 import static jpcsp.graphics.GeCommands.TPSM_PIXEL_STORAGE_MODE_4BIT_INDEXED;
 
 import java.io.IOException;
@@ -732,7 +733,7 @@ public class REShader extends BaseRenderingEngineFunction {
 
 			// Activate the rendering to a texture
 			if (renderTexture == null) {
-				renderTexture = new FBTexture(display.getTopAddrFb(), bufferWidth, width, height, pixelFormat);
+				renderTexture = new FBTexture(display.getTopAddrFb(), bufferWidth, width, height, getTexturePixelFormat(pixelFormat));
 				renderTexture.bind(re, false);
 				re.bindActiveTexture(ACTIVE_TEXTURE_FRAMEBUFFER, renderTexture.getTextureId());
 			} else {

src/jpcsp/HLE/modules150/sceDisplay.java
@@ -1414,14 +1414,15 @@ public class sceDisplay extends HLEModule {
         // Create a GE texture (the texture texFb might not have the right size)
         int texGe = re.genTexture();
 
+        int texturePixelFormat = getTexturePixelFormat(ge.getPixelFormat());
         re.bindTexture(texGe);
         re.setTextureFormat(ge.getPixelFormat(), false);
         re.setTexImage(0,
                 internalTextureFormat,
                 getResizedWidthPow2(ge.getBufferWidth()),
                 getResizedHeightPow2(Utilities.makePow2(ge.getHeight())),
-                ge.getPixelFormat(),
-                ge.getPixelFormat(),
+                texturePixelFormat,
+                texturePixelFormat,
                 0, null);
 
         re.setTextureMipmapMinFilter(TFLT_NEAREST);
@@ -1436,10 +1437,10 @@ public class sceDisplay extends HLEModule {
 
         // Copy the GE texture into temp buffer
         temp.clear();
-        re.getTexImage(0, ge.getPixelFormat(), ge.getPixelFormat(), temp);
+        re.getTexImage(0, texturePixelFormat, texturePixelFormat, temp);
 
         // Capture the GE image
-        CaptureManager.captureImage(ge.getTopAddr(), 0, temp, getResizedWidth(ge.getWidth()), getResizedHeight(ge.getHeight()), getResizedWidthPow2(ge.getBufferWidth()), ge.getPixelFormat(), false, 0, true, false);
+        CaptureManager.captureImage(ge.getTopAddr(), 0, temp, getResizedWidth(ge.getWidth()), getResizedHeight(ge.getHeight()), getResizedWidthPow2(ge.getBufferWidth()), texturePixelFormat, false, 0, true, false);
 
         // Delete the GE texture
         re.deleteTexture(texGe);
@@ -1878,6 +1879,13 @@ public class sceDisplay extends HLEModule {
         return vcount;
     }
 
+    public static int getTexturePixelFormat(int pixelFormat) {
+//    	return pixelFormat;
+		// Always use a 32-bit texture to store the GE.
+		// 16-bit textures are causing color artifacts.
+    	return GeCommands.TPSM_PIXEL_STORAGE_MODE_32BIT_ABGR8888;
+    }
+
     private int createTexture(int textureId, boolean isResized) {
         if (textureId != -1) {
             reDisplay.deleteTexture(textureId);
@@ -1903,8 +1911,8 @@ public class sceDisplay extends HLEModule {
                 internalTextureFormat,
                 isResized ? getResizedWidthPow2(fb.getBufferWidth()) : fb.getBufferWidth(),
                 isResized ? getResizedHeightPow2(Utilities.makePow2(fb.getHeight())) : Utilities.makePow2(fb.getHeight()),
-                fb.getPixelFormat(),
-                fb.getPixelFormat(),
+                getTexturePixelFormat(getPixelFormatFb()),
+                getTexturePixelFormat(getPixelFormatFb()),
                 0, null);
         reDisplay.setTextureMipmapMinFilter(GeCommands.TFLT_NEAREST);
         reDisplay.setTextureMipmapMagFilter(GeCommands.TFLT_NEAREST);

src/jpcsp/graphics/textures/GETextureManager.java
@@ -16,6 +16,8 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
 package jpcsp.graphics.textures;
 
+import static jpcsp.HLE.modules150.sceDisplay.getTexturePixelFormat;
+
 import java.util.HashMap;
 
 import jpcsp.graphics.GeCommands;
@@ -63,14 +65,8 @@ public class GETextureManager {
 		return geTexture;
 	}
 
-	private int getGePixelFormat(int pixelFormat) {
-		// Always use a 32-bit texture to store the GE.
-		// 16-bit textures are causing color artifacts.
-		return GeCommands.TPSM_PIXEL_STORAGE_MODE_32BIT_ABGR8888;
-	}
-
 	public GETexture getGETexture(IRenderingEngine re, int address, int bufferWidth, int width, int height, int pixelFormat, boolean useViewportResize) {
-		int gePixelFormat = getGePixelFormat(pixelFormat);
+		int gePixelFormat = getTexturePixelFormat(pixelFormat);
 		GETexture geTexture = checkGETexturePSM8888(address, bufferWidth, width, height, pixelFormat);
 		if (geTexture == null) {
 			geTexture = checkGETexture(address, bufferWidth, width, height, pixelFormat);
@@ -86,7 +82,7 @@ public class GETextureManager {
 	}
 
 	public GETexture getGEResizedTexture(IRenderingEngine re, GETexture baseGETexture, int address, int bufferWidth, int width, int height, int pixelFormat) {
-		int gePixelFormat = getGePixelFormat(pixelFormat);
+		int gePixelFormat = getTexturePixelFormat(pixelFormat);
 		GETexture geTexture = checkGETexturePSM8888(address, bufferWidth, width, height, pixelFormat);
 		if (geTexture == null) {
 			geTexture = checkGETexture(address, bufferWidth, width, height, pixelFormat);

commit 0ee573c0af1dc65f2bf3622d7c5c8b27fe71978b

Fixed the performance issue introduced by r3588 when enabling the shader implementation for the "Stencil test", at least it should now be back to the previous behavior.

src/jpcsp/graphics/RE/DebugProxy.java
@@ -21,6 +21,7 @@ import java.nio.IntBuffer;
 
 import jpcsp.graphics.Uniforms;
 import jpcsp.graphics.VideoEngine;
+import jpcsp.graphics.RE.software.PixelColor;
 
 /**
  * @author gid15
@@ -85,6 +86,14 @@ public class DebugProxy extends BaseRenderingEngineProxy {
 			log.debug(String.format("setBlendFunc src=%d, dst=%d", src, dst));
 		}
 		super.setBlendFunc(src, dst);
+	}
+
+	@Override
+	public void setBlendColor(float[] color) {
+		if (isLogDebugEnabled) {
+			log.debug(String.format("setBlendColor color=0x%08X", PixelColor.getColor(color)));
+		}
+		super.setBlendColor(color);
 	}
 
 	@Override

src/jpcsp/graphics/RE/REShader.java
@@ -970,7 +970,7 @@ public class REShader extends BaseRenderingEngineFunction {
 
 		if (useRenderToTexture) {
 			// Use the render texture if it is compatible with the current GE settings.
-			if (renderTexture.getResizedWidth() >= width && renderTexture.getResizedHeight() >= height && renderTexture.getBufferWidth() >= bufferWidth && renderTexture.getPixelFormat() == pixelFormat) {
+			if (renderTexture.getResizedWidth() >= width && renderTexture.getResizedHeight() >= height && renderTexture.getBufferWidth() >= bufferWidth) {
 				// Tell the shader which texture has to be used for the fbTex sampler.
 				re.bindActiveTexture(ACTIVE_TEXTURE_FRAMEBUFFER, renderTexture.getTextureId());
 				return;

src/jpcsp/graphics/textures/GETexture.java
@@ -373,7 +373,7 @@ public class GETexture {
 	}
 
 	public boolean isCompatible(int width, int height, int bufferWidth, int pixelFormat) {
-		if (width != this.width || height != this.height || bufferWidth != this.bufferWidth || pixelFormat != this.pixelFormat) {
+		if (width != this.width || height != this.height || bufferWidth != this.bufferWidth) {
 			return false;
 		}
 