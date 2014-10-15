jpcsp/jpcsp

commit 03bab86d630e9cdb59d1c03af917bd822a130aa9

Hack to workaround audio synchronization issues in videos for some applications.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3645 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/modules150/sceDisplay.java
@@ -569,6 +569,8 @@ public class sceDisplay extends HLEModule {
     private static final int[] stencilPixelMasks = new int[]{0, 0x7FFF, 0x0FFF, 0x00FFFFFF};
     private static final int[] stencilValueMasks = new int[]{0, 0x80, 0xF0, 0xFF};
     private static final int[] stencilValueShifts = new int[]{0, 8, 8, 24};
+    // Mpeg audio hack
+    private int framePerSecFactor;
 
     private class OnlyGeSettingsListener extends AbstractBoolSettingsListener {
 
@@ -952,6 +954,13 @@ public class sceDisplay extends HLEModule {
 
     private void setAntiAliasSamplesNum(int samples) {
         antiAliasSamplesNum = samples;
+    }
+
+    public void setFramePerSecFactor(int framePerSecFactor) {
+    	if (log.isInfoEnabled()) {
+    		log.info(String.format("setFramePerSecFactor %d", framePerSecFactor));
+    	}
+    	this.framePerSecFactor = framePerSecFactor;
     }
 
     @Override
@@ -2013,7 +2022,29 @@ public class sceDisplay extends HLEModule {
 
     @HLEFunction(nid = 0xDBA6C4C4, version = 150)
     public float sceDisplayGetFramePerSec() {
-        return FRAME_PER_SEC;
+    	// Some applications are using a video playback loop requiring a very exact
+    	// audio synchronization: the video playback keeps 4 buffers each for video
+    	// images and for decoded audio buffers. When the buffer timestamps between
+    	// audio and video differ by a too long delay value, the video playback breaks.
+    	// The application actually tries to skip video frames to sync up but usually
+    	// fails to it.
+    	//
+    	// The synchronization problem in Jpcsp is caused by the audio queues in sceAudio:
+    	// the blocking methods (sceAudioOutputXXXBlocking) are not blocking as much as the
+    	// PSP methods are doing. Especially, the first few calls (until the Jpcsp queue is
+    	// filled up) are not blocking at all. This is causing the audio to play faster than
+    	// the video at the beginning.
+    	//
+    	// The allowed delay is computed as follows:
+    	//    2 * int(sceMpeg.mpegTimestampPerSecond / sceDisplayGetFramePerSec() * 2) = 6006
+    	//
+    	// Allow artificially a longer delay by returning here a lower value:
+        float framePerSec = FRAME_PER_SEC / framePerSecFactor;
+
+        // The hack is only used once
+        framePerSecFactor = 1;
+
+        return framePerSec;
     }
 
     @HLEUnimplemented

src/jpcsp/Allegrex/compiler/nativeCode/Mpeg.java
@@ -1,0 +1,26 @@
+/*
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.Allegrex.compiler.nativeCode;
+
+import jpcsp.HLE.Modules;
+
+public class Mpeg {
+	public static void soundDelayHack() {
+		// See comment in sceDisplay.sceDisplayGetFramePerSec()
+		Modules.sceDisplayModule.setFramePerSecFactor(3);
+	}
+}

commit 14f739b1bed60a65f1845950b0b74c4d5e1cd4af

Fixed issue introduced by r3645

src/jpcsp/HLE/modules150/sceDisplay.java
@@ -1016,6 +1016,7 @@ public class sceDisplay extends HLEModule {
         prevFrameCount = 0;
         reportCount = 0;
         insideRendering = false;
+        framePerSecFactor = 1;
 
         vcount = 0;
 