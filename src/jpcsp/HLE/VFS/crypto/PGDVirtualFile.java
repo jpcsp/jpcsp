jpcsp/jpcsp

commit e8ca352a90edeee73847f4108864933a8fd53aaf

Implemented PGD and DLC (Drm) on-the-fly decryption: this avoids creating temporary files under tmp and is also supporting the decryption of very large EDAT files. Fixed sceIoChstat: some operations always fail on windows.

src/jpcsp/HLE/VFS/crypto/PGDVirtualFile.java
@@ -1,0 +1,57 @@
+/*
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.HLE.VFS.crypto;
+
+import jpcsp.HLE.VFS.BufferedVirtualFile;
+import jpcsp.HLE.VFS.IVirtualFile;
+
+public class PGDVirtualFile extends BufferedVirtualFile {
+	private boolean isValid;
+
+	public PGDVirtualFile(byte[] key, IVirtualFile pgdFile) {
+		init(key, pgdFile, 0);
+	}
+
+	protected PGDVirtualFile(byte[] key, IVirtualFile pgdFile, int dataOffset) {
+		init(key, pgdFile, dataOffset);
+	}
+
+	private void init(byte[] key, IVirtualFile pgdFile, int dataOffset) {
+		isValid = false;
+
+		if (isHeaderValid(pgdFile)) {
+			PGDBlockVirtualFile pgdBlockFile = new PGDBlockVirtualFile(pgdFile, key, dataOffset);
+
+			if (pgdBlockFile.isHeaderValid()) {
+				setBufferedVirtualFile(pgdBlockFile, pgdBlockFile.getBlockSize());
+				isValid = true;
+			}
+		}
+
+		if (!isValid) {
+			setBufferedVirtualFile(pgdFile, 0x1000);
+		}
+	}
+
+	protected boolean isHeaderValid(IVirtualFile pgdFile) {
+		return true;
+	}
+
+	public boolean isValid() {
+		return isValid;
+	}
+}

src/jpcsp/HLE/VFS/AbstractProxyVirtualFile.java
@@ -16,7 +16,10 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
 package jpcsp.HLE.VFS;
 
+import org.apache.log4j.Logger;
+
 import jpcsp.HLE.TPointer;
+import jpcsp.util.Utilities;
 
 /**
  * Proxy all the IVirtualFile interface calls to another virtual file.
@@ -25,6 +28,7 @@ import jpcsp.HLE.TPointer;
  *
  */
 public abstract class AbstractProxyVirtualFile implements IVirtualFile {
+	protected static Logger log = AbstractVirtualFileSystem.log;
 	protected IVirtualFile vFile;
 
 	protected AbstractProxyVirtualFile() {
@@ -46,6 +50,21 @@ public abstract class AbstractProxyVirtualFile implements IVirtualFile {
 	@Override
 	public int ioRead(TPointer outputPointer, int outputLength) {
 		return vFile.ioRead(outputPointer, outputLength);
+	}
+
+	/*
+	 * Perform the ioRead in PSP memory using the ioRead for a byte array.
+	 */
+	protected int ioReadBuf(TPointer outputPointer, int outputLength) {
+		if (outputLength <= 0) {
+			return 0;
+		}
+
+		byte[] outputBuffer = new byte[outputLength];
+		int readLength = ioRead(outputBuffer, 0, outputLength);
+		Utilities.writeBytes(outputPointer.getAddress(), readLength, outputBuffer, 0);
+
+		return readLength;
 	}
 
 	@Override

src/jpcsp/HLE/VFS/BufferedVirtualFile.java
@@ -33,9 +33,18 @@ public class BufferedVirtualFile extends AbstractProxyVirtualFile {
 	private int bufferIndex;
 	private int bufferLength;
 
+	protected BufferedVirtualFile() {
+	}
+
 	public BufferedVirtualFile(IVirtualFile vFile, int bufferSize) {
-		super(vFile);
+		setBufferedVirtualFile(vFile, bufferSize);
+	}
+
+	protected void setBufferedVirtualFile(IVirtualFile vFile, int bufferSize) {
+		setProxyVirtualFile(vFile);
 		buffer = new byte[bufferSize];
+		bufferIndex = 0;
+		bufferLength = 0;
 	}
 
 	private void copyFromBuffer(int outputAddr, int length) {
@@ -61,8 +70,10 @@ public class BufferedVirtualFile extends AbstractProxyVirtualFile {
 			return;
 		}
 
+		if (bufferLength > 0) {
+			bufferIndex = 0;
+		}
 		bufferLength = vFile.ioRead(buffer, 0, buffer.length);
-		bufferIndex = 0;
 	}
 
 	@Override
@@ -124,7 +135,6 @@ public class BufferedVirtualFile extends AbstractProxyVirtualFile {
 		bufferLength = 0;
 		bufferIndex = 0;
 		if (offset > virtualFileOffset) {
-			checkPopulateBuffer();
 			bufferIndex = (int) (offset - virtualFileOffset);
 		}
 

src/jpcsp/HLE/VFS/ITmpVirtualFileSystem.java
@@ -1,77 +1,0 @@
-/*
-This file is part of jpcsp.
-
-Jpcsp is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Jpcsp is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
- */
-package jpcsp.HLE.VFS;
-
-import static java.io.File.separatorChar;
-import static java.lang.Math.abs;
-
-import jpcsp.HLE.Modules;
-import jpcsp.HLE.kernel.types.SceIoStat;
-import jpcsp.settings.Settings;
-
-public interface ITmpVirtualFileSystem extends IVirtualFileSystem {
-	public static final IPurpose tmpPurposePGD = new PurposePGD();
-	public static final IPurpose tmpPurposeAtrac = new PurposeAtrac();
-	public IVirtualFile ioOpen(String fileName, int flags, int mode, IPurpose purpose);
-	public IVirtualFile ioOpen(String fileName, int flags, int mode, IPurpose purpose, IVirtualFile originalFile);
-
-	public static interface IPurpose {
-		public String getFileName(String fileName);
-	}
-
-	public static abstract class AbstractPurpose implements IPurpose {
-		protected String getFileName(String dir, String fileName) {
-			return String.format("%s%s%c%s", Settings.getInstance().getDiscDirectory(), dir, separatorChar, fileName);
-		}
-
-		protected String getFileName(String dir1, String dir2, String fileName) {
-			return String.format("%s%s%c%s%c%s", Settings.getInstance().getDiscDirectory(), dir1, separatorChar, dir2, separatorChar, fileName);
-		}
-
-		protected String getFileNameById(String fileName) {
-			int fileId = 0;
-
-			SceIoStat stat = Modules.IoFileMgrForUserModule.statFile(fileName);
-			if (stat != null) {
-				// Use the UMD start sector as file ID.
-				fileId = stat.getStartSector();
-			}
-
-			if (fileId == 0 && fileName != null) {
-				// If the file is not stored on the UMD (e.g. stored on ms0:),
-				// use a unique ID based on the file name as file ID.
-				fileId = abs(VirtualFileSystemManager.getFileNameLastPart(fileName).hashCode());
-			}
-
-			return String.format("File-%d", fileId);
-		}
-	}
-
-	public static class PurposePGD extends AbstractPurpose {
-		@Override
-		public String getFileName(String fileName) {
-			return getFileName("PGD", getFileNameById(fileName), "PGDfile.raw.decrypted");
-		}
-	}
-
-	public static class PurposeAtrac extends AbstractPurpose {
-		@Override
-		public String getFileName(String fileName) {
-			return getFileName("Atrac", fileName);
-		}
-	}
-}

src/jpcsp/HLE/VFS/SeekableDataInputVirtualFile.java
@@ -14,14 +14,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
-package jpcsp.connector;
+package jpcsp.HLE.VFS;
 
-/**
- * @author gid15
- *
- */
-public class Connector {
-	public static final String basePSPDirectory = "ms0:/tmp/";
-	public static final String jpcspConnectorName = "Jpcsp Connector 3xx";
-	public static final String commandFileName = "command.txt";
+import jpcsp.filesystems.SeekableDataInput;
+
+public class SeekableDataInputVirtualFile extends AbstractVirtualFile {
+	public SeekableDataInputVirtualFile(SeekableDataInput file) {
+		super(file);
+	}
 }

src/jpcsp/HLE/VFS/VirtualFileSystemManager.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 
 public class VirtualFileSystemManager {
 	protected HashMap<String, IVirtualFileSystem> virtualFileSystems = new HashMap<String, IVirtualFileSystem>();
-	protected ITmpVirtualFileSystem tmpVfs;
 
 	public void register(String name, IVirtualFileSystem vfs) {
 		name = name.toLowerCase();
@@ -30,10 +29,6 @@ public class VirtualFileSystemManager {
 	public void unregister(String name) {
 		name = name.toLowerCase();
 		virtualFileSystems.remove(name);
-	}
-
-	public void register(ITmpVirtualFileSystem tmpVfs) {
-		this.tmpVfs = tmpVfs;
 	}
 
 	public IVirtualFileSystem getVirtualFileSystem(String absoluteFileName, StringBuilder localFileName) {
@@ -53,10 +48,6 @@ public class VirtualFileSystemManager {
 		}
 
 		return virtualFileSystems.get(name);
-	}
-
-	public ITmpVirtualFileSystem getTmpVirtualFileSystem() {
-		return tmpVfs;
 	}
 
 	/**

src/jpcsp/HLE/VFS/crypto/EDATVirtualFile.java
@@ -1,0 +1,45 @@
+/*
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.HLE.VFS.crypto;
+
+import jpcsp.HLE.VFS.IVirtualFile;
+
+public class EDATVirtualFile extends PGDVirtualFile {
+	private static final int edatHeaderSize = 0x90;
+
+	public EDATVirtualFile(IVirtualFile pgdFile) {
+		super(null, pgdFile, edatHeaderSize);
+	}
+
+	@Override
+	protected boolean isHeaderValid(IVirtualFile pgdFile) {
+		byte[] header = new byte[edatHeaderSize];
+		int length = pgdFile.ioRead(header, 0, edatHeaderSize);
+
+		if (length != edatHeaderSize) {
+			return false;
+		}
+
+		if (header[0] != 0 || header[1] != 'P' || header[2] != 'S' || header[3] != 'P' || header[4] != 'E' || header[5] != 'D' || header[6] != 'A' || header[7] != 'T') {
+            // No "EDAT" found in the header,
+            log.warn("PSPEDAT header not found!");
+            return false;
+        }
+
+		return super.isHeaderValid(pgdFile);
+	}
+}

src/jpcsp/HLE/VFS/crypto/PGDBlockVirtualFile.java
@@ -1,0 +1,180 @@
+/*
+This file is part of jpcsp.
+
+Jpcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Jpcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jpcsp.HLE.VFS.crypto;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
+import jpcsp.HLE.TPointer;
+import jpcsp.HLE.VFS.AbstractProxyVirtualFile;
+import jpcsp.HLE.VFS.IVirtualFile;
+import jpcsp.crypto.CryptoEngine;
+import jpcsp.crypto.PGD;
+import jpcsp.util.Utilities;
+
+public class PGDBlockVirtualFile extends AbstractProxyVirtualFile {
+	private static final int pgdHeaderSize = 0x90;
+	private byte[] key;
+	private int dataOffset;
+	private int dataSize;
+	private int blockSize;
+	private boolean headerValid;
+	private byte[] buffer;
+	private byte[] header;
+	private PGD pgd;
+	private boolean sequentialRead;
+
+	public PGDBlockVirtualFile(IVirtualFile pgdFile, byte[] key, int dataOffset) {
+		super(pgdFile);
+
+		if (key != null) {
+			this.key = key.clone();
+		}
+		this.dataOffset = dataOffset;
+
+		super.ioLseek(dataOffset);
+
+		pgd = new CryptoEngine().getPGDEngine();
+
+		readHeader();
+
+		this.dataOffset += dataOffset;
+	}
+
+	private void readHeader() {
+		headerValid = false;
+		byte[] inBuf = new byte[pgdHeaderSize];
+		super.ioRead(inBuf, 0, pgdHeaderSize);
+
+		// Check if the "PGD" header is present
+        if (inBuf[0] != 0 || inBuf[1] != 'P' || inBuf[2] != 'G' || inBuf[3] != 'D') {
+            // No "PGD" found in the header,
+            // abort the decryption and leave the file unchanged
+            log.warn(String.format("No PGD header detected %02X %02X %02X %02X ('%c%c%c%c') detected in file '%s'", inBuf[0] & 0xFF, inBuf[1] & 0xFF, inBuf[2] & 0xFF, inBuf[3] & 0xFF, (char) inBuf[0], (char) inBuf[1], (char) inBuf[2], (char) inBuf[3], vFile));
+            return;
+        }
+
+        // Decrypt 0x30 bytes at offset 0x30 to expose the first header.
+        byte[] headerBuf = new byte[0x30 + 0x10];
+        System.arraycopy(inBuf, 0x10, headerBuf, 0, 0x10);
+        System.arraycopy(inBuf, 0x30, headerBuf, 0x10, 0x30);
+        if (key == null) {
+        	key = pgd.GetEDATPGDKey(inBuf, pgdHeaderSize);
+        }
+        header = pgd.DecryptPGD(headerBuf, headerBuf.length, key, 0);
+
+        // Extract the decryption parameters.
+        IntBuffer decryptedHeader = ByteBuffer.wrap(header).order(ByteOrder.LITTLE_ENDIAN).asIntBuffer();
+        dataSize = decryptedHeader.get(5);
+        blockSize = decryptedHeader.get(6);
+        dataOffset = decryptedHeader.get(7);
+        if (log.isDebugEnabled()) {
+            log.debug(String.format("PGD dataSize=%d, blockSize=%d, dataOffset=%d", dataSize, blockSize, dataOffset));
+            if (log.isTraceEnabled()) {
+                log.trace(String.format("PGD Header: %s", Utilities.getMemoryDump(inBuf, 0, pgdHeaderSize)));
+                log.trace(String.format("Decrypted PGD Header: %s", Utilities.getMemoryDump(header, 0, header.length)));
+            }
+        }
+
+        if (dataOffset < 0 || dataOffset > super.length() || dataSize < 0) {
+            // The decrypted PGD header is incorrect...
+            // abort the decryption and leave the file unchanged
+            log.warn(String.format("Incorrect PGD header: dataSize=%d, chunkSize=%d, hashOffset=%d", dataSize, blockSize, dataOffset));
+            return;
+        }
+
+        buffer = new byte[blockSize + 0x10];
+
+        headerValid = true;
+        sequentialRead = false;
+	}
+
+	public int getBlockSize() {
+		return blockSize;
+	}
+
+	public boolean isHeaderValid() {
+		return headerValid;
+	}
+
+	@Override
+	public int ioRead(byte[] outputBuffer, int outputOffset, int outputLength) {
+		int seed = 0;
+		if (!sequentialRead) {
+			seed = (int) (getPosition() >> 4);
+		}
+
+		int readLength = blockSize;
+		super.ioRead(buffer, 0x10, readLength);
+		System.arraycopy(header, 0, buffer, 0, 0x10);
+
+        byte[] decryptedBytes;
+        if (sequentialRead) {
+        	// This operation is more efficient than a complete DecryptPGD() call
+        	// but can only be used when decrypting sequential packets.
+        	decryptedBytes = pgd.UpdatePGDCipher(buffer, readLength + 0x10);
+        } else {
+        	pgd.FinishPGDCipher();
+        	decryptedBytes = pgd.DecryptPGD(buffer, readLength + 0x10, key, seed);
+        	sequentialRead = true;
+        }
+        int length = Math.min(outputLength, decryptedBytes.length);
+        System.arraycopy(decryptedBytes, 0, outputBuffer, outputOffset, length);
+
+        if (log.isTraceEnabled()) {
+        	log.trace(String.format("PGDBlockVirtualFile.ioRead length=0x%X: %s", length, Utilities.getMemoryDump(decryptedBytes, 0, length)));
+        }
+
+        return length;
+	}
+
+	@Override
+	public int ioRead(TPointer outputPointer, int outputLength) {
+		return ioReadBuf(outputPointer, outputLength);
+	}
+
+	@Override
+	public long ioLseek(long offset) {
+		long result = super.ioLseek(dataOffset + offset);
+		if (result >= 0 && result >= dataOffset) {
+			result -= dataOffset;
+		}
+
+		if (log.isTraceEnabled()) {
+			log.trace(String.format("PGDBlockVirtualFile.ioLseek offset=0x%X, result=0x%X", offset, result));
+		}
+
+		sequentialRead = false;
+
+		return result;
+	}
+
+	@Override
+	public long getPosition() {
+		long position = super.getPosition();
+		if (position >= 0 && position >= dataOffset) {
+			position -= dataOffset;
+		}
+		return position;
+	}
+
+	@Override
+	public long length() {
+		return dataSize;
+	}
+}

commit 0eb80e80551b59db568dec19a27dbb7608327c99

Fixed issue introduced by e8ca352 where PGD files with incorrect PGD header (e.g. decrypted PGD fles) were no longer recognized.

src/jpcsp/HLE/VFS/crypto/PGDVirtualFile.java
@@ -33,6 +33,7 @@ public class PGDVirtualFile extends BufferedVirtualFile {
 	private void init(byte[] key, IVirtualFile pgdFile, int dataOffset) {
 		isValid = false;
 
+		long position = pgdFile.getPosition();
 		if (isHeaderValid(pgdFile)) {
 			PGDBlockVirtualFile pgdBlockFile = new PGDBlockVirtualFile(pgdFile, key, dataOffset);
 
@@ -43,6 +44,7 @@ public class PGDVirtualFile extends BufferedVirtualFile {
 		}
 
 		if (!isValid) {
+			pgdFile.ioLseek(position);
 			setBufferedVirtualFile(pgdFile, 0x1000);
 		}
 	}