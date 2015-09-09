3c25717e04c66a1f5a93e0cacc2c232ecf2d2453
gid15@web.de <gid15@web.de>
2015-09-09 18:10:42 +0200

    Implemented sceKernelCreateHeap, sceKernelAllocHeapMemory, sceKernelFreeHeapMemory, sceKernelDeleteHeap

--- src/jpcsp/HLE/modules150/SysMemForKernel.java
+++ src/jpcsp/HLE/modules150/SysMemForKernel.java
@@ -16,25 +16,105 @@ along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
 package jpcsp.HLE.modules150;
 
+import java.util.HashMap;
+
+import jpcsp.Memory;
 import jpcsp.HLE.HLEFunction;
 import jpcsp.HLE.HLELogging;
-import jpcsp.HLE.HLEUnimplemented;
 import jpcsp.HLE.TPointer;
 import jpcsp.HLE.Modules;
+import jpcsp.HLE.kernel.managers.SceUidManager;
+import jpcsp.HLE.kernel.types.MemoryChunk;
+import jpcsp.HLE.kernel.types.MemoryChunkList;
 import jpcsp.HLE.modules.HLEModule;
+import jpcsp.HLE.modules150.SysMemUserForUser.SysMemInfo;
 
 import org.apache.log4j.Logger;
 
 @HLELogging
 public class SysMemForKernel extends HLEModule {
     public static Logger log = Modules.getLogger("SysMemForKernel");
+    protected HashMap<Integer, HeapInformation> heaps;
+
+    protected static class HeapInformation {
+    	private static final String uidPurpose = "SysMemForKernel-Heap";
+    	private static final int HEAP_BLOCK_HEADER_SIZE = 8;
+    	protected final int uid;
+		protected final int partitionId;
+    	protected final int size;
+    	protected final int flags;
+    	protected final String name;
+    	protected SysMemInfo sysMemInfo;
+    	protected int allocatedSize;
+    	protected MemoryChunkList freeMemoryChunks;
+
+    	public HeapInformation(int partitionId, int size, int flags, String name) {
+			this.partitionId = partitionId;
+			this.size = size;
+			this.flags = flags;
+			this.name = name;
+
+			allocatedSize = 0;
+
+			int type = SysMemUserForUser.PSP_SMEM_Low; // Which memory type to use?
+			sysMemInfo = Modules.SysMemUserForUserModule.malloc(partitionId, name, type, size, 0);
+			if (sysMemInfo == null) {
+				uid = -1;
+			} else {
+				MemoryChunk memoryChunk = new MemoryChunk(sysMemInfo.addr, size);
+				freeMemoryChunks = new MemoryChunkList(memoryChunk);
+
+				uid = SceUidManager.getNewUid(uidPurpose);
+			}
+    	}
+
+    	public void free() {
+    		if (sysMemInfo != null) {
+    			Modules.SysMemUserForUserModule.free(sysMemInfo);
+    			sysMemInfo = null;
+    			freeMemoryChunks = null;
+
+    			SceUidManager.releaseUid(uid, uidPurpose);
+    		}
+    	}
+
+    	public int allocBlock(int blockSize) {
+    		if (freeMemoryChunks == null) {
+    			return 0;
+    		}
+
+    		int addr = freeMemoryChunks.allocLow(blockSize + HEAP_BLOCK_HEADER_SIZE, 0);
+    		if (addr == 0) {
+    			return 0;
+    		}
+
+    		Memory.getInstance().write32(addr, blockSize);
+
+    		return addr + HEAP_BLOCK_HEADER_SIZE;
+    	}
+
+    	public void freeBlock(int addr) {
+    		addr -= HEAP_BLOCK_HEADER_SIZE;
+    		int blockSize = Memory.getInstance().read32(addr);
+
+    		MemoryChunk memoryChunk = new MemoryChunk(addr, blockSize);
+    		freeMemoryChunks.add(memoryChunk);
+    	}
+    }
 
     @Override
     public String getName() {
         return "SysMemForKernel";
     }
 
-    @HLEFunction(nid = 0xA089ECA4, version = 150)
+	@Override
+	public void start() {
+		heaps = new HashMap<Integer, SysMemForKernel.HeapInformation>();
+
+		super.start();
+	}
+
+	@HLEFunction(nid = 0xA089ECA4, version = 150)
     public int sceKernelMemset(TPointer destAddr, int data, int size) {
         destAddr.memset((byte) data, size);
 
@@ -50,10 +130,16 @@ public class SysMemForKernel extends HLEModule {
      * @param name        Name assigned to the new heap.
      * @return            The UID of the new heap, or if less than 0 an error. 
      */
-    @HLEUnimplemented
+	@HLELogging(level = "info")
     @HLEFunction(nid = 0x1C1FBFE7, version = 150)
     public int sceKernelCreateHeap(int partitionId, int size, int flags, String name) {
-    	return 0;
+    	HeapInformation info = new HeapInformation(partitionId, size, flags, name);
+
+    	if (info.uid >= 0) {
+    		heaps.put(info.uid, info);
+    	}
+
+    	return info.uid;
     }
 
     /**
@@ -63,10 +149,38 @@ public class SysMemForKernel extends HLEModule {
      * @param size   The number of bytes to allocate.
      * @return       The address of the allocated memory block, or NULL on error.
      */
-    @HLEUnimplemented
     @HLEFunction(nid = 0x636C953B, version = 150)
     public int sceKernelAllocHeapMemory(int heapId, int size) {
-        return 0;
+    	HeapInformation info = heaps.get(heapId);
+    	if (info == null) {
+    		return 0;
+    	}
+
+    	int addr = info.allocBlock(size);
+    	if (log.isDebugEnabled()) {
+    		log.debug(String.format("sceKernelAllocHeapMemory returning 0x%08X", addr));
+    	}
+
+    	return addr;
+    }
+
+    /**
+     * Free a memory block allocated from a heap.
+     * 
+     * @param heapId The UID of the heap where block belongs. 
+     * @param block  The block of memory to free from the heap.
+     * @return       0 on success, < 0 on error. 
+     */
+    @HLEFunction(nid = 0x7B749390, version = 150)
+    public int sceKernelFreeHeapMemory(int heapId, TPointer block) {
+    	HeapInformation info = heaps.get(heapId);
+    	if (info == null) {
+    		return -1;
+    	}
+
+    	info.freeBlock(block.getAddress());
+
+    	return 0;
     }
 
     /**
@@ -75,9 +189,15 @@ public class SysMemForKernel extends HLEModule {
      * @param heapId The UID of the heap to delete.
      * @return       0 on success, < 0 on error.
      */
-    @HLEUnimplemented
     @HLEFunction(nid = 0xC9805775, version = 150)
     public int sceKernelDeleteHeap(int heapId) {
-        return 0;
+    	HeapInformation info = heaps.remove(heapId);
+    	if (info == null) {
+    		return -1;
+    	}
+
+    	info.free();
+
+    	return 0;
     }
 }
