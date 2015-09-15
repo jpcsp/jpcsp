deb0519e82647475662d0da1d60599a6e92f3257
gid15@web.de <gid15@web.de>
2015-09-15 20:35:17 +0200

    Code clean-up: sceMeCore_driver

--- src/jpcsp/HLE/modules/sceMeCore_driver.java
+++ src/jpcsp/HLE/modules/sceMeCore_driver.java
@@ -1,20 +1,24 @@
-/*
-This file is part of jpcsp.
-
-Jpcsp is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Jpcsp is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Jpcsp.  If not, see <http://www.gnu.org/licenses/>.
- */
 package jpcsp.HLE.modules;
 
-public class sceMeCore_driver extends jpcsp.HLE.modules500.sceMeCore_driver {
+import org.apache.log4j.Logger;
+
+import jpcsp.HLE.HLEFunction;
+import jpcsp.HLE.HLELogging;
+import jpcsp.HLE.HLEUnimplemented;
+import jpcsp.HLE.Modules;
+
+@HLELogging
+public class sceMeCore_driver extends HLEModule {
+	public static Logger log = Modules.getLogger("sceMeCore_driver");
+
+	@Override
+	public String getName() {
+		return "sceMeCore_driver";
+	}
+
+	@HLEUnimplemented
+    @HLEFunction(nid = 0x051C1601, version = 500)
+    public int sceMeBootStart500(int unknown) {
+    	return 0;
+    }
 }
