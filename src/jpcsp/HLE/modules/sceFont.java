jpcsp/jpcsp

commit 3887d94e146a40978792136d6ff5aab9d9385ee3

Implemented sceReg functions to manipulate font entries.

src/jpcsp/HLE/modules/sceFont.java
@@ -106,7 +106,6 @@ public class sceFont extends HLEModule {
         fontsMap = new HashMap<Integer, Font>();
         loadFontRegistry();
         loadDefaultSystemFont();
-        loadAllFonts();
 
         super.start();
     }
@@ -174,8 +173,7 @@ public class sceFont extends HLEModule {
         }
     }
 
-    public class FontRegistryEntry {
-
+    public static class FontRegistryEntry {
         public int h_size;
         public int v_size;
         public int h_resolution;
@@ -215,9 +213,11 @@ public class sceFont extends HLEModule {
             this.expire_date = expire_date;
             this.shadow_option = shadow_option;
         }
+
+        public FontRegistryEntry() {
+        }
     }
     public static final int PGF_MAGIC = 'P' << 24 | 'G' << 16 | 'F' << 8 | '0';
-    public static final String fontDirPath = "flash0:/font";
     public static final String customFontFile = "debug.jpft";
     public static final int PSP_FONT_PIXELFORMAT_4 = 0; // 2 pixels packed in 1 byte (natural order)
     public static final int PSP_FONT_PIXELFORMAT_4_REV = 1; // 2 pixels packed in 1 byte (reversed order)
@@ -232,6 +232,7 @@ public class sceFont extends HLEModule {
     private HashMap<Integer, FontLib> fontLibsMap;
     private HashMap<Integer, Font> fontsMap;
     protected String uidPurpose = "sceFont";
+    private String fontDirPath = "flash0:/font";
     private List<FontRegistryEntry> fontRegistry;
     protected static final float pointDPI = 72.f;
 
@@ -241,6 +242,18 @@ public class sceFont extends HLEModule {
 
     private void setUseDebugFont(boolean status) {
         useDebugFont = status;
+    }
+
+    public List<FontRegistryEntry> getFontRegistry() {
+    	return fontRegistry;
+    }
+
+    public String getFontDirPath() {
+    	return fontDirPath;
+    }
+
+    public void setFontDirPath(String fontDirPath) {
+    	this.fontDirPath = fontDirPath;
     }
 
     protected void loadFontRegistry() {
@@ -775,11 +788,15 @@ public class sceFont extends HLEModule {
     }
 
     public Font getFont(int index) {
+    	if (internalFonts.size() == 0) {
+    		loadAllFonts();
+    	}
     	return internalFonts.get(index);
     }
 
     @HLEFunction(nid = 0x67F17ED7, version = 150, checkInsideInterrupt = true, stackUsage = 0x590)
     public int sceFontNewLib(TPointer paramsPtr, @CanBeNull TErrorPointer32 errorCodePtr) {
+    	loadAllFonts();
         errorCodePtr.setValue(0);
         FontLib fontLib = new FontLib(paramsPtr);
         fontLibsMap.put(fontLib.getHandle(), fontLib);

src/jpcsp/HLE/HLEModuleManager.java
@@ -155,7 +155,8 @@ public class HLEModuleManager {
         KUBridge(Modules.KUBridgeModule),
         SysclibForKernel(Modules.SysclibForKernelModule),
         semaphore(Modules.semaphoreModule),
-        ModuleMgrForKernel(Modules.ModuleMgrForKernelModule);
+        ModuleMgrForKernel(Modules.ModuleMgrForKernelModule),
+        sceReg(Modules.sceRegModule);
 
     	private HLEModule module;
     	private int firmwareVersionAsDefault;	// FirmwareVersion where the module is loaded by default

src/jpcsp/HLE/Modules.java
@@ -81,6 +81,7 @@ import jpcsp.HLE.modules.scePower;
 import jpcsp.HLE.modules.scePsmf;
 import jpcsp.HLE.modules.scePsmfPlayer;
 import jpcsp.HLE.modules.scePspNpDrm_user;
+import jpcsp.HLE.modules.sceReg;
 import jpcsp.HLE.modules.sceRtc;
 import jpcsp.HLE.modules.sceSasCore;
 import jpcsp.HLE.modules.sceSfmt19937;
@@ -189,6 +190,7 @@ public class Modules {
     public static SysclibForKernel SysclibForKernelModule = new SysclibForKernel();
     public static semaphore semaphoreModule = new semaphore();
     public static ModuleMgrForKernel ModuleMgrForKernelModule = new ModuleMgrForKernel();
+    public static sceReg sceRegModule = new sceReg();
 
     public static Logger log = Logger.getLogger("hle");
 

src/jpcsp/HLE/SyscallIgnore.java
@@ -28,12 +28,7 @@ public enum SyscallIgnore {
 	InterruptManagerForKernel_53991063(0x53991063),
 	Kprintf(0x84F370BC),
 	QueryInterruptManCB(0x00B6B0F3),
-	RegCloseRegistry(0xfa8a5739),
-	RegExit(0x9b25edf1),
-	RegFlushRegistry(0x39461b4d),
 	RegisterContextHooks(0x2CD783A1),
-	RegOpenRegistry(0x92e41280),
-	RegRemoveRegistry(0xdeda92bf),
 	ReleaseContextHooks(0x55242A8B),
 	ReturnToThread(0x43CD40EF),
 	SaveThreadContext(0x85F7766D),
@@ -71,19 +66,6 @@ public enum SyscallIgnore {
 	sceNetApDialogDummyTerm(0xF213BE65),
 	sceParseHttpResponseHeader(0xAD7BFDEF),//1.00+
 	sceParseHttpStatusLine(0x8077a433),//1.00+
-	sceRegCloseCategory(0x0cae832b),//1.00+
-	sceRegCreateKey(0x57641a81),//1.00+
-	sceRegFlushCategory(0x0d69bf40),//1.00+
-	sceRegGetKeyInfo(0xd4475aa8),//1.00+
-	sceRegGetKeys(0x2d211135),//1.00+
-	sceRegGetKeysNum(0x2c0db9dd),//1.00+
-	sceRegGetKeyValue(0x28a8e98a),//1.00+
-	sceRegGetKeyValueByName(0x30be0259),//1.00+
-	sceRegKickBackDiscover(0xc5768d02),//1.00+
-	sceRegOpenCategory(0x1d8a762e),//1.00+
-	sceRegRemoveCategory(0x4ca16893),//1.00+
-	sceRegRemoveKey(0x3615bc87),//1.00+
-	sceRegSetKeyValue(0x17768e14),//1.00+
 	sceSysconCtrlHRPower(0x44439604),//1.00+
 	sceSysregMeBusClockDisable(0x158AD4FC), //1.00+
 	sceSysregMeBusClockEnable(0x44F6CDA7),//1.00+

commit 902af981eb83dc509b2da2d1f082b760d056053c

Fixed issue introduced in 3887d94 when sceFontNewLib is called multiple times.

src/jpcsp/HLE/modules/sceFont.java
@@ -99,8 +99,6 @@ public class sceFont extends HLEModule {
     public void start() {
     	setSettingsListener("emu.useDebugFont", new UseDebugFontSettingsListerner());
 
-    	fontIndex = -1;
-        fontLibIndex = -1;
         internalFonts = new LinkedList<Font>();
         fontLibsMap = new HashMap<Integer, FontLib>();
         fontsMap = new HashMap<Integer, Font>();
@@ -109,9 +107,6 @@ public class sceFont extends HLEModule {
 
         super.start();
     }
-
-    public int fontIndex;
-    public int fontLibIndex;
 
     @HLEUidClass(errorValueOnNotFound = SceKernelErrors.ERROR_FONT_INVALID_LIBID)
     public class Font {
@@ -478,6 +473,8 @@ public class sceFont extends HLEModule {
     }
 
     protected void loadAllFonts() {
+        internalFonts.clear();
+
         // Load the fonts in the same order as on a PSP.
         // Some applications are always using the first font returned by
         // sceFontGetFontList.