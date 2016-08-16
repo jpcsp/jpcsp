jpcsp/jpcsp

commit 61ecddc24341a39667e7d869934ef7ef583fcd03

sceIoClose: improved compatibility by returning new error codes (0x800200D1 and 0x80020329). Based on JpcspTrace output.

src/jpcsp/HLE/modules/IoFileMgrForUser.java
@@ -1832,30 +1832,43 @@ public class IoFileMgrForUser extends HLEModule {
     private int hleIoClose(int id, boolean async) {
         int result;
 
-        if (log.isDebugEnabled()) {
-            log.debug("hleIoClose - id " + Integer.toHexString(id));
-        }
-
         IoInfo info = fileIds.get(id);
-        if (async) {
+        if (id == STDIN_ID || id == STDOUT_ID || id == STDERR_ID) {
+        	// Cannot close stdin, stdout, stderr
+        	result = SceKernelErrors.ERROR_KERNEL_ILLEGAL_PERMISSION;
+        	if (log.isDebugEnabled()) {
+        		log.debug(String.format("sceIoClose id=0x%X returning ERROR_KERNEL_ILLEGAL_PERMISSION(0x%08X)", id, result));
+        	}
+        } else if (async) {
             if (info != null) {
                 if (info.asyncPending || info.asyncResultPending) {
-                    log.warn("sceIoCloseAsync - id " + Integer.toHexString(id) + " PSP_ERROR_ASYNC_BUSY");
                     result = ERROR_KERNEL_ASYNC_BUSY;
+                	if (log.isDebugEnabled()) {
+                		log.debug(String.format("sceIoClose id=0x%X returning ERROR_KERNEL_ASYNC_BUSY(0x%08X)", id, result));
+                	}
                 } else {
                     info.closePending = true;
                     result = (int) updateResult(info, 0, true, false, IoOperation.close);
                 }
             } else {
                 result = ERROR_KERNEL_BAD_FILE_DESCRIPTOR;
+            	if (log.isDebugEnabled()) {
+            		log.debug(String.format("sceIoClose id=0x%X returning ERROR_KERNEL_BAD_FILE_DESCRIPTOR(0x%08X)", id, result));
+            	}
             }
         } else {
             try {
                 if (info == null) {
-                    if (id != 1 && id != 2) { // Ignore stdout and stderr.
-                        log.warn("sceIoClose - unknown id " + Integer.toHexString(id));
-                    }
                     result = ERROR_KERNEL_BAD_FILE_DESCRIPTOR;
+                	if (log.isDebugEnabled()) {
+                		log.debug(String.format("sceIoClose id=0x%X returning ERROR_KERNEL_BAD_FILE_DESCRIPTOR(0x%08X)", id, result));
+                	}
+                } else if (info.asyncPending || info.asyncResultPending) {
+                	// Cannot close while an async operation is running
+                    result = ERROR_KERNEL_ASYNC_BUSY;
+                	if (log.isDebugEnabled()) {
+                		log.debug(String.format("sceIoClose id=0x%X returning ERROR_KERNEL_ASYNC_BUSY(0x%08X)", id, result));
+                	}
                 } else {
                 	if (info.vFile != null) {
                 		info.vFile.ioClose();
@@ -1871,7 +1884,6 @@ public class IoFileMgrForUser extends HLEModule {
                 }
             } catch (IOException e) {
                 log.error("pspiofilemgr - error closing file: " + e.getMessage());
-                e.printStackTrace();
                 result = -1;
             }
             for (IIoListener ioListener : ioListeners) {

commit 11e909b5d0ff4c323181966defd63d3b48d8d211

Fixed issue in sceIoCloseAsync introduced by 61ecddc.

src/jpcsp/HLE/modules/IoFileMgrForUser.java
@@ -1560,6 +1560,8 @@ public class IoFileMgrForUser extends HLEModule {
         // The file was marked as closePending, so close it right away to avoid delays.
         if (info.closePending) {
             log.debug("hleIoWaitAsync - file marked with closePending, calling hleIoClose, not waiting");
+            info.asyncPending = false;
+            info.asyncResultPending = false;
             hleIoClose(info.id, false);
             waitForAsync = false;
         }