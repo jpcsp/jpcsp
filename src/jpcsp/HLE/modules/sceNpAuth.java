365bbffdbbe4b05cc22ba4d5f301fad6f5d64c16
gid15@web.de <gid15@web.de>
2016-09-05 21:54:13 +0200

    sceNpAuthCreateStartRequest: now call PSN to perform real authentication (asking for PSN account and password).

--- src/jpcsp/HLE/modules/sceNpAuth.java
+++ src/jpcsp/HLE/modules/sceNpAuth.java
@@ -18,7 +18,19 @@ package jpcsp.HLE.modules;
 
 import static jpcsp.HLE.kernel.types.SceKernelThreadInfo.THREAD_CALLBACK_USER_DEFINED;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
 
 import jpcsp.HLE.HLEFunction;
 import jpcsp.HLE.HLEModule;
@@ -32,13 +44,17 @@ import jpcsp.HLE.kernel.types.SceKernelErrors;
 import jpcsp.HLE.kernel.types.SceNpAuthRequestParameter;
 import jpcsp.HLE.kernel.types.SceNpTicket;
 import jpcsp.HLE.kernel.types.SceNpTicket.TicketParam;
+import jpcsp.memory.IMemoryReader;
+import jpcsp.memory.IMemoryWriter;
+import jpcsp.memory.MemoryReader;
+import jpcsp.memory.MemoryWriter;
 import jpcsp.util.Utilities;
 
 import org.apache.log4j.Logger;
 
 public class sceNpAuth extends HLEModule {
     public static Logger log = Modules.getLogger("sceNpAuth");
-    public static boolean useDummyTicket = true;
+    public static boolean useDummyTicket = false;
 
     public final static int STATUS_ACCOUNT_SUSPENDED = 0x80;
     public final static int STATUS_ACCOUNT_CHAT_RESTRICTED = 0x100;
@@ -50,6 +66,8 @@ public class sceNpAuth extends HLEModule {
     private int npFreeMemSize; // Free memory available to use by the NP utility.
     private SceKernelCallbackInfo npAuthCreateTicketCallback;
     private String serviceId;
+    private byte[] ticketBytes = new byte[10000];
+    private int ticketBytesLength;
 
 	@Override
 	public void start() {
@@ -93,6 +111,34 @@ public class sceNpAuth extends HLEModule {
 
     private void addTicketParam(SceNpTicket ticket) {
     	ticket.parameters.add(new TicketParam(TicketParam.PARAM_TYPE_NULL, new byte[0]));
+    }
+
+    private static String encodeURLParam(String value) {
+    	try {
+			return URLEncoder.encode(value, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return value;
+		}
+    }
+
+    private static void addURLParam(StringBuilder params, String name, String value) {
+    	if (params.length() > 0) {
+    		params.append("&");
+    	}
+    	params.append(name);
+    	params.append("=");
+    	params.append(encodeURLParam(value));
+    }
+
+    private static void addURLParam(StringBuilder params, String name, int addr, int length) {
+    	StringBuilder value = new StringBuilder();
+    	IMemoryReader memoryReader = MemoryReader.getMemoryReader(addr, length, 1);
+    	for (int i = 0; i < length; i++) {
+    		int c = memoryReader.readNext();
+    		value.append((char) c);
+    	}
+
+    	addURLParam(params, name, value.toString());
     }
 
     /**
@@ -151,11 +197,79 @@ public class sceNpAuth extends HLEModule {
 
     	serviceId = param.serviceId;
 
+    	if (!useDummyTicket) {
+			String loginId = JOptionPane.showInputDialog("Enter your PSN Sign-In ID (Email Address)");
+			if (loginId != null) {
+    			String password = JOptionPane.showInputDialog("Enter your PSN Password");
+    			if (password != null) {
+	    			StringBuilder params = new StringBuilder();
+	    			addURLParam(params, "serviceid", serviceId);
+	    			addURLParam(params, "loginid", loginId);
+	    			addURLParam(params, "password", password);
+	    			if (param.cookie != 0) {
+	    				addURLParam(params, "cookie", param.cookie, param.cookieSize);
+	    			}
+	    			if (param.entitlementIdAddr != 0) {
+	    				addURLParam(params, "entitlementid", param.entitlementId);
+	    				addURLParam(params, "consumedcount", Integer.toString(param.consumedCount));
+	    			}
+
+	    			HttpURLConnection connection = null;
+    				ticketBytesLength = 0;
+	    			try {
+		    			connection = (HttpURLConnection) new URL("https://auth.np.ac.playstation.net/nav/auth").openConnection();
+		    			connection.setRequestProperty("X-I-5-Version", "2.1");
+		    			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		    			connection.setRequestProperty("X-Platform-Version", "PSP 06.60");
+		    			connection.setRequestProperty("Content-Length", Integer.toString(params.length()));
+		    			connection.setRequestProperty("User-Agent", "Lediatio Lunto Ritna");
+		    			connection.setRequestMethod("POST");
+		    			connection.setDoOutput(true);
+		    			OutputStream os = connection.getOutputStream();
+		    			os.write(params.toString().getBytes());
+		    			os.close();
+		    			connection.connect();
+		    			int responseCode = connection.getResponseCode();
+		    			if (log.isDebugEnabled()) {
+		    				log.debug(String.format("Response code: %d", responseCode));
+		    				for (Map.Entry<String, List<String>> entry : connection.getHeaderFields().entrySet()) {
+		    					log.debug(String.format("%s: %s", entry.getKey(), entry.getValue()));
+		    				}
+		    			}
+
+		    			if (responseCode == 200) {
+		    				InputStream in = connection.getInputStream();
+		    				while (true) {
+		    					int length = in.read(ticketBytes, ticketBytesLength, ticketBytes.length - ticketBytesLength);
+		    					if (length < 0) {
+		    						break;
+		    					}
+		    					ticketBytesLength += length;
+		    				}
+		    				in.close();
+
+		    				if (log.isDebugEnabled()) {
+		    					log.debug(String.format("Received ticket: %s", Utilities.getMemoryDump(ticketBytes, 0, ticketBytesLength)));
+		    				}
+		    			}
+		    		} catch (MalformedURLException e) {
+		    			log.error(e);
+		    		} catch (IOException e) {
+		    			log.error(e);
+		    		} finally {
+	    				if (connection != null) {
+	    					connection.disconnect();
+	    				}
+		    		}
+    			}
+			}
+    	}
+
     	if (param.ticketCallback != 0) {
-    		int ticketLength = 248;
+    		int ticketLength = ticketBytesLength > 0 ? ticketBytesLength : 248;
     		npAuthCreateTicketCallback = Modules.ThreadManForUserModule.hleKernelCreateCallback("sceNpAuthCreateStartRequest", param.ticketCallback, param.callbackArgument);
-    		if (Modules.ThreadManForUserModule.hleKernelRegisterCallback(THREAD_CALLBACK_USER_DEFINED, npAuthCreateTicketCallback.uid)) {
-    			Modules.ThreadManForUserModule.hleKernelNotifyCallback(THREAD_CALLBACK_USER_DEFINED, npAuthCreateTicketCallback.uid, ticketLength);
+    		if (Modules.ThreadManForUserModule.hleKernelRegisterCallback(THREAD_CALLBACK_USER_DEFINED, npAuthCreateTicketCallback.getUid())) {
+    			Modules.ThreadManForUserModule.hleKernelNotifyCallback(THREAD_CALLBACK_USER_DEFINED, npAuthCreateTicketCallback.getUid(), ticketLength);
     		}
     	}
 
@@ -165,6 +279,8 @@ public class sceNpAuth extends HLEModule {
     @HLEUnimplemented
     @HLEFunction(nid = 0x3F1C1F70, version = 150)
     public int sceNpAuthGetTicket(int id, TPointer buffer, int length) {
+    	int result;
+
     	if (useDummyTicket) {
     		SceNpTicket ticket = new SceNpTicket();
     		ticket.version = 0x00000121;
@@ -192,11 +308,28 @@ public class sceNpAuth extends HLEModule {
     			log.debug(String.format("sceNpAuthGetTicket returning dummy ticket: %s", ticket));
     		}
     		ticket.write(buffer);
+    		result = ticket.sizeof();
+    	} else if (ticketBytesLength > 0) {
+        	result = Math.min(ticketBytesLength, length);
+        	IMemoryWriter memoryWriter = MemoryWriter.getMemoryWriter(buffer.getAddress(), result, 1);
+    		for (int i = 0; i < result; i++) {
+    			memoryWriter.writeNext(ticketBytes[i] & 0xFF);
+    		}
+
+    		if (log.isDebugEnabled()) {
+    			log.debug(String.format("sceNpAuthGetTicket returning real ticket: %s", Utilities.getMemoryDump(buffer.getAddress(), result)));
+    		}
     	} else {
         	buffer.clear(length);
+
+        	result = length;
+
+        	if (log.isDebugEnabled()) {
+        		log.debug(String.format("sceNpAuthGetTicket returning empty ticket"));
+        	}
     	}
 
-    	return 0;
+    	return result;
     }
 
     @HLEUnimplemented
@@ -227,14 +360,21 @@ public class sceNpAuth extends HLEModule {
     		return SceKernelErrors.ERROR_NP_MANAGER_INVALID_ARGUMENT;
     	}
 
-    	SceNpTicket ticket = new SceNpTicket();
-    	ticket.read(ticketBuffer);
-    	if (log.isDebugEnabled()) {
-    		log.debug(String.format("sceNpAuthGetTicketParam ticket: %s", ticket));
-    	}
+    	if (ticketBuffer.getValue32() == 0) {
+    		// This is an empty ticket, do no analyze it
+    		if (log.isDebugEnabled()) {
+    			log.debug(String.format("sceNpAuthGetTicketParam returning empty param from empty ticket"));
+    		}
+    	} else {
+	    	SceNpTicket ticket = new SceNpTicket();
+	    	ticket.read(ticketBuffer);
+	    	if (log.isDebugEnabled()) {
+	    		log.debug(String.format("sceNpAuthGetTicketParam ticket: %s", ticket));
+	    	}
 
-    	TicketParam ticketParam = ticket.parameters.get(paramNumber);
-    	ticketParam.writeForPSP(buffer);
+	    	TicketParam ticketParam = ticket.parameters.get(paramNumber);
+	    	ticketParam.writeForPSP(buffer);
+    	}
 
     	return 0;
     }
