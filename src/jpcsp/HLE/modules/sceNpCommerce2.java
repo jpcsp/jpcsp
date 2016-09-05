ec0e75bc45defc844a970709206e96b09017a43c
gid15@web.de <gid15@web.de>
2016-09-05 21:54:53 +0200

    Added draft sceNpCommerce2 functions.

--- src/jpcsp/HLE/modules/sceNpCommerce2.java
+++ src/jpcsp/HLE/modules/sceNpCommerce2.java
@@ -39,4 +39,172 @@ public class sceNpCommerce2 extends HLEModule {
     	// No parameters
     	return 0;
     }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x005B5F20, version = 150)
+    public int sceNpCommerce2GetProductInfoStart() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x05E7AFBC, version = 150)
+    public int sceNpCommerce2GetGameProductInfoFromContentInfo() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x1888A9FE, version = 150)
+    public int sceNpCommerce2DestroyReq() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x1C85ED88, version = 150)
+    public int sceNpCommerce2GetPrice() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x1C952DCB, version = 150)
+    public int sceNpCommerce2GetGameProductInfo() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x2B25F6E9, version = 150)
+    public int sceNpCommerce2CreateSessionStart() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x3371D5F1, version = 150)
+    public int sceNpCommerce2GetProductInfoCreateReq() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x490210E0, version = 150)
+    public int sceNpCommerce2DestroyGetProductInfoResult() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x4ECD4503, version = 150)
+    public int sceNpCommerce2CreateSessionCreateReq() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x54FE8871, version = 150)
+    public int sceNpCommerce2GetCategoryInfo() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x57C8BD4E, version = 150)
+    public int sceNpCommerce2GetCategoryInfoFromContentInfo() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x590A3229, version = 150)
+    public int sceNpCommerce2GetSessionInfo() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x5D8C2D99, version = 150)
+    public int sceNpCommerce2GetCategoryContentsCreateReq() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x6592FE83, version = 150)
+    public int sceNpCommerce2GetContentRatingDescriptor() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x6A2AE572, version = 150)
+    public int sceNpCommerce2GetContentRatingInfoFromGameProductInfo() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x6F1FE37F, version = 150)
+    public int sceNpCommerce2CreateCtx() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x80A7AFDB, version = 150)
+    public int sceNpCommerce2GetCategoryContentsGetResult() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xAA4A1E3D, version = 150)
+    public int sceNpCommerce2GetProductInfoGetResult() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xB0D7AA90, version = 150)
+    public int sceNpCommerce2GetContentInfo() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xBC61FFC8, version = 150)
+    public int sceNpCommerce2CreateSessionGetResult() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xC5505A19, version = 150)
+    public int sceNpCommerce2GetContentRatingInfoFromCategoryInfo() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xC7F32242, version = 150)
+    public int sceNpCommerce2AbortReq() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xCEAB1829, version = 150)
+    public int sceNpCommerce2InitGetCategoryContentsResult() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xDD6758FA, version = 150)
+    public int sceNpCommerce2GetCategoryContentsStart() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xED85ACCE, version = 150)
+    public int sceNpCommerce2DestroyGetCategoryContentsResult() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xF2278B90, version = 150)
+    public int sceNpCommerce2GetGameSkuInfoFromGameProductInfo() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xF297AB9C, version = 150)
+    public int sceNpCommerce2DestroyCtx() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0xFC30C19E, version = 150)
+    public int sceNpCommerce2InitGetProductInfoResult() {
+    	return 0;
+    }
 }
