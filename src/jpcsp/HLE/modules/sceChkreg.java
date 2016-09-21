70c259c9a7cc2e7084a04f38961008c609034a0b
gid15@web.de <gid15@web.de>
2016-09-21 18:27:50 +0200

    Implemented sceChkregGetPsCode

--- src/jpcsp/HLE/modules/sceChkreg.java
+++ src/jpcsp/HLE/modules/sceChkreg.java
@@ -26,6 +26,15 @@ import jpcsp.HLE.TPointer8;
 
 public class sceChkreg extends HLEModule {
     public static Logger log = Modules.getLogger("sceChkreg");
+    public static final int PS_CODE_JAPAN = 3;
+    public static final int PS_CODE_NORTH_AMERICA = 4;
+    public static final int PS_CODE_EUROPE = 5;
+    public static final int PS_CODE_KOREA = 6;
+    public static final int PS_CODE_AUSTRALIA = 9;
+    public static final int PS_CODE_HONGKONG = 10;
+    public static final int PS_CODE_TAIWAN = 11;
+    public static final int PS_CODE_RUSSIA = 12;
+    public static final int PS_CODE_CHINA = 13;
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x54495B19, version = 150)
@@ -33,9 +42,17 @@ public class sceChkreg extends HLEModule {
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0x59F8491D, version = 150)
-    public int sceChkregGetPsCode() {
+    public int sceChkregGetPsCode(TPointer8 psCode) {
+    	psCode.setValue(0, 1);
+    	psCode.setValue(1, 0);
+    	psCode.setValue(2, PS_CODE_EUROPE);
+    	psCode.setValue(3, 0);
+    	psCode.setValue(4, 1);
+    	psCode.setValue(5, 0);
+    	psCode.setValue(6, 1);
+    	psCode.setValue(7, 0);
+
     	return 0;
     }
 
