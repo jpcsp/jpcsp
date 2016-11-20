50d1f0ca6d9b9becaeabe105d92235fb5ef6b183
gid15@web.de <gid15@web.de>
2016-11-20 21:28:55 +0100

    Added one more PSN URL used to test internet connections.

--- src/jpcsp/remote/HTTPConfiguration.java
+++ src/jpcsp/remote/HTTPConfiguration.java
@@ -125,6 +125,7 @@ public class HTTPConfiguration {
 		new HttpServerConfiguration("www.shoutcast.com"),
 		new HttpServerConfiguration("cdn-a.sonyentertainmentnetwork.com", 443),
 		new HttpServerConfiguration("boxreg.trendmicro.com", 443),
-		new HttpServerConfiguration("eu.playstation.com", 443, false)
+		new HttpServerConfiguration("eu.playstation.com", 443, false),
+		new HttpServerConfiguration("fj00.psp.update.playstation.org")
 	};
 }
