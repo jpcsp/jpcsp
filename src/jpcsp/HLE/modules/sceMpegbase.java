68836b3e808395b89e2139082a40550b1fe59044
gid15@web.de <gid15@web.de>
2016-12-20 18:37:43 +0100

    Implemented sceMpegBaseCscAvc and sceMpegBaseCscAvcRange

--- src/jpcsp/HLE/modules/sceMpegbase.java
+++ src/jpcsp/HLE/modules/sceMpegbase.java
@@ -45,28 +45,124 @@ import jpcsp.memory.IMemoryWriter;
 import jpcsp.memory.MemoryReader;
 import jpcsp.memory.MemoryWriter;
 import jpcsp.util.Debug;
+import jpcsp.util.Utilities;
 
 public class sceMpegbase extends HLEModule {
 	public static Logger log = Modules.getLogger("sceMpegbase");
 
-    @HLEUnimplemented
+	private static void read(int addr, int length, int[] buffer, int offset) {
+		addr |= MemoryMap.START_RAM;
+
+    	// Optimize the most common case
+        if (memoryInt != null) {
+        	int length4 = length >> 2;
+        	int addrOffset = addr >> 2;
+	        for (int i = 0, j = offset; i < length4; i++) {
+	        	int value = memoryInt[addrOffset++];
+	        	buffer[j++] = (value      ) & 0xFF;
+	        	buffer[j++] = (value >>  8) & 0xFF;
+	        	buffer[j++] = (value >> 16) & 0xFF;
+	        	buffer[j++] = (value >> 24) & 0xFF;
+	        }
+        } else {
+	        IMemoryReader memoryReader = MemoryReader.getMemoryReader(addr, length, 1);
+	        for (int i = 0, j = offset; i < length; i++) {
+	        	buffer[j++] = memoryReader.readNext();
+	        }
+        }
+    }
+
+	private static void copy(Memory mem, int dst, int src, int length) {
+		mem.memcpy(dst | MemoryMap.START_RAM, src | MemoryMap.START_RAM, length);
+	}
+
+	private static void copyBlocks(Memory mem, int dst, int src, int blocks) {
+		copy(mem, dst, src, blocks << 4);
+	}
+
+    private int hleMpegBaseCscAvcRange(TPointer bufferRGB, int unknown, int bufferWidth, SceMp4AvcCscStruct mp4AvcCscStruct, int rangeX, int rangeY, int rangeWidth, int rangeHeight) {
+    	int width = mp4AvcCscStruct.width << 4;
+    	int height = mp4AvcCscStruct.height << 4;
+
+    	int videoPixelMode = TPSM_PIXEL_STORAGE_MODE_32BIT_ABGR8888;
+		int bytesPerPixel = sceDisplay.getPixelFormatBytes(videoPixelMode);
+        int destAddr = bufferRGB.getAddress();
+
+        int width2 = width >> 1;
+    	int height2 = height >> 1;
+        int length = width * height;
+        int length2 = width2 * height2;
+
+        // Read the YCbCr image
+        int[] luma = getIntBuffer(length);
+        int[] cb = getIntBuffer(length2);
+        int[] cr = getIntBuffer(length2);
+        read(mp4AvcCscStruct.buffer0, length / 4, luma, 0);
+        read(mp4AvcCscStruct.buffer1, length / 4, luma, 1 * length / 4);
+        read(mp4AvcCscStruct.buffer2, length / 4, luma, 2 * length / 4);
+        read(mp4AvcCscStruct.buffer3, length / 4, luma, 3 * length / 4);
+        read(mp4AvcCscStruct.buffer4, length2 / 2, cr, 0);
+        read(mp4AvcCscStruct.buffer5, length2 / 2, cr, length2 / 2);
+        read(mp4AvcCscStruct.buffer6, length2 / 2, cb, 0);
+        read(mp4AvcCscStruct.buffer7, length2 / 2, cb, length2 / 2);
+
+        // Convert YCbCr to ABGR
+        int[] abgr = getIntBuffer(length);
+        H264Utils.YUV2ABGR(width, height, luma, cb, cr, abgr);
+
+        releaseIntBuffer(luma);
+        releaseIntBuffer(cb);
+        releaseIntBuffer(cr);
+
+		// Do not cache the video image as a texture in the VideoEngine to allow fluid rendering
+        VideoEngine.getInstance().addVideoTexture(destAddr, destAddr + (rangeY + rangeHeight) * bufferWidth * bytesPerPixel);
+
+        // Write the ABGR image
+		if (videoPixelMode == TPSM_PIXEL_STORAGE_MODE_32BIT_ABGR8888 && memoryInt != null) {
+			// Optimize the most common case
+			int pixelIndex = rangeY * width + rangeX;
+        	int addr = destAddr;
+	        for (int i = 0; i < rangeHeight; i++) {
+	        	System.arraycopy(abgr, pixelIndex, memoryInt, addr >> 2, rangeWidth);
+	        	pixelIndex += width;
+	        	addr += bufferWidth * bytesPerPixel;
+	        }
+		} else {
+        	int addr = destAddr;
+	        for (int i = 0; i < rangeHeight; i++) {
+	        	IMemoryWriter memoryWriter = MemoryWriter.getMemoryWriter(addr, rangeWidth * bytesPerPixel, bytesPerPixel);
+	        	int pixelIndex = (i + rangeY) * width + rangeX;
+	        	for (int j = 0; j < rangeWidth; j++, pixelIndex++) {
+	        		int abgr8888 = abgr[pixelIndex];
+	        		int pixelColor = Debug.getPixelColor(abgr8888, videoPixelMode);
+	        		memoryWriter.writeNext(pixelColor);
+	        	}
+	        	memoryWriter.flush();
+	        	addr += bufferWidth * bytesPerPixel;
+	        }
+		}
+		releaseIntBuffer(abgr);
+
+		return 0;
+    }
+
     @HLEFunction(nid = 0xBEA18F91, version = 150)
-    public int sceMpegBasePESpacketCopy(TPointer32 packetInfo) {
+    public int sceMpegBasePESpacketCopy(@BufferInfo(lengthInfo=LengthInfo.fixedLength, length=16, usage=Usage.in) TPointer32 packetInfo) {
     	Memory mem = Memory.getInstance();
     	int infoAddr = packetInfo.getAddress();
     	while (infoAddr != 0) {
     		int bufferAddr = mem.read32(infoAddr + 0);
-    		int unknown = mem.read32(infoAddr + 4);
+    		int destinationAddr = mem.read32(infoAddr + 4) | MemoryMap.START_RAM;
     		int nextInfoAddr = mem.read32(infoAddr + 8);
     		int bufferLength = mem.read32(infoAddr + 12) & 0x00000FFF;
 
     		if (log.isDebugEnabled()) {
-    			log.debug(String.format("sceMpegBasePESpacketCopy packet at 0x%08X: bufferAddr=0x%08X, unknown=0x%08X, nextInfoAddr=0x%08X, bufferLength=0x%X", infoAddr, bufferAddr, unknown, nextInfoAddr, bufferLength));
+    			log.debug(String.format("sceMpegBasePESpacketCopy packet at 0x%08X: bufferAddr=0x%08X, destinationAddr=0x%08X, nextInfoAddr=0x%08X, bufferLength=0x%X: %s", infoAddr, bufferAddr, destinationAddr, nextInfoAddr, bufferLength, Utilities.getMemoryDump(bufferAddr, bufferLength)));
     		}
     		if (bufferLength == 0) {
     			return SceKernelErrors.ERROR_INVALID_SIZE;
     		}
-        	Modules.sceMpegModule.addToVideoBuffer(mem, bufferAddr, bufferLength);
+    		mem.memcpy(destinationAddr, bufferAddr, bufferLength);
         	infoAddr = nextInfoAddr;
     	}
 
@@ -76,7 +172,6 @@ public class sceMpegbase extends HLEModule {
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0xBE45C284, version = 150)
     public int sceMpegBaseYCrCbCopyVme(@BufferInfo(lengthInfo=LengthInfo.fixedLength, length=96, usage=Usage.in) TPointer destBufferYCrCb, @BufferInfo(lengthInfo=LengthInfo.fixedLength, length=40, usage=Usage.in) TPointer32 srcBufferYCrCb, int type) {
     	SceMpegYCrCbBuffer destMpegYCrCbBuffer = new SceMpegYCrCbBuffer();
@@ -93,18 +188,16 @@ public class sceMpegbase extends HLEModule {
     	Memory mem = destBufferYCrCb.getMemory();
     	int sizeY = srcMpegYCrCbBuffer.frameWidth * srcMpegYCrCbBuffer.frameHeight;
     	int sizeCrCb = sizeY >> 2;
-    	int baseAddr = MemoryMap.START_RAM;
-    	mem.memcpy(destMpegYCrCbBuffer.bufferY, srcMpegYCrCbBuffer.bufferY | baseAddr, sizeY);
-    	mem.memcpy(destMpegYCrCbBuffer.bufferY2, srcMpegYCrCbBuffer.bufferY2 | baseAddr, sizeY);
-    	mem.memcpy(destMpegYCrCbBuffer.bufferCr, srcMpegYCrCbBuffer.bufferCr | baseAddr, sizeCrCb);
-    	mem.memcpy(destMpegYCrCbBuffer.bufferCb, srcMpegYCrCbBuffer.bufferCb | baseAddr, sizeCrCb);
-    	mem.memcpy(destMpegYCrCbBuffer.bufferCr2, srcMpegYCrCbBuffer.bufferCr2 | baseAddr, sizeCrCb);
-    	mem.memcpy(destMpegYCrCbBuffer.bufferCb2, srcMpegYCrCbBuffer.bufferCb2 | baseAddr, sizeCrCb);
+    	copy(mem, destMpegYCrCbBuffer.bufferY, srcMpegYCrCbBuffer.bufferY, sizeY);
+    	copy(mem, destMpegYCrCbBuffer.bufferY2, srcMpegYCrCbBuffer.bufferY2, sizeY);
+    	copy(mem, destMpegYCrCbBuffer.bufferCr, srcMpegYCrCbBuffer.bufferCr, sizeCrCb);
+    	copy(mem, destMpegYCrCbBuffer.bufferCb, srcMpegYCrCbBuffer.bufferCb, sizeCrCb);
+    	copy(mem, destMpegYCrCbBuffer.bufferCr2, srcMpegYCrCbBuffer.bufferCr2, sizeCrCb);
+    	copy(mem, destMpegYCrCbBuffer.bufferCb2, srcMpegYCrCbBuffer.bufferCb2, sizeCrCb);
 
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0x7AC0321A, version = 150)
     public int sceMpegBaseYCrCbCopy(@BufferInfo(lengthInfo=LengthInfo.fixedLength, length=48, usage=Usage.in) TPointer dst, @BufferInfo(lengthInfo=LengthInfo.fixedLength, length=48, usage=Usage.in) TPointer src, int flags) {
     	SceMp4AvcCscStruct dstStruct = new SceMp4AvcCscStruct();
@@ -112,31 +205,32 @@ public class sceMpegbase extends HLEModule {
     	SceMp4AvcCscStruct srcStruct = new SceMp4AvcCscStruct();
     	srcStruct.read(src);
 
+    	int size1 = ((srcStruct.width + 16) >> 5) * (srcStruct.height >> 1);
+    	int size2 = (srcStruct.width >> 5) * (srcStruct.height >> 1);
+
     	if (log.isDebugEnabled()) {
     		log.debug(String.format("sceMpegBaseYCrCbCopy dstStruct: %s", dstStruct));
     		log.debug(String.format("sceMpegBaseYCrCbCopy srcStruct: %s", srcStruct));
+    		log.debug(String.format("sceMpegBaseYCrCbCopy size1=0x%X, size2=0x%X", size1, size2));
     	}
 
-    	int size1 = ((srcStruct.width + 16) >> 5) * ((srcStruct.height << 4) >> 1);
-    	int size2 = (srcStruct.width >> 5) * ((srcStruct.height << 4) >> 1);
     	Memory mem = Memory.getInstance();
     	if ((flags & 1) != 0) {
-    		mem.memcpy(dstStruct.buffer0, srcStruct.buffer0, size1);
-    		mem.memcpy(dstStruct.buffer1, srcStruct.buffer1, size2);
-    		mem.memcpy(dstStruct.buffer4, srcStruct.buffer4, size1 << 1);
-    		mem.memcpy(dstStruct.buffer5, srcStruct.buffer5, size2 << 1);
+    		copyBlocks(mem, dstStruct.buffer0, srcStruct.buffer0, size1);
+    		copyBlocks(mem, dstStruct.buffer1, srcStruct.buffer1, size2);
+    		copyBlocks(mem, dstStruct.buffer4, srcStruct.buffer4, size1 >> 1);
+    		copyBlocks(mem, dstStruct.buffer5, srcStruct.buffer5, size2 >> 1);
     	}
     	if ((flags & 2) != 0) {
-    		mem.memcpy(dstStruct.buffer2, srcStruct.buffer2, size1);
-    		mem.memcpy(dstStruct.buffer3, srcStruct.buffer3, size2);
-    		mem.memcpy(dstStruct.buffer6, srcStruct.buffer6, size1 << 1);
-    		mem.memcpy(dstStruct.buffer7, srcStruct.buffer7, size2 << 1);
+    		copyBlocks(mem, dstStruct.buffer2, srcStruct.buffer2, size1);
+    		copyBlocks(mem, dstStruct.buffer3, srcStruct.buffer3, size2);
+    		copyBlocks(mem, dstStruct.buffer6, srcStruct.buffer6, size1 >> 1);
+    		copyBlocks(mem, dstStruct.buffer7, srcStruct.buffer7, size2 >> 1);
     	}
 
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0xCE8EB837, version = 150)
     public int sceMpegBaseCscVme(TPointer bufferRGB, TPointer bufferRGB2, int bufferWidth, @BufferInfo(lengthInfo=LengthInfo.fixedLength, length=96, usage=Usage.in) TPointer32 bufferYCrCb) {
     	SceMpegYCrCbBuffer sceMpegYCrCbBuffer = new SceMpegYCrCbBuffer();
@@ -166,55 +260,9 @@ public class sceMpegbase extends HLEModule {
         int[] luma = getIntBuffer(length);
         int[] cb = getIntBuffer(length2);
         int[] cr = getIntBuffer(length2);
-        int dataAddrY = sceMpegYCrCbBuffer.bufferY;
-        int dataAddrCr = sceMpegYCrCbBuffer.bufferCr;
-        int dataAddrCb = sceMpegYCrCbBuffer.bufferCb;
-        if (memoryInt != null) {
-        	// Optimize the most common case
-        	int length4 = length >> 2;
-            int offset = dataAddrY >> 2;
-            for (int i = 0, j = 0; i < length4; i++) {
-            	int value = memoryInt[offset++];
-            	luma[j++] = (value      ) & 0xFF;
-            	luma[j++] = (value >>  8) & 0xFF;
-            	luma[j++] = (value >> 16) & 0xFF;
-            	luma[j++] = (value >> 24) & 0xFF;
-            }
-
-            int length16 = length2 >> 2;
-            offset = dataAddrCb >> 2;
-            for (int i = 0, j = 0; i < length16; i++) {
-            	int value = memoryInt[offset++];
-            	cb[j++] = (value      ) & 0xFF;
-            	cb[j++] = (value >>  8) & 0xFF;
-            	cb[j++] = (value >> 16) & 0xFF;
-            	cb[j++] = (value >> 24) & 0xFF;
-            }
-
-            offset = dataAddrCr >> 2;
-            for (int i = 0, j = 0; i < length16; i++) {
-            	int value = memoryInt[offset++];
-            	cr[j++] = (value      ) & 0xFF;
-            	cr[j++] = (value >>  8) & 0xFF;
-            	cr[j++] = (value >> 16) & 0xFF;
-            	cr[j++] = (value >> 24) & 0xFF;
-            }
-        } else {
-	        IMemoryReader memoryReader = MemoryReader.getMemoryReader(dataAddrY, length, 1);
-	        for (int i = 0; i < length; i++) {
-	        	luma[i] = memoryReader.readNext();
-	        }
-
-	        memoryReader = MemoryReader.getMemoryReader(dataAddrCb, 1);
-	        for (int i = 0; i < length2; i++) {
-	        	cb[i] = memoryReader.readNext();
-	        }
-
-	        memoryReader = MemoryReader.getMemoryReader(dataAddrCr, 1);
-	        for (int i = 0; i < length2; i++) {
-	        	cr[i] = memoryReader.readNext();
-	        }
-        }
+        read(sceMpegYCrCbBuffer.bufferY, length, luma, 0);
+        read(sceMpegYCrCbBuffer.bufferCb, length2, cb, 0);
+        read(sceMpegYCrCbBuffer.bufferCr, length2, cr, 0);
 
         // Convert YCbCr to ABGR
         int[] abgr = getIntBuffer(length);
@@ -261,22 +309,43 @@ public class sceMpegbase extends HLEModule {
         return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0x91929A21, version = 150)
-    public int sceMpegBaseCscAvc(int mpeg, int unknown1, int unknown2, @BufferInfo(lengthInfo=LengthInfo.fixedLength, length=48, usage=Usage.in) TPointer mp4AvcCscStructAddr) {
-    	SceMp4AvcCscStruct mp4AvcCscStruct = new SceMp4AvcCscStruct();
+    public int sceMpegBaseCscAvc(TPointer bufferRGB, int unknown, int bufferWidth, @BufferInfo(lengthInfo=LengthInfo.fixedLength, length=48, usage=Usage.in) TPointer mp4AvcCscStructAddr) {
+        SceMp4AvcCscStruct mp4AvcCscStruct = new SceMp4AvcCscStruct();
     	mp4AvcCscStruct.read(mp4AvcCscStructAddr);
+
+    	int rangeX = 0;
+        int rangeY = 0;
+        int rangeWidth = mp4AvcCscStruct.width << 4;
+        int rangeHeight = mp4AvcCscStruct.height << 4;
 
     	if (log.isDebugEnabled()) {
     		log.debug(String.format("sceMpegBaseCscAvc %s", mp4AvcCscStruct));
     	}
 
-        return 0;
+    	return hleMpegBaseCscAvcRange(bufferRGB, unknown, bufferWidth, mp4AvcCscStruct, rangeX, rangeY, rangeWidth, rangeHeight);
+    }
+
+    @HLEFunction(nid = 0x304882E1, version = 150)
+    public int sceMpegBaseCscAvcRange(TPointer bufferRGB, int unknown, @BufferInfo(lengthInfo=LengthInfo.fixedLength, length=16, usage=Usage.in) TPointer32 rangeAddr, int bufferWidth, @BufferInfo(lengthInfo=LengthInfo.fixedLength, length=48, usage=Usage.in) TPointer mp4AvcCscStructAddr) {
+        SceMp4AvcCscStruct mp4AvcCscStruct = new SceMp4AvcCscStruct();
+    	mp4AvcCscStruct.read(mp4AvcCscStructAddr);
+
+    	int rangeX = rangeAddr.getValue(0) << 4;
+        int rangeY = rangeAddr.getValue(4) << 4;
+        int rangeWidth = rangeAddr.getValue(8) << 4;
+        int rangeHeight = rangeAddr.getValue(12) << 4;
+
+        if (log.isDebugEnabled()) {
+            log.debug(String.format("sceMpegBaseCscAvcRange range x=%d, y=%d, width=%d, height=%d, %s", rangeX, rangeY, rangeWidth, rangeHeight, mp4AvcCscStruct));
+        }
+
+        return hleMpegBaseCscAvcRange(bufferRGB, unknown, bufferWidth, mp4AvcCscStruct, rangeX, rangeY, rangeWidth, rangeHeight);
     }
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x0530BE4E, version = 150)
-    public int sceMpegbase_0530BE4E() {
+    public int sceMpegbase_0530BE4E(int unknown) {
         return 0;
     }
 
