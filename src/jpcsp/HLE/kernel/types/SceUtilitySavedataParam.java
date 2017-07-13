jpcsp/jpcsp

commit 1e3d3f0ca1ea34fe60544ac100249b8571aa3a42

Fixed sceUtilitySavedata mode=MAKEDATA: do not encrypt the saved data.

src/jpcsp/HLE/kernel/types/SceUtilitySavedataParam.java
@@ -624,7 +624,7 @@ public class SceUtilitySavedataParam extends pspUtilityBaseDialog {
         byte[] sdkey = key;
 
         // Write main data.
-        if (CryptoEngine.getSavedataCryptoStatus()) {
+        if (CryptoEngine.getSavedataCryptoStatus() && secure) {
             if (CryptoEngine.getExtractSavedataKeyStatus()) {
                 String tmpPath = Settings.getInstance().getDiscTmpDirectory();
                 new File(tmpPath).mkdirs();

commit 52678d43e6bbaf3241010519fd08e14326053712

Fixed encryption of savedata files which was broken by 1e3d3f0

src/jpcsp/HLE/kernel/types/SceUtilitySavedataParam.java
@@ -614,10 +614,6 @@ public class SceUtilitySavedataParam extends pspUtilityBaseDialog {
         }
     }
 
-    public void save(Memory mem) throws IOException {
-        save(mem, false);
-    }
-
     public void save(Memory mem, boolean secure) throws IOException {
         String path = getBasePath();
 

src/jpcsp/HLE/modules/sceUtility.java
@@ -1226,7 +1226,7 @@ public class sceUtility extends HLEModule {
                     }
 
                     try {
-                        savedataParams.save(mem);
+                        savedataParams.save(mem, true);
                         savedataParams.base.result = 0;
                     } catch (IOException e) {
                         savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_SAVE_ACCESS_ERROR;
@@ -1270,7 +1270,7 @@ public class sceUtility extends HLEModule {
                         }
                         case inProgress: {
                             try {
-                                savedataParams.save(mem);
+                                savedataParams.save(mem, true);
                                 savedataParams.base.result = 0;
                             } catch (IOException e) {
                                 savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_SAVE_ACCESS_ERROR;
@@ -1355,7 +1355,7 @@ public class sceUtility extends HLEModule {
                                 if (log.isDebugEnabled()) {
                                     log.debug(String.format("Saving savedata %s", savedataParams.saveName));
                                 }
-                                savedataParams.save(mem);
+                                savedataParams.save(mem, true);
                                 savedataParams.base.result = 0;
                             } catch (IOException e) {
                                 savedataParams.base.result = SceKernelErrors.ERROR_SAVEDATA_SAVE_ACCESS_ERROR;