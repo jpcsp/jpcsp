720c08e7c1f603bc304c4d04024b70e5e9f05edc
gid15@web.de <gid15@web.de>
2018-02-01 21:20:22 +0100

    Small fix in Kirk command 7: the size of the input data has to be taken from the header.

--- src/jpcsp/crypto/KIRK.java
+++ src/jpcsp/crypto/KIRK.java
@@ -563,8 +563,8 @@ public class KIRK {
 
         AES128 aes = new AES128("AES/CBC/NoPadding");
 
-        byte[] inBuf = new byte[size];
-        in.get(inBuf, 0, size);
+        byte[] inBuf = new byte[header.dataSize];
+        in.get(inBuf, 0, header.dataSize);
         byte[] outBuf = aes.decrypt(inBuf, decKey, priv_iv);
 
         out.clear();
