fea9c582c397495c765ea566afce4ae366c2b70d
gid15@web.de <gid15@web.de>
2019-07-13 23:02:07 +0200

    sceEFlash: added draft functions

--- src/jpcsp/HLE/modules/sceEFlash.java
+++ src/jpcsp/HLE/modules/sceEFlash.java
@@ -18,7 +18,9 @@ package jpcsp.HLE.modules;
 
 import org.apache.log4j.Logger;
 
+import jpcsp.HLE.HLEFunction;
 import jpcsp.HLE.HLEModule;
+import jpcsp.HLE.HLEUnimplemented;
 import jpcsp.HLE.Modules;
 
 /**
@@ -28,4 +30,57 @@ import jpcsp.HLE.Modules;
 public class sceEFlash extends HLEModule {
     public static Logger log = Modules.getLogger("sceEFlash");
 
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x6ED51C3B, version = 661)
+    public int sceEFlash_driver_6ED51C3B() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x8FB8591D, version = 661)
+    public int sceEFlash_driver_8FB8591D() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x966AB475, version = 661)
+    public int sceEFlash_driver_966AB475() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x63F1149A, version = 661)
+    public int sceEFlash_driver_63F1149A() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x9317C91A, version = 661)
+    public int sceEFlash_driver_9317C91A() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x01E0B50F, version = 661)
+    public int sceEFlash_driver_01E0B50F() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x54C7E3BD, version = 661)
+    public int sceEFlash_driver_54C7E3BD() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x9065E889, version = 661)
+    public int sceEFlash_driver_9065E889() {
+    	return 0;
+    }
+
+    @HLEUnimplemented
+    @HLEFunction(nid = 0x8411F4ED, version = 661)
+    public int sceEFlash_driver_8411F4ED() {
+    	return 0;
+    }
 }
