f9a116f78a939f27440e5edd5fd0c70cddc78537
gid15@web.de <gid15@web.de>
2020-05-11 19:05:31 +0200

    Improved compatibility of sceGeListEnQueue: verify that stackDepth < 256 in the optional arguments.

--- src/jpcsp/HLE/modules/sceGe_user.java
+++ src/jpcsp/HLE/modules/sceGe_user.java
@@ -380,6 +380,9 @@ public class sceGe_user extends HLEModule {
 	        if (log.isDebugEnabled()) {
 	        	log.debug(String.format("hleGeListEnQueue optParams=%s", optParams));
 	        }
+	        if (optParams.stackDepth >= 256) {
+	        	return SceKernelErrors.ERROR_INVALID_SIZE;
+	        }
     	}
 
     	boolean useCachedMemory = false;
