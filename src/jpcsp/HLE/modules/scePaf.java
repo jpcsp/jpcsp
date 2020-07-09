f6d6a3cd8f17844e91918058ff63f55e9387d596
gid15@web.de <gid15@web.de>
2020-07-09 18:18:07 +0200

    Implemented a few trivial scePaf functions.

--- src/jpcsp/HLE/modules/scePaf.java
+++ src/jpcsp/HLE/modules/scePaf.java
@@ -21,12 +21,17 @@ import java.util.HashMap;
 import org.apache.log4j.Logger;
 
 import jpcsp.Allegrex.CpuState;
+import jpcsp.HLE.BufferInfo;
+import jpcsp.HLE.BufferInfo.LengthInfo;
+import jpcsp.HLE.BufferInfo.Usage;
 import jpcsp.HLE.CanBeNull;
 import jpcsp.HLE.HLEFunction;
 import jpcsp.HLE.HLEModule;
 import jpcsp.HLE.HLEUnimplemented;
 import jpcsp.HLE.Modules;
+import jpcsp.HLE.PspString;
 import jpcsp.HLE.TPointer;
+import jpcsp.HLE.TPointer32;
 import jpcsp.HLE.modules.SysMemUserForUser.SysMemInfo;
 
 public class scePaf extends HLEModule {
@@ -34,12 +39,12 @@ public class scePaf extends HLEModule {
     private HashMap<Integer, SysMemInfo> allocated = new HashMap<Integer, SysMemInfo>();
 
     @HLEFunction(nid = 0xA138A376, version = 660)
-    public int scePaf_sprintf(CpuState cpu, TPointer buffer, String format) {
+    public int scePaf_A138A376_sprintf(CpuState cpu, TPointer buffer, String format) {
     	return Modules.SysclibForKernelModule.sprintf(cpu, buffer, format);
     }
 
     @HLEFunction(nid = 0x0FCDFA1E, version = 150)
-    public int scePaf_0FCDFA1E(int size) {
+    public int scePaf_0FCDFA1E_malloc(int size) {
     	SysMemInfo sysMemInfo = Modules.SysMemUserForUserModule.malloc(SysMemUserForUser.USER_PARTITION_ID, "scePaf_0FCDFA1E", SysMemUserForUser.PSP_SMEM_Low, size, 0);
     	if (sysMemInfo == null) {
     		return 0;
@@ -52,7 +57,7 @@ public class scePaf extends HLEModule {
     }
 
     @HLEFunction(nid = 0xB4652CFE, version = 150)
-    public int scePaf_B4652CFE(@CanBeNull TPointer destAddr, TPointer srcAddr, int size) {
+    public int scePaf_B4652CFE_memcpy(@CanBeNull TPointer destAddr, TPointer srcAddr, int size) {
     	return Modules.SysclibForKernelModule.memcpy(destAddr, srcAddr, size);
     }
 
@@ -68,10 +73,9 @@ public class scePaf extends HLEModule {
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0x1F02DD65, version = 150)
-    public int scePaf_1F02DD65() {
-    	return 0;
+    public int scePaf_1F02DD65_strncpy(@CanBeNull @BufferInfo(lengthInfo=LengthInfo.nextNextParameter, usage=Usage.out) TPointer destAddr, @CanBeNull @BufferInfo(lengthInfo=LengthInfo.nextParameter, usage=Usage.in) TPointer srcAddr, int size) {
+    	return Modules.SysclibForKernelModule.strncpy(destAddr, srcAddr, size);
     }
 
     @HLEUnimplemented
@@ -86,34 +90,35 @@ public class scePaf extends HLEModule {
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0x3C4BC2CD, version = 150)
-    public int scePaf_3C4BC2CD() {
-    	return 0;
+    public int scePaf_3C4BC2CD_strtol(@CanBeNull PspString string, @CanBeNull TPointer32 endString, int base) {
+    	return Modules.SysclibForKernelModule.strtol(string, endString, base);
     }
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x3E564415, version = 150)
-    public int scePaf_3E564415() {
-    	return 0;
+    public int scePaf_3E564415(@BufferInfo(lengthInfo = LengthInfo.fixedLength, length = 0x620, usage = Usage.inout) TPointer unknownBuffer, @BufferInfo(lengthInfo = LengthInfo.fixedLength, length = 29, usage = Usage.in) TPointer unknownBuffer3, @BufferInfo(usage = Usage.out) TPointer32 unknownBuffer4, @BufferInfo(usage = Usage.out) TPointer32 unknownBuffer5, @BufferInfo(usage = Usage.out) TPointer32 unknownBuffer6, @BufferInfo(usage = Usage.out) TPointer32 unknownBuffer7, @BufferInfo(usage = Usage.out) TPointer32 unknownBuffer8, @BufferInfo(usage = Usage.out) TPointer32 unknownBuffer9) {
+    	return 1;
     }
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x43759F51, version = 150)
-    public int scePaf_43759F51() {
-    	return 0;
+    public int scePaf_43759F51(@BufferInfo(lengthInfo = LengthInfo.fixedLength, length = 0x620, usage = Usage.inout) TPointer unknownBuffer) {
+    	int unknownBuffer2Size = 0xFC;
+    	SysMemInfo sysMemInfo = Modules.SysMemUserForUserModule.malloc(SysMemUserForUser.USER_PARTITION_ID, "scePaf_8C5CC663", SysMemUserForUser.PSP_SMEM_Low, unknownBuffer2Size, 0);
+    	TPointer unknownBuffer2 = new TPointer(getMemory(), sysMemInfo.addr);
+    	unknownBuffer2.clear(unknownBuffer2Size);
+    	return unknownBuffer2.getAddress();
     }
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x4E43A742, version = 150)
-    public int scePaf_4E43A742() {
-    	return 0;
+    public void scePaf_4E43A742(@BufferInfo(lengthInfo = LengthInfo.fixedLength, length = 0x620, usage = Usage.inout) TPointer unknownBuffer, @BufferInfo(lengthInfo = LengthInfo.fixedLength, length = 0xFC, usage = Usage.inout) TPointer unknownBuffer2) {
     }
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x5A12583F, version = 150)
-    public int scePaf_5A12583F() {
-    	return 0;
+    public void scePaf_5A12583F(@BufferInfo(lengthInfo = LengthInfo.fixedLength, length = 0x620, usage = Usage.inout) TPointer unknownBuffer, int unknown, TPointer unknownCallback) {
     }
 
     @HLEUnimplemented
@@ -134,16 +139,18 @@ public class scePaf extends HLEModule {
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0x706ABBFF, version = 150)
-    public int scePaf_706ABBFF() {
-    	return 0;
+    public int scePaf_706ABBFF_strncpy(@CanBeNull @BufferInfo(lengthInfo=LengthInfo.nextNextParameter, usage=Usage.out) TPointer destAddr, @CanBeNull @BufferInfo(lengthInfo=LengthInfo.nextParameter, usage=Usage.in) TPointer srcAddr, int size) {
+    	return Modules.SysclibForKernelModule.strncpy(destAddr, srcAddr, size);
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0x71B92320, version = 150)
-    public int scePaf_71B92320() {
-    	return 0;
+    public void scePaf_71B92320_free(TPointer address) {
+    	SysMemInfo sysMemInfo = allocated.remove(address.getAddress());
+    	if (sysMemInfo == null) {
+    		return;
+    	}
+    	Modules.SysMemUserForUserModule.free(sysMemInfo);
     }
 
     @HLEUnimplemented
@@ -160,8 +167,12 @@ public class scePaf extends HLEModule {
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x8C5CC663, version = 150)
-    public int scePaf_8C5CC663() {
-    	return 0;
+    public int scePaf_8C5CC663(PspString unknownVersion, int unknown1, int unknown2, int unknown3) {
+    	int unknownBufferSize = 0x620;
+    	SysMemInfo sysMemInfo = Modules.SysMemUserForUserModule.malloc(SysMemUserForUser.USER_PARTITION_ID, "scePaf_8C5CC663", SysMemUserForUser.PSP_SMEM_Low, unknownBufferSize, 0);
+    	TPointer unknownBuffer = new TPointer(getMemory(), sysMemInfo.addr);
+    	unknownBuffer.clear(unknownBufferSize);
+    	return unknownBuffer.getAddress();
     }
 
     @HLEUnimplemented
@@ -170,16 +181,14 @@ public class scePaf extends HLEModule {
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0x9A418CCC, version = 150)
-    public int scePaf_9A418CCC() {
-    	return 0;
+    public int scePaf_9A418CCC_memcpy(@CanBeNull TPointer destAddr, TPointer srcAddr, int size) {
+    	return Modules.SysclibForKernelModule.memcpy(destAddr, srcAddr, size);
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0x9CD6C5F4, version = 150)
-    public int scePaf_9CD6C5F4() {
-    	return 0;
+    public int scePaf_9CD6C5F4_memcmp(@BufferInfo(lengthInfo = LengthInfo.nextNextParameter, usage = Usage.in) TPointer src1Addr, @BufferInfo(lengthInfo = LengthInfo.nextParameter, usage = Usage.in) TPointer src2Addr, int size) {
+    	return Modules.SysclibForKernelModule.memcmp(src1Addr, src2Addr, size);
     }
 
     @HLEUnimplemented
@@ -194,10 +203,9 @@ public class scePaf extends HLEModule {
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0xB05D9677, version = 150)
-    public int scePaf_B05D9677() {
-    	return 0;
+    public int scePaf_B05D9677_memcmp(@BufferInfo(lengthInfo = LengthInfo.nextNextParameter, usage = Usage.in) TPointer src1Addr, @BufferInfo(lengthInfo = LengthInfo.nextParameter, usage = Usage.in) TPointer src2Addr, int size) {
+    	return Modules.SysclibForKernelModule.memcmp(src1Addr, src2Addr, size);
     }
 
     @HLEUnimplemented
@@ -206,10 +214,9 @@ public class scePaf extends HLEModule {
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0xBB89C9EA, version = 150)
-    public int scePaf_BB89C9EA() {
-    	return 0;
+    public int scePaf_BB89C9EA_memset(@CanBeNull TPointer destAddr, int data, int size) {
+    	return Modules.SysclibForKernelModule.memset(destAddr, data, size);
     }
 
     @HLEUnimplemented
@@ -224,10 +231,9 @@ public class scePaf extends HLEModule {
     	return 0;
     }
 
-    @HLEUnimplemented
     @HLEFunction(nid = 0xD9E2D6E1, version = 150)
-    public int scePaf_D9E2D6E1() {
-    	return 0;
+    public int scePaf_D9E2D6E1_memset(@CanBeNull TPointer destAddr, int data, int size) {
+    	return Modules.SysclibForKernelModule.memset(destAddr, data, size);
     }
 
     @HLEUnimplemented
