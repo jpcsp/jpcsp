878f878a8c1901eb939024ec5541496218d56f0d
gid15@web.de <gid15@web.de>
2020-07-14 21:39:34 +0200

    Draft implementation for pspvmc_0820B291

--- src/jpcsp/HLE/modules/pspvmc.java
+++ src/jpcsp/HLE/modules/pspvmc.java
@@ -10,9 +10,13 @@ import jpcsp.HLE.HLEModule;
 import jpcsp.HLE.HLEUnimplemented;
 import jpcsp.HLE.Modules;
 import jpcsp.HLE.TPointer;
+import jpcsp.HLE.TPointer32;
 
+// PS1 Virtual Memory Card (VMC).
+// See https://www.psdevwiki.com/ps3/PS1_Savedata
 public class pspvmc extends HLEModule {
     public static Logger log = Modules.getLogger("pspvmc");
+    private static final int PMV_MAGIC = 0x564D5000; // "\0PMV"
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x38A87A12, version = 150)
@@ -23,7 +27,15 @@ public class pspvmc extends HLEModule {
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x0820B291, version = 150)
-    public int pspvmc_0820B291(int unknown0, int unknown1) {
+    public int pspvmc_0820B291(@BufferInfo(lengthInfo = LengthInfo.fixedLength, length = 128, usage = Usage.inout) TPointer vmcBuffer, @BufferInfo(usage = Usage.out) TPointer32 vmcSizeAddr) {
+    	if (vmcBuffer.getValue32(0) != PMV_MAGIC) {
+    		return 0x80109021;
+    	}
+    	if (vmcBuffer.getValue32(8) != 0) {
+    		return 0x80109022;
+    	}
+    	vmcSizeAddr.setValue(vmcBuffer.getValue32(4));
+
     	return 0;
     }
 
