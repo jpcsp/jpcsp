96e11db3af4404d04982e9fa81cd3b2794f59c76
gid15@web.de <gid15@web.de>
2020-11-22 17:19:44 +0100

    Trying to avoid DOS end of line for the linux scripts.

--- start-linux-amd64.sh
+++ start-linux-amd64.sh
@@ -1,2 +1,2 @@
-X#!/bin/sh
-Xjava -Xmx2048m -Xss2m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/linux-amd64 -jar bin/jpcsp.jar $@
+#!/bin/sh
+java -Xmx2048m -Xss2m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/linux-amd64 -jar bin/jpcsp.jar $@
--- start-linux-x86.sh
+++ start-linux-x86.sh
@@ -1,2 +1,2 @@
-X#!/bin/sh
-Xjava -Xmx1024m -Xss2m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/linux-x86 -jar bin/jpcsp.jar $@
+#!/bin/sh
+java -Xmx1024m -Xss2m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/linux-x86 -jar bin/jpcsp.jar $@
